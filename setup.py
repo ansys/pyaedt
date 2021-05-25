a6a63ae32801642e1f57a5966eb1386b5af06eb2
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-05-25 14:31:19 +0200

    Initial Commit

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ if sys.version_info >= (3, 0):
                         "pythonnet >= 2.4.0;platform_system=='Windows'"]
 else:
     install_requires = []
+
 setuptools.setup(
     name="pyaedt",
     version=version,
@@ -17,8 +18,8 @@ setuptools.setup(
     author_email="massimo.capodiferro@ansys.com",
     description="higher-level ANSYS Electronics Destkop framework",
     install_requires=install_requires,
-    packages=['pyaedt', 'pyaedt.misc', 'pyaedt.application', 'pyaedt.modeler', 'pyaedt.modules',
-              'pyaedt.generic', 'pyaedt.generic', 'pyaedt.edb_core'],
+    packages=['pyaedt', 'pyaedt.core.misc', 'pyaedt.core.application', 'pyaedt.core.modeler', 'pyaedt.core.modules',
+              'pyaedt.core.generic', 'pyaedt.core.edb_core'],
     data_files=[('Documentation', glob.iglob('pyaedt/Documentation/**/*', recursive=True)),
                 ('dlls', glob.iglob('pyaedt/core/dlls/**/*', recursive=True)),
                 ('License', glob.iglob('pyaedt/*.md', recursive=True)),
