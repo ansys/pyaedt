d713d393fa5e81e975fb2bf79528e4ebebe77b30
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-06-07 16:29:27 -0400

    Update simplorer.py

--- pyaedt/simplorer.py
+++ pyaedt/simplorer.py
@@ -2,10 +2,6 @@
 Simplorer Class
 ----------------------------------------------------------------
 
-
-Description
-==================================================
-
 This class contains all Simplorer functionalities.
 
 
