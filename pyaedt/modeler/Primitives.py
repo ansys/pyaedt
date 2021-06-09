3a2d8da4c0a261b3f45f5df7877f899397952a01
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2021-06-09 11:49:29 +0200

    Doc/update docs (#58)
    * Docstrings update:
    - remove 2020R2 comments
    - removed picture in readme
    
    * Docstrings update:
    - remove 2020R2 comments
    - removed picture in readme
    - cleaned Doc
    
    * Docstrings update:
    - remove 2020R2 comments
    - removed picture in readme
    - cleaned Doc
    
    * Update Documentation
    
    * Added Optimetrics.py example
    Improved unit test
    
    * Added Optimetrics.py example
    Improved unit test

--- pyaedt/modeler/Primitives.py
+++ pyaedt/modeler/Primitives.py
@@ -2,14 +2,6 @@
 Primitives Library Class
 ----------------------------------------------------------------
 
-Disclaimer
-==================================================
-
-**Copyright (c) 1986-2021, ANSYS Inc. unauthorised use, distribution or duplication is prohibited**
-
-**This tool release is unofficial and not covered by standard Ansys Support license.**
-
-
 Description
 ==================================================
 
@@ -1122,7 +1114,6 @@ class Primitives(object):
                       matname=None, xsection_type=None, xsection_orient=None,
                       xsection_width=1, xsection_topwidth=1, xsection_height=1,
                       xsection_num_seg=0, xsection_bend_type=None):
-
         """Draw a Polyline Object in the 3D modeler
 
         Returns an object of type Polyline, allowing for manipulation of the Polyline.
@@ -1133,9 +1124,6 @@ class Primitives(object):
             array of positions of each point of polyline
             A position is a list of 2D or 3D coordinates. Position coordinate values can be numbers or valid
             AEDT string expressions e.g. [0, 1, 2] or ["0mm", "5mm", "1mm"] or ["x1", "y1"] etc.
-
-        object_id : int, default=None
-            optional object id from the AEDTmodeler, will be ignored if position_list is not None
 
         segment_type: str or PolylineSegment or list, default=None
             default behaviour: all points are connected as "Line" segments
@@ -1185,7 +1173,7 @@ class Primitives(object):
         >>> from pyaedt.desktop import Desktop
         >>> from pyaedt.Maxwell import Maxwell3d
         >>> from pyaedt.modeler.Primitives import PolylineSegment
-        >>> desktop=Desktop(specified_version="2020.2", AlwaysNew=False)
+        >>> desktop=Desktop(specified_version="2021.1", AlwaysNew=False)
         >>> aedtapp = Maxwell3D()
         >>> aedtapp.modeler.model_units = "mm"
         >>> primitives = aedtapp.modeler.primitives
