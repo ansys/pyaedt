d5dc2b37a6529096ae551fafc907817b23141393
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-07-09 15:41:57 +0200

    updated_pipeline

--- pyaedt/__init__.py
+++ pyaedt/__init__.py
@@ -1,16 +1,31 @@
 import os
-from .generic.general_methods import aedt_exception_handler, generate_unique_name, retry_ntimes
-from .hfss3dlayout import Hfss3dLayout
-from .hfss import Hfss
-from .circuit import Circuit
-from .q3d import Q2d, Q3d
-from .siwave import Siwave
-from .icepak import Icepak
-from .edb import Edb
-from .maxwell import Maxwell2d, Maxwell3d
-from .mechanical import Mechanical
-from .rmxprt import Rmxprt
-from .simplorer import Simplorer
-from .desktop import Desktop
-from .emit import Emit
-
+try:
+    from .generic.general_methods import aedt_exception_handler, generate_unique_name, retry_ntimes
+    from .hfss3dlayout import Hfss3dLayout
+    from .hfss import Hfss
+    from .circuit import Circuit
+    from .q3d import Q2d, Q3d
+    from .siwave import Siwave
+    from .icepak import Icepak
+    from .edb import Edb
+    from .maxwell import Maxwell2d, Maxwell3d
+    from .mechanical import Mechanical
+    from .rmxprt import Rmxprt
+    from .simplorer import Simplorer
+    from .desktop import Desktop
+    from .emit import Emit
+except:
+    from .generic.general_methods import aedt_exception_handler, generate_unique_name, retry_ntimes
+    from .hfss3dlayout import Hfss3dLayout
+    from .hfss import Hfss
+    from .circuit import Circuit
+    from .q3d import Q2d, Q3d
+    from .siwave import Siwave
+    from .icepak import Icepak
+    from .edb import Edb
+    from .maxwell import Maxwell2d, Maxwell3d
+    from .mechanical import Mechanical
+    from .rmxprt import Rmxprt
+    from .simplorer import Simplorer
+    from .desktop import Desktop
+    from .emit import Emit
