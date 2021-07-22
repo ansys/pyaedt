12bde2abfde07fe01c299f91ad50f7d3988161df
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-07-22 11:23:45 +0200

    Added Ipy CI

--- _unittest_ironpython/run_unittests_batchmode.cmd
+++ _unittest_ironpython/run_unittests_batchmode.cmd
@@ -1,1 +1,2 @@
-"C:\Program Files\AnsysEM\AnsysEM21.1\Win64\ansysedt.exe" -RunScriptAndExit "C:\actions-runner\_work\PyAEDT\PyAEDT\_unittest_ironpython\run_unittests.py"
+set ANSYSEM_FEATURE_SF6694_NON_GRAPHICAL_COMMAND_EXECUTION_ENABLE=1
+"C:\Program Files\AnsysEM\AnsysEM21.1\Win64\ansysedt.exe" -ng -RunScriptAndExit "C:\actions-runner\_work\PyAEDT\PyAEDT\_unittest_ironpython\run_unittests.py"
