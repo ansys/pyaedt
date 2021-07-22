3f0c4d8fa9740d63e04766b86bf53e11b9d3db72
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-07-22 10:21:22 +0200

    Added Ipy CI

--- _unittest_ironpython/run_unittests_batchmode.cmd
+++ _unittest_ironpython/run_unittests_batchmode.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\AnsysEM\AnsysEM21.1\Win64\ansysedt.exe" -RunScriptAndExit C:\data\pyaedt\_unittest_ironpython\run_unittests.py
+"C:\Program Files\AnsysEM\AnsysEM21.1\Win64\ansysedt.exe" -RunScriptAndExit _unittest_ironpython\run_unittests.py
