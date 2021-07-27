45b5ee59a0bd4a2fdc13ed16852c1e6770e86185
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-07-27 16:10:06 -0400

    Update Model3D.py

--- pyaedt/modeler/Model3D.py
+++ pyaedt/modeler/Model3D.py
@@ -4,7 +4,7 @@ from .Modeler import GeometryModeler
 from  .Primitives3D import Primitives3D
 
 class Modeler3D(GeometryModeler):
-    """Modeler 3D application inteface.
+    """Provides the Modeler 3D application inteface.
     
     Parameters
     ----------
@@ -47,9 +47,9 @@ class Modeler3D(GeometryModeler):
             Full path to the A3DCOMP file.
         component_name : str, optional
             Name of the component. The default is ``None``.
-        variables_to_include : list
+        variables_to_include : list, optinal
              List of variables to include. The default is ``[]``.
-        exclude_region :
+        exclude_region : bool, optional
              Whether to exclude the region. The default is ``False``.
 
         Returns
