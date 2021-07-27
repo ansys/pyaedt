f7f550dc4db8da479fa47c2a1660f7c6ca3f8975
Maxime Rey <maxime.rey@ansys.com>
2021-07-27 15:46:40 +0200

    Fix rebase issue.

--- pyaedt/hfss.py
+++ pyaedt/hfss.py
@@ -795,7 +795,24 @@ class Hfss(FieldAnalysis3D, object):
         -------
         str
             Name of port created when successful, ``False`` otherwise.
+
+        Examples
+        --------
+
+        Create two boxes that will be used to create a lumped port
+        named ``'LumpedPort'``.
+        >>> box1 = hfss.modeler.primitives.create_box([0, 0, 50], [10, 10, 5],
+        ...                                           "BoxLumped1","copper")
+        >>> box2 = hfss.modeler.primitives.create_box([0, 0, 60], [10, 10, 5],
+        ...                                           "BoxLumped2", "copper")
+        >>> hfss.create_lumped_port_between_objects("BoxLumped1", "BoxLumped2",
+        ...                                         hfss.AxisDir.XNeg, 50,
+        ...                                         "LumpedPort", True, False)
+        pyaedt Info: Connection Correctly created
+        'LumpedPort'
+
         """
+
         if not self.modeler.primitives.does_object_exists(startobj) or not self.modeler.primitives.does_object_exists(
                 endobject):
             self._messenger.add_error_message("One or both objects do not exist. Check and retry.")
@@ -1000,8 +1017,24 @@ class Hfss(FieldAnalysis3D, object):
         -------
         :class:`pyaedt.modules.Boundary.BoundaryObject`
             Boundary object.
-        
+
+
+        Examples
+        --------
+
+        Create two boxes that will be used to create a wave port
+        named ``'WavePort'``.
+        >>> box1 = hfss.modeler.primitives.create_box([0,0,0], [10,10,5],
+        ...                                           "BoxWave1", "copper")
+        >>> box2 = hfss.modeler.primitives.create_box([0, 0, 10], [10, 10, 5],
+        ...                                           "BoxWave2", "copper")
+        >>> wave_port = hfss.create_wave_port_between_objects("BoxWave1", "BoxWave2",
+        ...                                                   hfss.AxisDir.XNeg, 50, 1,
+        ...                                                   "WavePort", False)
+        pyaedt Info: Connection Correctly created
+
         """
+
         if not self.modeler.primitives.does_object_exists(startobj) or not self.modeler.primitives.does_object_exists(
                 endobject):
             self._messenger.add_error_message("One or both objects do not exist. Check and retry.")
