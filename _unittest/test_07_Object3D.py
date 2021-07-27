ef268731ae4d5b1831b100a6bf1d386f76566a2d
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-07-27 17:34:19 +0200

    changed way to check if a str is a number

--- _unittest/test_07_Object3D.py
+++ _unittest/test_07_Object3D.py
@@ -87,16 +87,16 @@ class TestClass:
         assert len(bb) == 6
 
     def test_01_delete_object(self):
-        o = self.create_copper_box()
+        o = self.create_copper_box("DeleteBox")
         name = o.name
         o.delete()
         assert not self.aedtapp.modeler.primitives[name]
         assert not o.__dict__
 
     def test_01_subtract_object(self):
-        o1 = self.create_copper_box()
-        o2 = self.create_copper_sphere()
-        o3 = self.create_copper_cylinder()
+        o1 = self.create_copper_box("subtract_box")
+        o2 = self.create_copper_sphere("subtract_sphere")
+        o3 = self.create_copper_cylinder("subtract_cylinder")
 
         a = o1.clone()
         a.subtract(o2)
@@ -109,7 +109,7 @@ class TestClass:
 
 
     def test_02_face_edge_vertex(self):
-        o = self.create_copper_box()
+        o = self.create_copper_box("faces_box")
         object_faces = o.faces
         assert len(object_faces) == 6
         for face in object_faces:
@@ -126,8 +126,8 @@ class TestClass:
             assert len(vertex.position) == 3
 
     def test_03_FacePrimitive(self):
-        o_box = self.create_copper_box()
-        o_sphere = self.create_copper_sphere()
+        o_box = self.create_copper_box("PrimitiveBox")
+        o_sphere = self.create_copper_sphere("PrimitiveSphere")
         planar_face = o_box.faces[0]
         assert planar_face.center == [5.0, 5.0, 5.0]
         planar_face.move_with_offset(1)
@@ -141,18 +141,18 @@ class TestClass:
         assert not non_planar_face.normal
 
     def test_04_object_material_property_invalid(self):
-        o_box = self.create_copper_box()
+        o_box = self.create_copper_box("Invalid1")
         o_box.material_name = "Copper1234Invalid"
         assert o_box.material_name == "copper"
 
     def test_04_object_material_property_valid(self):
-        o_box = self.create_copper_box()
+        o_box = self.create_copper_box("Valid2")
         o_box.material_name = "aluminum"
         assert o_box.material_name == "aluminum"
 
 
     def test_05_object3d_properties_transparency(self):
-        o = self.create_copper_box()
+        o = self.create_copper_box("TransparencyBox")
 
         o.transparency = 50
         assert o.transparency == 1.0
@@ -175,7 +175,7 @@ class TestClass:
         assert o.color == (0, 0, 255)
 
     def test_07_object_clone_and_get_properties(self):
-        o = self.create_copper_box()
+        o = self.create_copper_box("Properties_Box")
         initial_object = o
         initial_object.transparency = 0.76
         new_object = initial_object.clone()
