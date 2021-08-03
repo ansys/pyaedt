a21fcdff57d0f35b32799ba2f34fd7fa61a54620
Maxime Rey <maxime.rey@ansys.com>
2021-08-03 16:05:55 +0200

    Handle the recursion for the module discovery.

--- doctest_coverage.py
+++ doctest_coverage.py
@@ -8,21 +8,21 @@ from argparse import ArgumentParser
 import pyaedt
 import pdb
 
-def discover_modules(entry=pyaedt, recurse=True):
+def discover_modules(entry=pyaedt, recurse=False):
     """Discover the submodules present under an entry point.
 
     If ``recurse=True``, search goes all the way into descendants of the
     entry point. Only modules are gathered, because within a module
     ``doctest``'s discovery can work recursively.
 
-    Should work for ``pyvista`` as entry, but no promises for its more
+    Should work for ``pyaedt`` as entry, but no promises for its more
     general applicability.
 
     Parameters
     ----------
     entry : module, optional
         The entry point of the submodule search. Defaults to the main
-        ``pyvista`` module.
+        ``pyaedt`` module.
 
     recurse : bool, optional
         Whether to recurse into submodules.
@@ -36,37 +36,46 @@ def discover_modules(entry=pyaedt, recurse=True):
 
     entry_name = entry.__name__
     found_modules = {}
-    next_entries = {entry}
+    next_entries = [entry]
     while next_entries:
         next_modules = {}
         for entry in next_entries:
-            for attr_short_name in dir(entry):
-                attr = getattr(entry, attr_short_name)
-                if not isinstance(attr, ModuleType):
+            for attribute in dir(entry):
+                attribute_value = getattr(entry, attribute)
+                if not isinstance(attribute_value, ModuleType):
                     continue
 
-                module_name = attr.__name__
+                module_name = attribute_value.__name__
 
                 if module_name.startswith(entry_name):
-                    next_modules[module_name] = attr
+                    next_modules[module_name] = attribute_value
 
         if not recurse:
-            return next_modules
+            found_modules.update(next_modules)
+            break
+
         # find as-of-yet-undiscovered submodules
-        next_entries = {
+        next_entries = [
             module
             for module_name, module in next_modules.items()
             if module_name not in found_modules
+        ]
 
-        }
-        for k in list(next_modules.keys()):
-            if next_modules[k].__file__.endswith("__init__.py"):
-                del next_modules[k]
         found_modules.update(next_modules)
+
+        # for k in list(next_modules.keys()):
+        #     if next_modules[k].__file__.endswith("__init__.py"):
+        #         del next_modules[k]
+
+
+    # Remove the name package folders from the 'found_modules' dicationary.
+    for k in list(found_modules.keys()):
+        if found_modules[k].__file__.endswith("__init__.py"):
+            del found_modules[k]
 
     return found_modules
 
-def check_doctests(modules=None):
+def evaluate_examples_coverage(modules=None):
     """Check whether doctests can be run as-is without errors.
 
     Parameters
@@ -74,21 +83,7 @@ def check_doctests(modules=None):
     modules : dict, optional
         (module name -> module) mapping of submodules defined in a
         package as returned by ``discover_modules()``. If omitted,
-        ``discover_modules()`` will be called for ``pyvista``.
-
-    respect_skips : bool, optional
-        Whether to ignore doctest examples that contain a DOCTEST:+SKIP
-        directive.
-
-    verbose : bool, optional
-        Whether to print passes/failures as the testing progresses.
-        Failures are printed at the end in every case.
-
-    Returns
-    -------
-    failures : dict of (Exception, str)  tuples
-        An (object name -> (exception raised, failing code)) mapping
-        of failed doctests under the specified modules.
+        ``discover_modules()`` will be called for ``pyaedt``.
 
     """
 
@@ -99,8 +94,8 @@ def check_doctests(modules=None):
     doctests = {}
     for module_name, module in modules.items():
         doctests[module_name] = {
-            dt.name: dt
-            for dt in DocTestFinder(recurse=True).find(module, globs={})
+            doctest.name: doctest
+            for doctest in DocTestFinder(recurse=True).find(module, globs={})
             }
 
     print ("Name                                      Methods     Miss     Cover")
@@ -108,16 +103,20 @@ def check_doctests(modules=None):
 
 
     # loop over doctests in alphabetical order for sanity
-    sorted_modules = sorted(doctests)
-    for module_name in sorted_modules:
+    sorted_module_names = sorted(doctests)
+    all_methods_with_example = []
+    all_methods_without_example = []
+    for module_name in sorted_module_names:
         methods_with_example = []
         methods_without_example = []
 
         for dt_name in doctests[module_name]:
-            if not doctests[module_name][dt_name].examples:
+            if (not doctests[module_name][dt_name].examples) & (not dt_name.startswith("_")):
+                all_methods_without_example.append(dt_name)
                 methods_without_example.append(dt_name)
             else:
                 methods_with_example.append(dt_name)
+                all_methods_with_example.append(dt_name)
 
         total = len(doctests[module_name])
         missing = len(methods_without_example)
@@ -125,46 +124,18 @@ def check_doctests(modules=None):
         if total!=0:
             percentage_covered = covered/total*100
         else:
+            # If no docstring is found in the module, coverage is considered to be 100%.
             percentage_covered = 100
 
-        print(f'{module_name : <37}       {total : ^5}       {missing : ^5}    {percentage_covered}')
+        print(f'{module_name : <37}{total : ^19}{missing : ^4}{percentage_covered:8.2f}')
 
-        # # mock print to suppress output from a few talkative tests
-        # globs = {'print': (lambda *args, **kwargs: ...)}
-        # for iline, example in enumerate(dt.examples, start=1):
-        #     if (not example.source.strip()
-        #             or (respect_skips and skip_pattern.search(example.source))):
-        #         continue
-        #     try:
-        #         exec(example.source, globs)
-        #     except Exception as exc:
-        #         if verbose:
-        #             print(f'FAILED: {dt.name} -- {repr(exc)}')
-        #         erroring_code = ''.join([
-        #             example.source
-        #             for example in dt.examples[:iline]
-        #         ])
-        #         failures[dt_name] = exc, erroring_code
-        #         break
-        # else:
-        #     if verbose:
-        #         print(f'PASSED: {dt.name}')
-
-    # print(f'\n{passes} passes and {fails} failures '
-    #       f'out of {total} total doctests.\n')
-    # if not fails:
-    #     return failures
-
-    # print('List of failures:')
-    # for name, (exc, erroring_code) in failures.items():
-    #     print('-' * 60)
-    #     print(f'{name}:')
-    #     print(indent(erroring_code, '    '))
-    #     print(repr(exc))
-    # print('-' * 60)
-
-    # return failures
+    # Get the stats for the entire package
+    package_total = len(all_methods_with_example) + len(all_methods_without_example)
+    package_missing = len(all_methods_without_example)
+    package_percentage_covering = (package_total - package_missing) / package_total * 100
+    print ('-' * 79)
+    print(f'{"Total" : <37}{package_total : ^19}{package_missing : ^4}{package_percentage_covering:8.2f}')
 
 
 if __name__ == "__main__":
-    check_doctests()
+    evaluate_examples_coverage()
