368424b162402366f0107e9731ca3c84e192e5d6
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-04 16:44:33 -0400

    Update Optimetrics.py

--- examples/00-basic-examples/Optimetrics.py
+++ examples/00-basic-examples/Optimetrics.py
@@ -1,97 +1,99 @@
 """
-
 Optimetrics Example Analysis
---------------------------------------------
-This tutorial shows how you can use PyAedt to create a project in HFSS and create all optimetrics setup
+----------------------------
+This example shows how you can use PyAEDT to create a project in HFSS and create all optimetrics setups.
 """
 # sphinx_gallery_thumbnail_path = 'Resources/optimetrics.png'
 
 from pyaedt import Hfss
 from pyaedt import Desktop
 import os
-###################################
-# Starting Desktop in Non-Graphical mode. User can change boolean to run in graphical mode
+
+###############################################################################
+# Launch AEDT in non-graphical mode. 
+# You can change the Boolean parameter ``NonGraphical`` to ``False`` to launch  
+# AEDT in graphical mode.
 
 NG = False
 d =Desktop("2021.1", NG=NG)
 
-###################################
-# Initialize Hfss object and creates needed design variables. Hfss will have 2 design variables w1 and w2
+###############################################################################
+# Initialize the `Hfss` object and create the needed design variables. 
+# HFSS will have two design variables, ``w1`` and ``w2``.
 
 hfss = Hfss()
 hfss["w1"] = "1mm"
 hfss["w2"] = "100mm"
 
-###################################
-# Create waveguide and sheets on it. This method creates one of the standard waveguides structure and parametrize it.
-# User may also create rectangles of WG openings to assign ports later
+###############################################################################
+# Create a waveguide with sheets on it. 
+# This method creates one of the standard waveguide structures and parametrizes it.
+# You can also create rectangles of waveguide openings and assign ports later.
 
 wg1, p1, p2= hfss.modeler.create_waveguide([0,0,0],hfss.CoordinateSystemAxis.YAxis,"WG17",wg_thickness="w1",wg_length="w2", create_sheets_on_openings=True)
 
-###################################
-# Create Wave ports on sheets
+###############################################################################
+# Create wave ports on the sheets.
 
 hfss.create_wave_port_from_sheet(p1, axisdir=hfss.AxisDir.ZPos, portname="1")
 hfss.create_wave_port_from_sheet(p2, axisdir=hfss.AxisDir.ZPos, portname="2")
 
-###################################
-# Here we create a setup and a frequency sweep that will be used as base for Optimetrics setups
+###############################################################################
+# Create a setup and a frequency sweep to use as the base for the Optimetrics setups.
 
 setup = hfss.create_setup()
 hfss.create_linear_step_sweep(setupname=setup.name, unit='GHz', freqstart=1, freqstop=5, step_size=0.1,
                                                    sweepname="Sweep1", save_fields=True)
-###################################
-# Optimetrics Parametrics Setup
-# ---------------------------------
-# Simple parametrics analysis with output calculations
 
+###############################################################################
+# Optimetrics Parametrics Setup
+# ------------------------------
+# Simple parametrics analysis with output calculations.
 
 sweep = hfss.opti_parametric.add_parametric_setup("w2", "LIN 90mm 200mm 5mm")
 sweep.add_variation("w1", "LIN 0.1mm 2mm 0.1mm")
 sweep.add_calculation(calculation="dB(S(1,1))", calculation_value="2.5GHz", reporttype="Modal Solution Data")
 sweep.add_calculation(calculation="dB(S(1,1))", calculation_value="2.6GHz", reporttype="Modal Solution Data")
 
-###################################
+###############################################################################
 # Optimetrics Sensitivity Setup
-# ---------------------------------
-# Sensitivity analysis with output calculations
+# -----------------------------
+# Sensitivity analysis with output calculations.
 
 sweep2 = hfss.opti_sensitivity.add_sensitivity(calculation="dB(S(1,1))", calculation_value="2.5GHz")
 sweep2.add_calculation(calculation="dB(S(1,1))", calculation_value="2.6GHz")
 
-###################################
+###############################################################################
 # Optimetrics Optimization Setup
-# ---------------------------------
-# Optimization based on a goal and cal calculation
+# ------------------------------
+# Optimization based on goals and calculations.
 
 sweep3 = hfss.opti_optimization.add_optimization(calculation="dB(S(1,1))", calculation_value="2.5GHz")
 sweep3.add_goal(calculation="dB(S(1,1))", calculation_value="2.6GHz")
 sweep3.add_goal(calculation="dB(S(1,1))", calculation_value="2.6GHz", calculation_type="rd", calculation_stop="5GHz")
 sweep3.add_goal(calculation="dB(S(1,1))", calculation_value="2.6GHz", calculation_type="rd", calculation_stop="5GHz",condition="Maximize")
 
-###################################
-# Optimetrics DX Setup
-# ---------------------------------
-# DX based on a goal and cal calculation
+###############################################################################
+# Optimetrics DesignXplorer (DX) Setup
+# ------------------------------------
+# DX based on a goal and calculation.
 
 sweep4 = hfss.opti_designxplorer.add_dx_setup(variables_to_include=["w1"])
 sweep4.add_goal(calculation="dB(S(1,1))", calculation_value="2.6GHz")
 
-###################################
-# Optimetrics DOE Setup
-# ---------------------------------
-# DOE based on a goal and cal calculation
+###############################################################################
+# Optimetrics DOE (Design of Experiments) Setup
+# ---------------------------------------------
+# DOE based on a goal and calculation.
 
 
 sweep5 = hfss.opti_doe.add_doe(calculation="dB(S(1,1))", calculation_value="2.6GHz")
 sweep5.add_goal(calculation="dB(S(1,1))", calculation_value="2.6GHz")
 sweep5.add_calculation(calculation="dB(S(1,1))", calculation_value="2.5GHz")
 
+###############################################################################
+# Close AEDT.
+# -----------
 
-###################################
-# Close Desktop
-# ---------------------------------
 if os.name != "posix":
     d.force_close_desktop()
-
-
