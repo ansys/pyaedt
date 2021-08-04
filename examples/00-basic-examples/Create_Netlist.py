066331de26a2f540f5b86224f4047da6a437af59
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-04 15:09:35 -0400

    Update Create_Netlist.py

--- examples/00-basic-examples/Create_Netlist.py
+++ examples/00-basic-examples/Create_Netlist.py
@@ -1,9 +1,9 @@
 """
-
 Netlist Example Analysis
---------------------------------------------
-# This Example shows how to Import Netlist in AEDT Nexxim
-Netlists supported are HSPICE and, partially, Mentor
+------------------------
+# This example shows how to import Netlist data into a 
+# Circuit design. Netlist files supported are HSPICE and, 
+# partially, Mentor.
 """
 
 
@@ -12,8 +12,8 @@ import sys
 import os
 
 #########################################################
-# Import Packages
-# Setup The local path to the Path Containing AEDTLIb
+# Import packages.
+# Set the local path to the path for AEDTLib.
 
 
 from pyaedt import examples
@@ -32,36 +32,36 @@ print(temp_folder)
 
 
 #########################################################
-# Import of Main Classes needed: Desktop and Circuit
+# Import the main classes needed: Desktop and Circuit.
 
 
 from pyaedt import Circuit
 from pyaedt import Desktop
 
 ###############################################################################
-# Launch Desktop and Circuit
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# This examples will use AEDT 2021.1 in Graphical mode
+# Launch AEDT and Circuit.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This example launches AEDT 2021.1 in graphical mode.
 
-# This examples will use SI units.
+# This examples uses SI units.
 
 desktopVersion = "2021.1"
 
 
 ###############################################################################
-# NonGraphical
+# Launch AEDT in non-graphical mode.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Change Boolean to False to open AEDT in graphical mode
+# Change the Boolean parameter Non-Graphical to False to open AEDT in graphical mode.
 
 NonGraphical = False
 NewThread = True
 
 
 ###############################################################################
-# Launch AEDT and Circuit Design
+# Launch AEDT and Circuit.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Desktop Class initialize Aedt and start it on specified version and specified graphical mode. NewThread Boolean variables defines if
-# a user wants to create a new instance of AEDT or try to connect to existing instance of it
+# The Desktop class initializes AEDT and starts it on specified version in a specified graphical mode. 
+# The Boolean parameter NewThread defines whether to create a new instance of AEDT or try to connect to an existing instance of it
 
 
 
@@ -71,30 +71,30 @@ aedtapp = Circuit()
 
 
 #########################################################
-# Save Project to temp folder. Can be changed
+# Save the project to the temp folder, which can be changed.
 
 aedtapp.save_project(os.path.join(temp_folder, "my_firt_netlist.aedt"))
 
 
 
 #########################################################
-# Define a design variable
-# using $ prefix user will create a project variable
+# Define a design variable.
+# Using $ prefix creates a project variable.
 
 aedtapp["Voltage"]="5"
 
 
 #########################################################
-# Launch command to create Schematic
-# This method will read the netlist and parse it. All components will be parsed but only speficied
-# categories will be mapped. In particular : R, L, C, Q, U, J, V, I components will be mapped
+# Launch the command to create a schematic.
+# This method reads the netlist file and parses it. All components will be parsed but only specified
+# categories will be mapped. In particular, R, L, C, Q, U, J, V, and I components will be mapped.
 
 aedtapp.create_schematic_from_netlist(myfile)
 
 
 
 #########################################################
-# Close Project....or continue adding functionalities
+# Close project after adding any other desired functionalities.
 
 
 
