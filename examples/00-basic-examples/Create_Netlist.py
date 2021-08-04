9fae2ed4403299aab626dc139cb9332f5a5e717a
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-04 15:48:01 -0400

    Update Create_Netlist.py

--- examples/00-basic-examples/Create_Netlist.py
+++ examples/00-basic-examples/Create_Netlist.py
@@ -2,19 +2,16 @@
 Netlist Example Analysis
 ------------------------
 # This example shows how to import Netlist data into a 
-# Circuit design. Netlist files supported are HSPICE and, 
+# Circuit design. Supported Netlist files are HSPICE and, 
 # partially, Mentor.
 """
-
-
 
 import sys
 import os
 
-#########################################################
+###############################################################################
 # Import packages.
 # Set the local path to the path for AEDTLib.
-
 
 from pyaedt import examples
 netlist = examples.download_netlist()
@@ -30,10 +27,8 @@ if not os.path.exists(temp_folder): os.makedirs(temp_folder)
 myfile = os.path.join(netlist)
 print(temp_folder)
 
-
-#########################################################
-# Import the main classes needed: Desktop and Circuit.
-
+###############################################################################
+# Import the main classes needed: `Desktop` and `Circuit`.
 
 from pyaedt import Circuit
 from pyaedt import Desktop
@@ -51,61 +46,47 @@ desktopVersion = "2021.1"
 ###############################################################################
 # Launch AEDT in non-graphical mode.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Change the Boolean parameter Non-Graphical to False to open AEDT in graphical mode.
+# Change the Boolean parameter ``NonGraphical`` to ``False`` to launch AEDT in 
+# graphical mode.
 
 NonGraphical = False
 NewThread = True
 
-
 ###############################################################################
 # Launch AEDT and Circuit.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# The Desktop class initializes AEDT and starts it on specified version in a specified graphical mode. 
-# The Boolean parameter NewThread defines whether to create a new instance of AEDT or try to connect to an existing instance of it
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# The `Desktop` class initializes AEDT and starts it on a specified version in 
+# a specified graphical mode. The Boolean parameter ``NewThread`` defines whether
+# to create a new instance of AEDT or try to connect to existing instance of it.
 
 
 desktop = Desktop(desktopVersion, NonGraphical, NewThread)
 aedtapp = Circuit()
 
-
-#########################################################
+###############################################################################
 # Save the project to the temp folder, which can be changed.
 
 aedtapp.save_project(os.path.join(temp_folder, "my_firt_netlist.aedt"))
 
 
-
-#########################################################
+###############################################################################
 # Define a design variable.
-# Using $ prefix creates a project variable.
+# Using a ``$`` prefix creates a project variable.
 
 aedtapp["Voltage"]="5"
 
 
-#########################################################
-# Launch the command to create a schematic.
-# This method reads the netlist file and parses it. All components will be parsed but only specified
-# categories will be mapped. In particular, R, L, C, Q, U, J, V, and I components will be mapped.
+###############################################################################
+# Create a schematic from a netlist file.
+# This method reads the netlist file and parses it. All components will be parsed 
+# but only specified categories will be mapped. In particular, R, L, C, Q, U, J, V, 
+# and I components will be mapped.
 
 aedtapp.create_schematic_from_netlist(myfile)
 
-
-
-#########################################################
-# Close project after adding any other desired functionalities.
-
-
-
+###############################################################################
+# Close the project (after adding any other desired functionalities).
 
 if os.name != "posix":
     aedtapp.close_project()
     desktop.force_close_desktop()
-
-#########################################################
-
-
-
-
-
