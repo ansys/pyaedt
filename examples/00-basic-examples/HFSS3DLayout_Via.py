09f39d6108d72c7e3b6c2d71e7003e26a98aa02d
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-04 15:55:18 -0400

    Update HFSS3DLayout_Via.py

--- examples/00-basic-examples/HFSS3DLayout_Via.py
+++ examples/00-basic-examples/HFSS3DLayout_Via.py
@@ -1,24 +1,20 @@
 """
-
-HFSS3DLayout Parametric Via Analysis
---------------------------------------------
-This Example shows how to use HFSS3DLayout to create a parametric design, solve it and get results
+HFSS 3D Layout Parametric Via Analysis
+--------------------------------------
+This example shows how to use HFSS 3D Layout to create and solve a parametric design.
 """
 # sphinx_gallery_thumbnail_path = 'Resources/3dlayout.png'
 
-
-
-#########################################
-# Import Hfss3d Layout object and initialize it on version 2021.1
+###############################################################################
+# Import `Hfss3dlayout` object and initialize it on version 2021.1.
 #
 
 from pyaedt import Hfss3dLayout
 import os
 h3d = Hfss3dLayout(specified_version="2021.1", AlwaysNew=False)
 
-#########################################
-# Setup of all parametric variables will be used into the layout
-#
+###############################################################################
+# Set up all parametric variables to be used in the layout.
 
 h3d["viatotrace"]="5mm"
 h3d["viatovia"]="10mm"
@@ -26,25 +22,22 @@ h3d["w1"]="1mm"
 h3d["sp"]="0.5mm"
 h3d["len"]="50mm"
 
-#########################################
-# Create a stackup
-#
+###############################################################################
+# Create a stackup.
 
 h3d.modeler.layers.add_layer("GND","signal",thickness="0", isnegative=True)
 h3d.modeler.layers.add_layer("diel","dielectric",thickness="0.2mm",material="FR4_epoxy")
 h3d.modeler.layers.add_layer("TOP","signal",thickness="0.035mm", elevation="0.2mm")
 
-#########################################
-# Create of signal net and ground planes
-#
+###############################################################################
+# Create a signal net and ground planes.
 
 h3d.modeler.primitives.create_line("TOP", [[0,0],["len",0]],lw="w1", netname="microstrip", name="microstrip")
 h3d.modeler.primitives.create_rectangle("TOP",[0,"-w1/2-sp"],["len","-w1/2-sp-20mm"])
 h3d.modeler.primitives.create_rectangle("TOP",[0,"w1/2+sp"],["len","w1/2+sp+20mm"])
 
-#########################################
-# Create of vias with parametric position
-#
+###############################################################################
+# Create vias with parametric positions.
 
 h3d.modeler.primitives.create_via(x="viatovia",y="-viatotrace",name="via1")
 h3d.modeler.primitives.create_via(x="viatovia",y="viatotrace", name="via2")
@@ -53,31 +46,29 @@ h3d.modeler.primitives.create_via(x="2*viatovia",y="viatotrace")
 h3d.modeler.primitives.create_via(x="3*viatovia",y="-viatotrace")
 h3d.modeler.primitives.create_via(x="3*viatovia",y="viatotrace")
 
-#########################################
-# Add Circuit Ports to setup
-#
+###############################################################################
+# Add circuit ports to the setup.
 
 h3d.create_edge_port("microstrip", 0)
 h3d.create_edge_port("microstrip", 2)
 
-#########################################
-# Create setup and sweep
-#
+###############################################################################
+# Create a setup and sweep.
 
 setup=h3d.create_setup()
 h3d.create_frequency_sweep(setupname=setup.name, unit='GHz', freqstart=3, freqstop=7, num_of_freq_points=1001,
                                                    sweepname="sweep1", sweeptype="interpolating",
                                                    interpolation_tol_percent=1, interpolation_max_solutions=255,
                                                    save_fields=False, use_q3d_for_dc=False)
-#########################################
-# Solve and plot results
-#
+
+###############################################################################
+# Solve and plot results.
 
 h3d.analyze_nominal()
 h3d.post.create_rectangular_plot(["db(S(Port1,Port1))","db(S(Port1,Port2))"],families_dict=h3d.available_variations.nominal_w_values_dict)
 
 #########################################
-# Close Electronic Desktop
+# Close AEDT.
 #
 if os.name != "posix":
     h3d.close_desktop()
