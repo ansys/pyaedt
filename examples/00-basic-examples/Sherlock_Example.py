2c15164820e1dc7da77d211301c814e0035767b9
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-04 17:45:11 -0400

    Update Sherlock_Example.py

--- examples/00-basic-examples/Sherlock_Example.py
+++ examples/00-basic-examples/Sherlock_Example.py
@@ -1,11 +1,9 @@
 """
-
-Icepack Setup From Sherlock Inputs
---------------------------------------------
-This Example shows how to create an Icepak Project starting from Sherlock Files (step and csv) and aedb board 
+Icepack Setup from Sherlock Inputs
+----------------------------------
+This example shows how to create an Icepak project starting from Sherlock files (STEP and CSV) and AEDB board.
 """
 # sphinx_gallery_thumbnail_path = 'Resources/sherlock.png'
-
 
 import time
 import os
@@ -22,9 +20,9 @@ temp_folder = os.path.join(tmpfold, generate_unique_name("Example"))
 if not os.path.exists(temp_folder): os.makedirs(temp_folder)
 print(temp_folder)
 
-######################################
-# Input Variables
-# this lists all input variables needed to the example to run
+###############################################################################
+# Input variables.
+# This lists all input variables needed to run the example.
 
 material_name = 'MaterialExport.csv'
 component_properties =  'TutorialBoardPartsList.csv'
@@ -34,17 +32,18 @@ aedt_odb_design_name = "PCB"
 stackup_thickness = 2.11836
 outline_polygon_name = "poly_14188"
 
-######################################
-# Import pyaedt and start AEDT
-# AEDT will run in nongraphical mode
+###############################################################################
+# Import PyAEDT and launch AEDT.
+# This example launches AEDT 2021.1 in graphical mode.
 
 from pyaedt import Icepak
 from pyaedt import Desktop
 
 ###############################################################################
-# NonGraphical
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Change Boolean to False to open AEDT in graphical mode
+# Launch AEDT in non-graphical mode.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Change the Boolean parameter ``NonGraphical`` to ``False`` to launch AEDT in 
+# graphical mode.
 
 NonGraphical = False
 
@@ -57,77 +56,74 @@ validation = os.path.join(temp_folder, "validation.log")
 file_path = os.path.join(input_dir, component_step)
 project_name = os.path.join(temp_folder, component_step[:-3] + "aedt")
 
-
-############################################################################
-# Create an Icepak project and delete Region to improve performances
+###############################################################################
+# Create an Icepak project and delete the region to improve performance.
 
 ipk = Icepak()
 
-############################################################################
-# Removing region and disabling autosave to speedup import
+###############################################################################
+# Remove the region and disable autosave to speed up the import.
 
 d.disable_autosave()
 ipk.modeler.primitives.delete("Region")
 component_name = "from_ODB"
 
-######################################
-# Import PCB from aedb file
+###############################################################################
+# Import a PCB from AEDB file.
 
 odb_path = os.path.join(input_dir, aedt_odb_project)
 ipk.create_pcb_from_3dlayout(component_name, odb_path, aedt_odb_design_name,extenttype="Polygon",
                                outlinepolygon=outline_polygon_name)
 
-############################################################################
-# create an offset Coordinate system to match odb++ with sherlock step file
-
+###############################################################################
+# Create an offset coordinate system to match odb++ with the Sherlock STEP file.
 
 ipk.modeler.create_coordinate_system([0, 0, stackup_thickness/2], mode="view", view="XY")
 
-######################################
-# import cad
-
+###############################################################################
+# Import a CAD file.
 
 ipk.modeler.import_3d_cad(file_path, refresh_all_ids=False)
 
-############################################################################
-#save cad and refresh properties from aedt file parsing
+###############################################################################
+# Save the CAD file and refresh properties from AEDT file parsing.
 
 ipk.save_project(project_name, refresh_obj_ids_after_save=True)
 
-######################################
-# removing pcb objects
+###############################################################################
+# Remove PCB objects.
 
 ipk.modeler.primitives.delete_objects_containing("pcb", False)
 
-######################################
-# Creating Region
+###############################################################################
+# Create a region.
 
 ipk.modeler.create_air_region(*[20,20,300,20,20,300])
 
-######################################
-# assigning Materials
+###############################################################################
+# Assign materials.
 
 ipk.assignmaterial_from_sherlock_files(component_list, material_list)
 
-############################################################################
-# Deleting Object with no material Assignment
+###############################################################################
+# Delete objects with no material assignments.
 
 no_material_objs = ipk.modeler.primitives.get_objects_by_material("")
 ipk.modeler.primitives.delete(no_material_objs)
 ipk.save_project()
 
-######################################
-# Assign Power to Component Blocks
-
+###############################################################################
+# Assign power to component blocks.
 
 all_objects = ipk.modeler.primitives.object_names
 
-######################################
-# Assign Power blocks
+###############################################################################
+# Assign power blocks.
+
 total_power = ipk.assign_block_from_sherlock_file(component_list)
 
-######################################
-# Setup and Boundaries
+###############################################################################
+# Set up boundaries
 
 ipk.mesh.automatic_mesh_pcb(4)
 
@@ -139,14 +135,14 @@ setup1.props["Secondary Gradient"] =True
 setup1.update()
 ipk.assign_openings(ipk.modeler.primitives.get_object_faces("Region"))
 
-############################################################################
-# Check for intersection using Validation and fix it by assigning Priorities
-
+###############################################################################
+# Check for intersection using validation and fix it by assigning priorities.
 
 ipk.assign_priority_on_intersections()
 
-######################################
-# Saving and closing
+###############################################################################
+# Save and close the project.
+
 ipk.save_project()
 
 
@@ -155,7 +151,3 @@ if os.name != "posix":
     ipk.close_desktop()
 print("Elapsed time: {}".format(datetime.timedelta(seconds=end)))
 print("Project Saved in {} ".format(temp_folder))
-
-
-
-
