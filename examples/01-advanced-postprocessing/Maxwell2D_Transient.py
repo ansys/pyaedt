53ac0e9aa2e37f244019bf3fe6e7bf6736a49fe3
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2021-08-10 22:09:49 +0200

    Doc/issue 370 (#376)
    * Rename some variables and improve some comments.
    
    * Fix typo.
    
    * Fix typo.
    
    * Add some links for the required modules.
    
    * Update Maxwell2D_Transient.py
    
    * Update Maxwell2D_Transient.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/01-advanced-postprocessing/Maxwell2D_Transient.py
+++ examples/01-advanced-postprocessing/Maxwell2D_Transient.py
@@ -2,13 +2,22 @@
 
 Maxwell 2D Analysis
 -------------------
-This example shows how you can use PyAedt to create a project in
-in Maxwell 2D and run a transient simulation.
+This example shows how you can use PyAEDT to create a project in Maxwell 2D
+and run a transient simulation. It runs only on Windows using CPython.
 
-To provide the advanced postprocessing features needed for this example, Matplotlib, NumPy, and 
-PyVista must be installed on the machine.
+The following libraries are required for the advanced postprocessing features 
+used in this example:
 
-This examples runs only on Windows using CPython.
+- `Matplotlib <https://pypi.org/project/matplotlib/>`_
+- `Numpty <https://pypi.org/project/numpy/>`_
+- `PyVista <https://pypi.org/project/pyvista/>`_
+
+Install these with:
+
+.. code::
+
+   pip install numpy pyvista matplotlib
+
 """
 
 import os
@@ -17,48 +26,50 @@ from pyaedt import Maxwell2d
 ###############################################################################
 # Launch AEDT in Graphical Mode
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# You change the Boolean parameter ``NonGraphical`` to ``False`` to launch  
+# You change the Boolean parameter ``non_graphical`` to ``False`` to launch  
 # AEDT in graphical mode.
 
-NonGraphical = True
+non_graphical = True
 
 ###############################################################################
 # Insert a Maxwell 2D Design and Save the Project
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This example inserts a Maxwell 2D design and then saves the project.
 
-m2d=Maxwell2d(solution_type="TransientXY", specified_version="2021.1", NG=NonGraphical)
-project_dir = m2d.generate_temp_project_directory("Example")
-m2d.save_project(os.path.join(project_dir,"M2d.aedt"))
+maxwell_2d=Maxwell2d(solution_type="TransientXY", specified_version="2021.1", NG=non_graphical)
+project_dir = maxwell_2d.generate_temp_project_directory("Example")
+maxwell_2d.save_project(os.path.join(project_dir,"M2d.aedt"))
 
 ###############################################################################
 # Create a Rectangle and Duplicate It
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This example creates a rectangle and then duplicates it.
 
-rect1 = m2d.modeler.primitives.create_rectangle([0, 0, 0],[20, 10], "Rectangle1", "copper")
+rect1 = maxwell_2d.modeler.primitives.create_rectangle([0, 0, 0],[20, 10], "Rectangle1", "copper")
 added = rect1.duplicate_along_line([14,0,0])
-rect2 = m2d.modeler.primitives[added[0]]
+rect2 = maxwell_2d.modeler.primitives[added[0]]
 
 ###############################################################################
-# Create a Air Region
+# Create an Air Region
 # ~~~~~~~~~~~~~~~~~~~
-# This comman creates an air region.
+# This command creates an air region.
 
-region = m2d.modeler.primitives.create_region([100, 100, 100, 100, 100, 100])
+region = maxwell_2d.modeler.primitives.create_region([100, 100, 100, 100, 100, 100])
 
 ###############################################################################
 # Assign Windings to Sheets and a Balloon to the Air Region
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-m2d.assign_winding([rect1.name, rect2.name], name="PHA")
-m2d.assign_balloon(region.edges)
+# This example assigns windings to shhets and a balloon too the air region.
+
+maxwell_2d.assign_winding([rect1.name, rect2.name], name="PHA")
+maxwell_2d.assign_balloon(region.edges)
 
 ###############################################################################
 # Add a Transient Setup
 # ~~~~~~~~~~~~~~~~~~~~~
 # This example adds a transient setup.
 
-setup = m2d.create_setup()
+setup = maxwell_2d.create_setup()
 setup.props["StopTime"] ="0.02s"
 setup.props["TimeStep"] = "0.0002s"
 setup.props["SaveFieldsType"] = "Every N Steps"
@@ -72,14 +83,14 @@ setup.update()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~
 # This command creates a rectangular plot.
 
-m2d.post.create_rectangular_plot("InputCurrent(PHA)",primary_sweep_variable="Time", families_dict={"Time":["All"]}, plotname="Winding Plot 1")
+maxwell_2d.post.create_rectangular_plot("InputCurrent(PHA)",primary_sweep_variable="Time", families_dict={"Time":["All"]}, plotname="Winding Plot 1")
 
 ###############################################################################
 # Solve the Model
 # ~~~~~~~~~~~~~~~
 # This command solves the model.
 
-m2d.analyze_nominal()
+maxwell_2d.analyze_nominal()
 
 ###############################################################################
 # Create the Output and Plot It Using PyVista
@@ -93,7 +104,7 @@ face_lists = rect1.faces
 face_lists += rect2.faces
 timesteps=[str(i*1e-3)+"s" for i in range(21)]
 id_list = [f.id for f in face_lists]
-#animatedGif=m2d.post.animate_fields_from_aedtplt_2("Mag_B", id_list, "Surface", intrinsic_dict={'Time': '0s'}, variation_variable="Time",variation_list=timesteps, off_screen=True, export_gif=True)
+#animatedGif=maxwell_2d.post.animate_fields_from_aedtplt_2("Mag_B", id_list, "Surface", intrinsic_dict={'Time': '0s'}, variation_variable="Time",variation_list=timesteps, off_screen=True, export_gif=True)
 
 
 ###############################################
@@ -101,4 +112,4 @@ id_list = [f.id for f in face_lists]
 # ~~~~~~~~~~
 # This command closes AEDT.
 
-m2d.close_desktop()
+maxwell_2d.close_desktop()
