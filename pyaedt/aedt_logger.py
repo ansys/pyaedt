436d35ace95634ab19eba14810fa6c6ee1d79206
Maxime Rey <maxime.rey@ansys.com>
2021-09-17 11:44:14 +0200

    Add the add_logger() method to add logger for the design and project.

--- pyaedt/aedt_logger.py
+++ pyaedt/aedt_logger.py
@@ -21,10 +21,11 @@ class AedtLogger():
             self._file_handler.setLevel(level)
             self._file_handler.setFormatter(formatter)
 
+        self._to_stdout = None
         if to_stdout:
             self._std_out_handler = logging.StreamHandler()
             self._std_out_handler.setLevel(level)
-            self._file_handler.setFormatter(formatter)
+            self._std_out_handler.setFormatter(formatter)
 
         #self._desktop = weakref.ref(desktop)
         self._desktop = desktop
@@ -32,22 +33,31 @@ class AedtLogger():
         self._global.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Global', level))
         self._global.setLevel(level)
 
-        self._project = logging.getLogger(self._main.oDesktop.GetActiveProject().GetName())
-        self._project.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Project', level))
-        self._project.setLevel(level)
-
-        self._design = logging.getLogger(self._main.oDesktop.GetActiveProject().GetActiveDesign().GetName())
-        self._design.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Design', level))
-        self._design.setLevel(level)
-
-        if self._file_handler is not None:
+        if self._file_handler:
             self._global.addHandler(self._file_handler)
-            self._project.addHandler(self._file_handler)
-            self._design.addHandler(self._file_handler)
-        if to_stdout:
+        if self._to_stdout:
             self._global.addHandler(self._std_out_handler)
-            self._project.addHandler(self._std_out_handler)
-            self._design.addHandler(self._std_out_handler)
+
+    def add_logger(self, destination, level=logging.DEBUG):
+        """Add logger for either a project or a design and uniquely identifyit by their name."""
+        if destination != 'Project':
+            self._project = logging.getLogger(self._main.oDesktop.GetActiveProject().GetName())
+            self._project.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Project', level))
+            self._project.setLevel(level)
+            if self._file_handler:
+                self._project.addHandler(self._file_handler)
+            if self._to_stdout:
+                self._project.addHandler(self._std_out_handler)
+        elif destination != 'Design':
+            self._design = logging.getLogger(self._main.oDesktop.GetActiveProject().GetActiveDesign().GetName())
+            self._design.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Design', level))
+            self._design.setLevel(level)
+            if self._file_handler:
+                self._design.addHandler(self._file_handler)
+            if self._to_stdout:
+                self._design.addHandler(self._std_out_handler)
+        else:
+            raise ValueError("The destination must be either 'Project' or 'Design'.")
 
     @property
     def global_logger(self):
