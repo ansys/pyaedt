9ba35eadd1d840f0213c9bc2681508e2da248e7d
Maxime Rey <maxime.rey@ansys.com>
2021-09-20 11:47:08 +0200

    Add unit test for the logger.

---
+++ _unittest/test_36_AedtLogger.py
@@ -0,0 +1,34 @@
+import os
+
+try:
+    import pytest
+except ImportError:
+    import _unittest_ironpython.conf_unittest as pytest
+# Setup paths for module imports
+from _unittest.conftest import scratch_path
+import gc
+
+# Import required modules
+from pyaedt.desktop import Desktop
+
+
+class TestClass:
+    def setup_class(self):
+        self.desktop = Desktop()
+
+    def teardown_class(self):
+        assert self.desktop.close_desktop()
+        gc.collect()
+
+    def test_01_Desktop(self):
+        logger = self.desktop.logger
+        #The default logger level is DEBUGGING.
+        logger.global_logger.info("Debug message for testing.")
+        logger.global_logger.info("Info message for testing.")
+        logger.global_logger.warning("Warning message for testing.")
+        logger.global_logger.error("Error message for testing.")
+        logger.global_logger.info("Critical message for testing.")
+        assert logger.get_messages() == "Warning"
+        logger.clear_messages()
+        assert logger.get_messages() == ""
+        assert True
