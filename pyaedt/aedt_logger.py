e1f18851a3bace5e9907fb92a3b7994e7ed3197f
Maxime Rey <maxime.rey@ansys.com>
2021-09-21 12:00:49 +0200

    Add properties to access project and design logger.

--- pyaedt/aedt_logger.py
+++ pyaedt/aedt_logger.py
@@ -6,23 +6,26 @@ from .import log_handler
 
 class AedtLogger():
 
-    def __init__(self, desktop, level=logging.DEBUG, filename=None, to_stdout=False):
+    def __init__(self, messenger, level=logging.DEBUG, filename=None, to_stdout=False):
 
         """ no env var here..."""
 
         #self._desktop = weakref.ref(desktop)
-        self._desktop = desktop
+        self._messenger = messenger
         self._global = logging.getLogger('global')
-        self._global.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Global', level))
+        self._global.addHandler(log_handler._LogHandler(self._messenger, 'Global', level))
         self._global.setLevel(level)
 
         formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)-8s:%(message)s', datefmt='%Y/%m/%d %H.%M.%S')
 
+        self._file_handler = None
         if filename:
             self._file_handler = logging.FileHandler(filename)
             self._file_handler.setLevel(level)
             self._file_handler.setFormatter(formatter)
             self._global.addHandler(self._file_handler)
+
+        self._std_out_handler = None
         if to_stdout:
             self._std_out_handler = logging.StreamHandler()
             self._std_out_handler.setLevel(level)
@@ -32,31 +35,41 @@ class AedtLogger():
     def add_logger(self, destination, level=logging.DEBUG):
         """Add logger for either an active project or an active design."""
         if destination == 'Project':
-            self._project = logging.getLogger(self.GetActiveProject().GetName())
-            self._project.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Project', level))
+            self._project = logging.getLogger(self._messenger._project_name)
+            self._project.addHandler(log_handler._LogHandler(self._messenger, 'Project', level))
             self._project.setLevel(level)
-            if self._file_handler:
+            if self._file_handler is not None:
                 self._project.addHandler(self._file_handler)
-            if self._to_stdout:
+            if self._std_out_handler is not None:
                 self._project.addHandler(self._std_out_handler)
         elif destination == 'Design':
-            self._design = logging.getLogger(self._desktop.GetActiveProject().GetActiveDesign().GetName())
-            self._design.addHandler(log_handler._LogHandler(self._desktop.messenger, 'Design', level))
+            self._design = logging.getLogger(self._messenger._design_name)
+            self._design.addHandler(log_handler._LogHandler(self._messenger, 'Design', level))
             self._design.setLevel(level)
-            if self._file_handler:
+            if self._file_handler is not None:
                 self._design.addHandler(self._file_handler)
-            if self._to_stdout:
+            if self._std_out_handler is not None:
                 self._design.addHandler(self._std_out_handler)
         else:
             raise ValueError("The destination must be either 'Project' or 'Design'.")
 
     def get_messages(self):
-        self._desktop.messenger.get_messages(self, project_name=None, design_name=None)
+        self._messenger.get_messages(self, project_name=None, design_name=None)
 
     def clear_messages(self):
-        self._desktop.messenger.clear_messages(self, project_name=None, design_name=None, level=2)
+        self._messenger.clear_messages(self, project_name=None, design_name=None, level=2)
 
     @property
     def global_logger(self):
         """Global logger."""
         return self._global
+
+    @property
+    def project_logger(self):
+        """Global logger."""
+        return self._project
+
+    @property
+    def design_logger(self):
+        """Global logger."""
+        return self._design
