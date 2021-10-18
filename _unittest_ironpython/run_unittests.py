3a850f01a9e48c82c244a234c4df30bc57f1ede7
Mohamed Koubaa <koubaa.m@gmail.com>
2021-10-18 14:50:24 -0500

    improve ironpython unit testing (#508)
    * import ironpython unit testing
    
    add command line arg for test filter
    use env var in case AEDT is installed in a non-default location
    
    * Update _unittest_ironpython/run_unittests.py
    
    Co-authored-by: Mohamed Koubaa <mohamed.koubaa@ansys.com>
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- _unittest_ironpython/run_unittests.py
+++ _unittest_ironpython/run_unittests.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 import unittest
@@ -10,8 +11,12 @@ sys.path.append(path_dir)
 
 os.environ["UNITTEST_CURRENT_TEST"] = "1"
 run_dir = os.path.abspath(os.path.dirname(__file__))
-print(run_dir)
 
+args_env = os.environ.get("RUN_UNITTESTS_ARGS", "")
+parser = argparse.ArgumentParser()
+parser.add_argument("--test-filter", "-t", default="test_*.py", help="test filter")
+args = parser.parse_args(args_env.split())
+test_filter = args.test_filter
 
 def discover_and_run(start_dir, pattern=None):
     """Discover and run tests cases, returning the result."""
@@ -24,16 +29,17 @@ def discover_and_run(start_dir, pattern=None):
     # run the test suite
     log_file = os.path.join(start_dir, "runner_unittest.log")
     with open(log_file, "w") as f:
+        f.write("Test filter: {}\n".format(test_filter))
         f.write("Test started {}\n".format(datetime.now()))
         runner = unittest.TextTestRunner(f, verbosity=2)
         result = runner.run(test_suite)
 
 
-discover_and_run(run_dir, pattern="test_*.py")
-
+discover_and_run(run_dir, pattern=test_filter)
 success_file = os.path.join(run_dir, "tests_succeeded.log")
 with open(success_file, "w") as f:
     f.write("ok")
+
 if is_ironpython and "oDesktop" in dir(sys.modules["__main__"]):
     pid = sys.modules["__main__"].oDesktop.GetProcessID()
     if pid > 0:
