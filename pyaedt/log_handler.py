a7f112fd94df0af4044a5c12cfceb92f19a34145
Maxime Rey <maxime.rey@ansys.com>
2021-10-18 11:05:32 +0200

    Revert "Fixed Ironpython"
    This reverts commit 2e197660246393fedd11c6e43546c916666248d0.

--- pyaedt/log_handler.py
+++ pyaedt/log_handler.py
@@ -6,7 +6,7 @@ class LogHandler(logging.Handler):
 
     Parameters
     ----------
-    aedt_app_messenger : :class:`pyaedt.application.MessageManager.AEDTMessageManager`
+    aedt_app_messenger : str
         AEDT app log manager.
     log_destination: str
         AEDT has 3 different logs: `'Global'`, `'Desktop'`, `'Project'`.
@@ -16,8 +16,7 @@ class LogHandler(logging.Handler):
 
     def __init__(self, aedt_app_messenger, log_destination, level=logging.INFO):
         # base class's constructor must be called to set level and filters.
-        logging.Handler.__init__(self, level)
-        # super().__init__(level)
+        super().__init__(level)
         self.destination = log_destination
         self.messenger = aedt_app_messenger
 
