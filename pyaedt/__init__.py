496fcba3fffe9ba5b6120242e00d79402920fd32
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-10-27 11:03:21 +0200

    added get_pyaedt_app method

--- pyaedt/__init__.py
+++ pyaedt/__init__.py
@@ -3,6 +3,9 @@
 import os
 os.environ["SF6694_NON_GRAPHICAL_COMMAND_EXECUTION"] = "1"
 os.environ["SF159726_SCRIPTOBJECT"] = "1"
+from pyaedt.generic.general_methods import aedt_exception_handler, generate_unique_name, retry_ntimes
+from pyaedt.generic.general_methods import is_ironpython, _pythonver, inside_desktop
+from pyaedt.aedt_logger import AedtLogger
 from pyaedt.generic.design_types import (Hfss3dLayout,
                                          Hfss,
                                          Circuit,
@@ -20,8 +23,6 @@ from pyaedt.generic.design_types import (Hfss3dLayout,
                                          get_pyaedt_app,
                                          Desktop)
 
-from pyaedt.generic.general_methods import aedt_exception_handler, generate_unique_name, retry_ntimes
-from pyaedt.generic.general_methods import is_ironpython, _pythonver, inside_desktop
-from pyaedt.aedt_logger import AedtLogger
+
 
 
