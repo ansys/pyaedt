e74d8694367a908304b2b7a0fd1168e317fe3546
Maxime Rey <maxime.rey@ansys.com>
2021-10-29 18:50:27 +0200

    echo error level

--- _unittest_ironpython/run_unittests_batchmode.cmd
+++ _unittest_ironpython/run_unittests_batchmode.cmd
@@ -1,3 +1,4 @@
 set ANSYSEM_FEATURE_SF6694_NON_GRAPHICAL_COMMAND_EXECUTION_ENABLE=1
 set RUN_UNITTESTS_ARGS=%*
 "%ANSYSEM_ROOT211%\ansysedt.exe" -ng -RunScriptAndExit "_unittest_ironpython\run_unittests.py"
+echo %ERRORLEVEL%
