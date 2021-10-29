a3965354abbde69b58f96172df656c45d1342250
Maxime Rey <maxime.rey@ansys.com>
2021-10-29 19:36:50 +0200

    Force the failure in the cmd.

--- _unittest_ironpython/run_unittests_batchmode.cmd
+++ _unittest_ironpython/run_unittests_batchmode.cmd
@@ -2,7 +2,4 @@ set ANSYSEM_FEATURE_SF6694_NON_GRAPHICAL_COMMAND_EXECUTION_ENABLE=1
 set RUN_UNITTESTS_ARGS=%*
 "%ANSYSEM_ROOT211%\ansysedt.exe" -ng -RunScriptAndExit "_unittest_ironpython\run_unittests.py"
 
-if errorlevel 1 (
-   echo Failure Reason Given is %errorlevel%
-   exit /b %errorlevel%
-)
+exit /b 1
