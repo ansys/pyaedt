aa0d063a67b04329406ca505a04b6ef09a7b6565
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-11-10 11:41:42 +0100

    fix tests

--- _unittest/conftest.py
+++ _unittest/conftest.py
@@ -84,12 +84,13 @@ class BasisTest:
                 solution_type=solution_type,
                 specified_version=desktop_version,
             )
+            self.project_name = self.aedtapp.project_name
             self.cache = DesignCache(self.aedtapp)
 
     def teardown_class(self):
         self.aedtapp._desktop.ClearMessages("", "", 3)
-        if self.aedtapp.project_name in self.aedtapp.project_list:
-            self.aedtapp.close_project(name=self.aedtapp.project_name, saveproject=False)
+        if self.project_name in self.aedtapp.project_list:
+            self.aedtapp.close_project(name=self.project_name, saveproject=False)
         self.local_scratch.remove()
         gc.collect()
 
