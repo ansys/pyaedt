ef782659fc35fcaaec6d3961034053d92540fae3
Maxime Rey <maxime.rey@ansys.com>
2021-11-22 20:50:36 +0100

    Mock the stdout for the ironpython logging tests.

--- _unittest/test_14_AedtLogger.py
+++ _unittest/test_14_AedtLogger.py
@@ -3,6 +3,7 @@ import tempfile
 import os
 import io
 import sys
+import unittest.mock
 try:
     import pytest
 except ImportError:
@@ -23,74 +24,74 @@ class TestClass:
         pass
     # @pytest.mark.xfail
     # def test_01_global(self, clean_desktop_messages, clean_desktop, hfss):
-        # logger = hfss.logger
-        # # The default logger level is DEBUGGING.
-        # logger.debug("Global debug message for testing.")
-        # logger.info("Global info message for testing.")
-        # logger.warning("Global warning message for testing.")
-        # logger.error("Global error message for testing.")
-        # logger.info("Global critical message for testing.")
+    #     logger = hfss.logger
+    #     # The default logger level is DEBUGGING.
+    #     logger.debug("Global debug message for testing.")
+    #     logger.info("Global info message for testing.")
+    #     logger.warning("Global warning message for testing.")
+    #     logger.error("Global error message for testing.")
+    #     logger.info("Global critical message for testing.")
 
-        # # Project logger
-        # logger.add_logger("Project")
-        # logger.project.debug("Project debug message for testing.")
-        # logger.project.info("Project info message for testing.")
-        # logger.project.warning("Project warning message for testing.")
-        # logger.project.error("Project error message for testing.")
-        # logger.project.info("Project critical message for testing.")
+    #     # Project logger
+    #     logger.add_logger("Project")
+    #     logger.project.debug("Project debug message for testing.")
+    #     logger.project.info("Project info message for testing.")
+    #     logger.project.warning("Project warning message for testing.")
+    #     logger.project.error("Project error message for testing.")
+    #     logger.project.info("Project critical message for testing.")
 
-        # # Current active design logger
-        # logger.add_logger("Design")
-        # logger.design.debug("Design debug message for testing.")
-        # logger.design.info("Design info message for testing.")
-        # logger.design.warning("Design warning message for testing.")
-        # logger.design.error("Design error message for testing.")
-        # logger.design.info("Design critical message for testing.")
+    #     # Current active design logger
+    #     logger.add_logger("Design")
+    #     logger.design.debug("Design debug message for testing.")
+    #     logger.design.info("Design info message for testing.")
+    #     logger.design.warning("Design warning message for testing.")
+    #     logger.design.error("Design error message for testing.")
+    #     logger.design.info("Design critical message for testing.")
 
-        # global_messages = logger.get_messages().global_level
-        # assert len(global_messages) >= 11
+    #     global_messages = logger.get_messages().global_level
+    #     assert len(global_messages) >= 11
 
-        # pyaedt_version = False
-        # python_version = False
-        # project = False
-        # for message in global_messages:
-        #     if '[info] pyaedt v' in message:
-        #         pyaedt_version = True
-        #         continue
-        #     if '[info] Python version' in message:
-        #         python_version = True
-        #         continue
-        #     if '[info] Project' in message:
-        #         project = True
+    #     pyaedt_version = False
+    #     python_version = False
+    #     project = False
+    #     for message in global_messages:
+    #         if '[info] pyaedt v' in message:
+    #             pyaedt_version = True
+    #             continue
+    #         if '[info] Python version' in message:
+    #             python_version = True
+    #             continue
+    #         if '[info] Project' in message:
+    #             project = True
 
-        # print("#######")
-        # print("Global")
-        # print(global_messages)
-        # assert '[info] Global debug message for testing.' in global_messages
-        # assert '[info] Global info message for testing.' in global_messages
-        # assert '[warning] Global warning message for testing.' in global_messages
-        # assert '[error] Global error message for testing.' in global_messages
-        # assert '[info] Global critical message for testing.' in global_messages
+    #     print("#######")
+    #     print("Global")
+    #     print(global_messages)
+    #     assert '[info] Global debug message for testing.' in global_messages
+    #     assert '[info] Global info message for testing.' in global_messages
+    #     assert '[warning] Global warning message for testing.' in global_messages
+    #     assert '[error] Global error message for testing.' in global_messages
+    #     assert '[info] Global critical message for testing.' in global_messages
 
-        # design_messages = logger.get_messages().design_level
-        # assert len(design_messages) >= 6
-        # assert '[info] Successfully loaded project materials !' in design_messages[0]
-        # assert '[info] Design debug message for testing.' in design_messages[1]
-        # assert '[info] Design info message for testing.' in design_messages[2]
-        # assert '[warning] Design warning message for testing.' in design_messages[3]
-        # assert '[error] Design error message for testing.' in design_messages[4]
-        # assert '[info] Design critical message for testing.' in design_messages[5]
+    #     design_messages = logger.get_messages().design_level
+    #     assert len(design_messages) >= 6
+    #     assert '[info] Successfully loaded project materials !' in design_messages[0]
+    #     assert '[info] Design debug message for testing.' in design_messages[1]
+    #     assert '[info] Design info message for testing.' in design_messages[2]
+    #     assert '[warning] Design warning message for testing.' in design_messages[3]
+    #     assert '[error] Design error message for testing.' in design_messages[4]
+    #     assert '[info] Design critical message for testing.' in design_messages[5]
 
-        # project_messages = logger.get_messages().project_level
-        # assert len(project_messages) >= 5
-        # assert '[info] Project debug message for testing.' in project_messages[0]
-        # assert '[info] Project info message for testing.' in project_messages[1]
-        # assert '[warning] Project warning message for testing.' in project_messages[2]
-        # assert '[error] Project error message for testing.' in project_messages[3]
-        # assert '[info] Project critical message for testing.' in project_messages[4]
+    #     project_messages = logger.get_messages().project_level
+    #     assert len(project_messages) >= 5
+    #     assert '[info] Project debug message for testing.' in project_messages[0]
+    #     assert '[info] Project info message for testing.' in project_messages[1]
+    #     assert '[warning] Project warning message for testing.' in project_messages[2]
+    #     assert '[error] Project error message for testing.' in project_messages[3]
+    #     assert '[info] Project critical message for testing.' in project_messages[4]
 
-        # logger.clear_messages("", "", 2)
-        # assert not logger.get_messages().global_level
+    #     logger.clear_messages("", "", 2)
+    #     assert not logger.get_messages().global_level
 
     def test_02_output_file_with_app_filter(self):
         content = None
@@ -217,18 +218,26 @@ class TestClass:
 
     @pytest.mark.skipif(is_ironpython, reason="stdout redirection does not work in IronPython.")
     def test_05_disable_stdout(self):
-        capture = CaptureStdOut()
-        with capture:
-            logger = AedtLogger(self.aedtapp._messenger, to_stdout=True)
-            logger.info("Info for Global")
-            logger.disable_stdout_log()
-            logger.info ("Info after disabling the stdout handler.")
-            logger.enable_stdout_log()
-            logger.info ("Info after re-enabling the stdout handler.")
+        stream = unittest.mock.MagicMock()
+        stream.write = unittest.mock.MagicMock()
+        stream.write.side_effect = print_to_real_stdout
+        sys.stdout = stream
 
-        assert "pyaedt info: Info for Global" in capture.content
-        assert "Info after disabling the stdout handler." not in capture.content
-        assert "Info after re-enabling the stdout handler." in capture.content
+        logger = AedtLogger(self.aedtapp._messenger, to_stdout=True)
+        logger.info("Info for Global")
+        logger.disable_stdout_log()
+        logger.info ("Info after disabling the stdout handler.")
+        logger.enable_stdout_log()
+        logger.info ("Info after re-enabling the stdout handler.")
+
+        sys.stdout = sys.__stdout__
+
+        stream.write.assert_any_call("Info for Global")
+        stream.write.assert_any_call("Info after disabling the stdout handler.")
+        stream.write.assert_any_call("Info for Global")
+
+def print_to_real_stdout(value: str) -> None:
+    sys.__stdout__.write(value)
 
 
 class CaptureStdOut():
