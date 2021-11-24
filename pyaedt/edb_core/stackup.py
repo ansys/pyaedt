c549de1d0c80f3598afc5475817a332bb6d6df57
svandenb-dev <74993647+svandenb-dev@users.noreply.github.com>
2021-11-24 23:00:51 +0100

    flip stackup layers (#602)
    * flip stackup layers
    
    * style fix
    
    * Fixed ipy issue
    
    * Update pyaedt/edb_core/stackup.py
    
    * Rename lyr in layer.
    
    * Rename lyr in layer.
    
    * Rename lyr variable in layer.
    
    * Remove unexpected transition.
    
    * revert solder ball
    
    * revert solder ball
    
    * revert solder ball
    
    * revert solder ball
    
    * revert solder ball
    
    * revert solder ball
    
    Co-authored-by: Massimo Capodiferro <massimo.capodiferro@ansys.com>
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    Co-authored-by: Maxime Rey <maxime.rey@ansys.com>

--- pyaedt/edb_core/stackup.py
+++ pyaedt/edb_core/stackup.py
@@ -4,9 +4,11 @@ This module contains the `EdbStackup` class.
 """
 from __future__ import absolute_import
 
+import math
 import warnings
 
 from pyaedt.edb_core.EDB_Data import EDBLayers
+from pyaedt.edb_core.general import convert_py_list_to_net_list
 from pyaedt.generic.general_methods import aedt_exception_handler, is_ironpython
 
 try:
@@ -217,6 +219,81 @@ class EdbStackup(object):
             self._edb_value(relative_permittivity_low), self._edb_value(relative_permittivity_high)
         )
         return self._add_dielectric_material_model(name, material_def)
+
+    @aedt_exception_handler
+    def flip_stackup_and_apply_transform(self, angle=0.0, offset_x=0.0, offset_y=0.0, mirror=True):
+        """Flip the current layer stackup of a layout. Transform parameters currently not supported.
+
+        Parameters
+        ----------
+        angle : double
+            The rotation angle applied on the design (not supported for the moment).
+        offset_x : double
+            The x offset value (not supported for the moment.
+        offset_y : double
+            The y offset value (not supported for the moment.
+        mirror : bool
+            mirror the flipped design, default value True (not supported for the moment.
+
+        Returns
+        -------
+        bool
+            ``True`` when succeed ``False`` if not.
+        """
+        lc = self._active_layout.GetLayerCollection()
+        new_lc = self._edb.Cell.LayerCollection()
+        max_elevation = 0.0
+        for layer in lc.Layers(self._edb.Cell.LayerTypeSet.StackupLayerSet):
+            if not 'RadBox' in layer.GetName():  # Ignore RadBox
+                lower_elevation = layer.GetLowerElevation() * 1.0e6
+                upper_elevation = layer.GetUpperElevation() * 1.0e6
+                max_elevation = max([max_elevation, lower_elevation, upper_elevation])
+
+        non_stackup_layers = []
+        for layer in lc.Layers(self._edb.Cell.LayerTypeSet.AllLayerSet):
+            cloned_layer = layer.Clone()
+            if not cloned_layer.IsStackupLayer():
+                non_stackup_layers.append(cloned_layer)
+                continue
+
+            if not 'RadBox' in layer.GetName():
+                upper_elevation = layer.GetUpperElevation() * 1.0e6
+                updated_lower_el = max_elevation - upper_elevation
+                val = self._edb_value("{}um".format(updated_lower_el))
+                cloned_layer.SetLowerElevation(val)
+                new_lc.AddStackupLayerAtElevation(cloned_layer)
+
+        layer_list = convert_py_list_to_net_list(non_stackup_layers)
+        new_lc.AddLayers(layer_list)
+        if self._active_layout.SetLayerCollection(new_lc):
+            cell_name = self._active_layout.GetCell().GetName()
+            cell_inst = self._edb.Cell.Hierarchy.CellInstance.FindByName(self._active_layout, cell_name)
+            cell_trans = cell_inst.GetTransform()
+            _angle = self._edb_value(angle * math.pi / 180.0)
+            _offset_x = self._edb_value(offset_x)
+            _offset_y = self._edb_value(offset_y)
+            cell_trans.SetRotationValue(_angle)
+            cell_trans.SetXOffsetValue(_offset_x)
+            cell_trans.SetYOffsetValue(_offset_y)
+            cell_trans.SetMirror(mirror)
+            cmp_list = [cmp for cmp in self._active_layout.Groups if cmp.GetComponent() is not None]
+            for cmp in cmp_list:
+                cmp_type = cmp.GetComponentType()
+                cmp_prop = cmp.GetComponentProperty().Clone()
+                if cmp_type == self._edb.Definition.ComponentType.IC:
+                    die_prop = cmp_prop.GetDieProperty().Clone()
+                    chip_orientation = die_prop.GetOrientation()
+                    if chip_orientation == self._edb.Definition.DieOrientation.ChipDown:
+                        die_prop.SetOrientation(self._edb.Definition.DieOrientation.ChipUp)
+                        cmp_prop.SetDieProperty(die_prop)
+                    else:
+                        die_prop.SetOrientation(self._edb.Definition.DieOrientation.ChipDown)
+                        cmp_prop.SetDieProperty(die_prop)
+                cmp.SetComponentProperty(cmp_prop)
+            return True
+
+        else:
+            return False
 
     @aedt_exception_handler
     def create_djordjevicsarkar_material(self, name, relative_permittivity, loss_tangent, test_frequency):
