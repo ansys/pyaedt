440ba9017fb00bff17ea845a91c8d33ea317855e
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-11-29 14:54:53 +0100

    Replaced xpos and ypos arguments with location. Now Circuit and Simplorer have uniform way of Working within Pyaedt

--- examples/00-basic-examples/Circuit_Example.py
+++ examples/00-basic-examples/Circuit_Example.py
@@ -61,7 +61,7 @@ setup1.update()
 
 inductor = aedt_app.modeler.schematic.create_inductor("L1", 1e-9, [0, 0])
 resistor = aedt_app.modeler.schematic.create_resistor("R1", 50, [0.0254, 0])
-capacitor = aedt_app.modeler.schematic.create_capacitor("C1", 1e-12, [0.0400, 0])
+capacitor = aedt_app.modeler.schematic.create_capacitor("C1", 1e-12)
 
 ###############################################################################
 # Get Component Pins
@@ -76,8 +76,8 @@ pins_resistor = resistor.pins
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
 # These methods create a port and a ground, which are needed for a circuit anlaysis.
 
-port = aedt_app.modeler.components.create_interface_port("myport", [-0.0254, 0])
-gnd = aedt_app.modeler.components.create_gnd([0.0508, -0.00254])
+port = aedt_app.modeler.components.create_interface_port("myport")
+gnd = aedt_app.modeler.components.create_gnd()
 ###############################################################################
 # Connect Components
 # ~~~~~~~~~~~~~~~~~~
