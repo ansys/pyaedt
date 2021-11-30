7385aaaeedc7593e6a3918b620072e6f50cc5189
Massimo Capodiferro <massimo.capodiferro@ansys.com>
2021-11-30 09:56:54 +0100

    Replaced xpos and ypos arguments with location. Now Circuit and Simplorer have uniform way of Working within Pyaedt Added suggestions

--- pyaedt/__init__.py
+++ pyaedt/__init__.py
@@ -1,5 +1,5 @@
-# Import exception handling here due to:
-# https://github.com/pyansys/PyAEDT/pull/243
+# -*- coding: utf-8 -*-
+
 import os
 
 os.environ["ANSYSEM_FEATURE_SF6694_NON_GRAPHICAL_COMMAND_EXECUTION_ENABLE"] = "1"
@@ -9,8 +9,12 @@ from pyaedt.generic import constants
 from pyaedt.generic.general_methods import aedt_exception_handler, generate_unique_name, _retry_ntimes
 from pyaedt.generic.general_methods import is_ironpython, _pythonver, inside_desktop, convert_remote_object
 from pyaedt.aedt_logger import AedtLogger
-from pyaedt.generic.design_types import (Hfss3dLayout,
-                                         Hfss,
+try:
+    from pyaedt.generic.design_types import Hfss3dLayout
+except:
+    from pyaedt.generic.design_types import Hfss3dLayout
+
+from pyaedt.generic.design_types import (Hfss,
                                          Circuit,
                                          Q2d,
                                          Q3d,
