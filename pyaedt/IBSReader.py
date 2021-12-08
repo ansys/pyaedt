dd88f099103af5299d55057e1bfb0a45f2b863e0
Maxime Rey <maxime.rey@ansys.com>
2021-12-08 16:12:34 +0100

    Remove the 'EndOfStream' condition.

--- pyaedt/IBSReader.py
+++ pyaedt/IBSReader.py
@@ -1,5 +1,5 @@
-import io
 import os
+import typing
 
 Components = []
 ModelSelectors = []
@@ -29,23 +29,22 @@ def read_project(fileName: str):
 
     # Read *.ibis file.
     with open(fileName,'r') as ts:
-        s_line = ts.readline()
+        current_line = ts.readline()
         # Component
-        if IsStartedWith(s_line, "[Component] ") == True:
-            read_component(s_line, ts)
-        elif IsStartedWith(s_line, "[Model] ") == True:
-            replace_model(s_line, ts)
-        elif IsStartedWith(s_line, "[Model Selector] ") == True:
-            read_model_selector(s_line, ts)
+        if IsStartedWith(current_line, "[Component] ") == True:
+            read_component(current_line, ts)
+        elif IsStartedWith(current_line, "[Model] ") == True:
+            replace_model(current_line, ts)
+        elif IsStartedWith(current_line, "[Model Selector] ") == True:
+            read_model_selector(current_line, ts)
 
 # Model
-def replace_model(current_line: str, ts: io.StringIO):
+def replace_model(current_line: str, ts: typing.TextIO):
     global Models
     if IsStartedWith(current_line, "[Model] ") != True:
         return
 
     model = Model()
-
     model.name = current_line.split("]")(1).strip()
     current_line = ts.ReadLine.replace("\t", "").strip()
 
@@ -61,15 +60,12 @@ def replace_model(current_line: str, ts: io.StringIO):
     while ts.AtEndOfStream != True:
         current_line = ts.ReadLine.strip.replace("clamp", "Clamp")
 
-        # Clamp
         if IsStartedWith(current_line, "[GND Clamp]") == True:
             model.Clamp = True
             break
         elif IsStartedWith(current_line, "[GND_Clamp]") == True:
             model.Clamp = True
             break
-
-        # Enable
         elif IsStartedWith(current_line, "Enable ", True) == True:
             model.Enable = current_line[len("Enable") + 1:].strip()
         elif IsStartedWith(current_line, "[Rising Waveform]") == True:
@@ -80,7 +76,7 @@ def replace_model(current_line: str, ts: io.StringIO):
     Models.append(model)
 
 # Model Selector
-def read_model_selector(current_line: str, ts: io.StringIO):
+def read_model_selector(current_line: str, ts: typing.TextIO):
     if IsStartedWith(current_line, "[Model Selector] ") != True :
         return
 
@@ -90,37 +86,33 @@ def read_model_selector(current_line: str, ts: io.StringIO):
     while IsStartedWith(current_line, "|") == True:
         current_line = ts.ReadLine
 
-    if ts.AtEndOfStream == True:
-        return
-
     # Model Selector
     while (IsStartedWith(current_line, "|") is False and current_line.strip() != ""):
-        model_selector.ModelSelectorItems.append(MakeModel(current_line.strip()))
+        model_selector.ModelSelectorItems.append(make_model(current_line.strip()))
         current_line = ts.ReadLine
 
     # ModelSelectorItem
     model_selector.FillModelReference(Models)
     ModelSelectors.append(model_selector)
 
-def MakeModel(s_line: str) -> ModelSelectorItem:
+def make_model(current_line: str) -> ModelSelectorItem:
     item = ModelSelectorItem()
-    i_start = s_line.index(" ", 1)
+    i_start = current_line.index(" ", 1)
 
     if i_start > 0:
-        item.name = s_line[i_start:].strip()
-        item.Description = s_line[i_start:].strip()
+        item.name = current_line[i_start:].strip()
+        item.Description = current_line[i_start:].strip()
 
     return item
 
 # Component
-def read_component(current_line: str, ts: io.StringIO):
-
-    Components = []
+def read_component(current_line: str, ts: typing.TextIO):
+    global Components
     if IsStartedWith(current_line, "[Component] ") != True:
         return
 
     component = Component()
-    component.name = GetComponentName(current_line)
+    component.name = get_component_name(current_line)
     current_line = ts.ReadLine
 
     if IsStartedWith(current_line, "[Manufacturer]") == True:
@@ -129,26 +121,18 @@ def read_component(current_line: str, ts: io.StringIO):
     current_line = ts.ReadLine
 
     while True:
-        curLine = ts.ReadLine
+        current_line = ts.ReadLine
         if IsStartedWith(current_line, "[Package]") == True:
             break
 
-    # '    If IsStartedWith(curLine, "[Package]") = True Then
-    # '        component.Package = Trim(Replace(Replace$(curLine, "[Package]", ""), "|", ""))
-    # '    End If
-
-    FillPackageInfo(component, ts)
+    fill_packageI_info(component, current_line, ts)
 
     # [pin]
-    while IsStartedWith(current_line, "[Pin] ") == True or ts.AtEndOfStream == True:
+    while IsStartedWith(current_line, "[Pin] ") == True:
         current_line = ts.ReadLine
 
-    if ts.AtEndOfStream == True:
-        return
+    # current_line = ts.ReadLine
 
-    current_line = ts.ReadLine
-
-    # [Pin]
     while True:
         current_line = ts.ReadLine
         if IsStartedWith(current_line, "|") == True:
@@ -158,62 +142,59 @@ def read_component(current_line: str, ts: io.StringIO):
         current_line = ts.ReadLine
 
     while IsStartedWith(current_line, "|") == False:
-        component.Pins.append(MakePinObject(current_line))
+        component.Pins.append(make_pin_object(current_line))
         current_line = ts.ReadLine
         if current_line == "":
             break
 
     Components.append(component)
 
-def FillPackageInfo(component: Component, ts3: io.StringIO):
-
-    with open(ts3) as ts:
-        while IsStartedWith(curLine, "|") == True:
-            curLine = ts.readline()
+def fill_packageI_info(component: Component, current_line: str, ts: typing.TextIO):
+    while IsStartedWith(current_line, "|") == True:
+        current_line = ts.readline()
 
     # the component object must be created first.
-    component.R_pkg.FillData("R_pkg", curLine.strip())
-    curLine = ts.ReadLine
-    component.L_pkg.FillData("L_pkg", curLine.strip())
-    curLine = ts.ReadLine
-    component.C_pkg.FillData("C_pkg", curLine.strip())
+    component.R_pkg.FillData("R_pkg", current_line.strip())
+    current_line = ts.ReadLine
+    component.L_pkg.FillData("L_pkg", current_line.strip())
+    current_line = ts.ReadLine
+    component.C_pkg.FillData("C_pkg", current_line.strip())
 
-def GetComponentName(line: str) -> str:
+def get_component_name(line: str) -> str:
     name = ""
     name = line.replace("[Component]", "")
     return name.strip
 
 # Pin
-def MakePinObject(line: str) -> Pin:
+def make_pin_object(line: str) -> Pin:
     pin = Pin()
-    currString = ""
+    current_string = ""
 
-    currString = line.strip().Replace("\t", " ")
-    pin.PinName = GetFirstParameter(currString)
+    current_string = line.strip().Replace("\t", " ")
+    pin.pin_name = get_first_parameter(current_string)
 
-    currString = currString[len(pin.PinName) + 1:].strip()
-    pin.SignalName = GetFirstParameter(currString)
+    current_string = current_string[len(pin.pin_name) + 1:].strip()
+    pin.signal_name = get_first_parameter(current_string)
 
-    currString = currString[len(pin.SignalName) + 1:].strip()
-    pin.modelName = GetFirstParameter(currString)
+    current_string = current_string[len(pin.signal_name) + 1:].strip()
+    pin.model_name = get_first_parameter(current_string)
 
-    currString = currString[len(pin.modelName) + 1:].strip()
-    pin.R_pin = GetFirstParameter(currString)
+    current_string = current_string[len(pin.model_name) + 1:].strip()
+    pin.r_pin = get_first_parameter(current_string)
 
-    currString = currString[len(pin.R_pin) + 1:].strip()
-    pin.L_pin = GetFirstParameter(currString)
+    current_string = current_string[len(pin.r_pin) + 1:].strip()
+    pin.l_pin = get_first_parameter(current_string)
 
-    currString = currString[len(pin.L_pin) + 1:].strip()
-    pin.C_pin = GetFirstParameter(currString)
+    current_string = current_string[len(pin.l_pin) + 1:].strip()
+    pin.c_pin = get_first_parameter(current_string)
 
     return pin
 
-
-def GetFirstParameter(sString: str) ->str:
-    if sString == "":
+def get_first_parameter(line: str) ->str:
+    if line == "":
         return ""
 
-    data = sString.split(" ")
+    data = line.split(" ")
     return data(0).strip()
 
 def IsStartedWith(src: str, find: str, ignore_case: bool=True) -> bool:
