6e15b7ea539026cdff408d062ea39369a7e1752f
Maxime Rey <maxime.rey@ansys.com>
2021-12-10 14:34:59 +0100

    Test ibis reader: components and models.

--- _unittest/test_15_ibs_reader.py
+++ _unittest/test_15_ibs_reader.py
@@ -1,47 +1,25 @@
-# Setup paths for module imports
-import tempfile
 import os
-import io
-import sys
 from pyaedt.generic import ibs_reader
 try:
     import pytest
-    import unittest.mock
 except ImportError:
     import _unittest_ironpython.conf_unittest as pytest
 
 from pyaedt.generic.general_methods import is_ironpython
-# Import required modules
-from pyaedt.aedt_logger import AedtLogger
-from pyaedt import Hfss
+
 
 class TestClass:
-    def setup_class(self):
-        pass
 
-    def teardown_class(self):
-        pass
+    def test_01_read_ibis(self):
+        ibs_reader.read_project(os.path.join(os.getcwd(), "_unittest", "example_models", "u26a_800_modified.ibs" ))
 
-    def test_01_read_ibis_file(self):
-        ibs_reader.read_project(os.path.join(os.getcwd(), "example_models", "u26a_800_modified.ibs" ))
+        assert len(ibs_reader.Components) == 3
+        
+        assert ibs_reader.Components[0].name == "MT47H64M4BP-3_25"
+        assert ibs_reader.Components[1].name == "MT47H32M8BP-3_25"
+        assert ibs_reader.Components[2].name == "MT47H16M16BG-3_25"
 
-
-class CaptureStdOut():
-    """Capture standard output with a context manager."""
-
-    def __init__(self):
-        self._stream = io.StringIO()
-
-    def __enter__(self):
-        sys.stdout = self._stream
-
-    def __exit__(self, type, value, traceback):
-        sys.stdout = sys.__stdout__
-
-    def release(self):
-        self._stream.close()
-
-    @property
-    def content(self):
-        """Return the captured content."""
-        return self._stream.getvalue()
+        assert len(ibs_reader.Models) == 17
+        assert ibs_reader.Models[0].name == 'DQ_FULL_800'
+        assert ibs_reader.Models[1].name == 'DQ_FULL_ODT50_800'
+        assert ibs_reader.Models[16].name == "NF_IN_800"
