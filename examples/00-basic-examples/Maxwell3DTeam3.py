958d16a51ff844ef20f4ab1ad921be346f15774d
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-12-14 16:14:07 +0100

    apply black

--- examples/00-basic-examples/Maxwell3DTeam3.py
+++ examples/00-basic-examples/Maxwell3DTeam3.py
@@ -21,14 +21,20 @@ Solver = "EddyCurrent"
 DesktopVersion = "2021.2"
 NonGraphical = False
 
-M3D = Maxwell3d(projectname=Project_Name,designname=Design_Name,solution_type=Solver,specified_version=DesktopVersion, non_graphical=NonGraphical)
+M3D = Maxwell3d(
+    projectname=Project_Name,
+    designname=Design_Name,
+    solution_type=Solver,
+    specified_version=DesktopVersion,
+    non_graphical=NonGraphical,
+)
 uom = M3D.modeler.model_units = "mm"
 primitives = M3D.modeler.primitives
 
 ###############################################################################
 # Add the variable Coil Position, it will later be used to adjust position of the coil
 Coil_Position = -20
-M3D['Coil_Position'] = str(Coil_Position) + uom  # Creates a design variable in Maxwell
+M3D["Coil_Position"] = str(Coil_Position) + uom  # Creates a design variable in Maxwell
 
 ################################################################################
 # Create TEAM3 aluminium material for the ladder plate
@@ -40,29 +46,33 @@ mat.conductivity = 32780000
 # Draw Geometry and Assign Mesh Operations
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Draw a background region, it will take default Air properties
-M3D.modeler.create_air_region(x_pos=100,y_pos=100,z_pos=100,x_neg=100,y_neg=100,z_neg=100)
+M3D.modeler.create_air_region(x_pos=100, y_pos=100, z_pos=100, x_neg=100, y_neg=100, z_neg=100)
 
 ################################################################################
 # Draw ladder plate and assign 'team3_aluminium'
 M3D.modeler.primitives.create_box([-30, -55, 0], [60, 110, -6.35], name="LadderPlate", matname="team3_aluminium")
 M3D.modeler.primitives.create_box([-20, -35, 0], [40, 30, -6.35], name="CutoutTool1")
 M3D.modeler.primitives.create_box([-20, 5, 0], [40, 30, -6.35], name="CutoutTool2")
-M3D.modeler.subtract('LadderPlate', ['CutoutTool1','CutoutTool2'], keepOriginals=False)
+M3D.modeler.subtract("LadderPlate", ["CutoutTool1", "CutoutTool2"], keepOriginals=False)
 
 ################################################################################
 # Add a mesh refinement to the ladder plate
-M3D.mesh.assign_length_mesh('LadderPlate',maxlength=3,maxel=None,meshop_name='Ladder_Mesh')
+M3D.mesh.assign_length_mesh("LadderPlate", maxlength=3, maxel=None, meshop_name="Ladder_Mesh")
 
 ################################################################################
 # Draw Search Coil and Assign a Stranded Current Excitation
 # The 'stranded' type forces current density to be constant in the coil
-M3D.modeler.primitives.create_cylinder(cs_axis='Z', position=[0,'Coil_Position',15], radius=40, height=20,name="SearchCoil", matname="copper")
-M3D.modeler.primitives.create_cylinder(cs_axis='Z', position=[0,'Coil_Position',15], radius=20, height=20,name="Bore", matname="copper")
-M3D.modeler.subtract('SearchCoil', 'Bore', keepOriginals=False)
-M3D.modeler.section('SearchCoil','YZ')
-M3D.modeler.separate_bodies('SearchCoil_Section1')
-M3D.modeler.primitives.delete('SearchCoil_Section1_Separate1')
-M3D.assign_current(["SearchCoil_Section1"], amplitude=1260, solid = False, name = 'SearchCoil_Excitation')
+M3D.modeler.primitives.create_cylinder(
+    cs_axis="Z", position=[0, "Coil_Position", 15], radius=40, height=20, name="SearchCoil", matname="copper"
+)
+M3D.modeler.primitives.create_cylinder(
+    cs_axis="Z", position=[0, "Coil_Position", 15], radius=20, height=20, name="Bore", matname="copper"
+)
+M3D.modeler.subtract("SearchCoil", "Bore", keepOriginals=False)
+M3D.modeler.section("SearchCoil", "YZ")
+M3D.modeler.separate_bodies("SearchCoil_Section1")
+M3D.modeler.primitives.delete("SearchCoil_Section1_Separate1")
+M3D.assign_current(["SearchCoil_Section1"], amplitude=1260, solid=False, name="SearchCoil_Excitation")
 
 ################################################################################
 # Draw a line we will later use it to plot Bz on, (z-component of Flux Density)
@@ -70,13 +80,13 @@ M3D.assign_current(["SearchCoil_Section1"], amplitude=1260, solid = False, name 
 Line_Points = [["0mm", "-55mm", "0.5mm"], ["0mm", "55mm", "0.5mm"]]
 P1 = primitives.create_polyline(Line_Points, name="Line_AB")
 P2 = primitives.create_polyline(Line_Points, name="Line_AB_MeshRefinement")
-P2.set_crosssection_properties(type='Circle',width='0.5mm')
+P2.set_crosssection_properties(type="Circle", width="0.5mm")
 
 ###############################################################################
 # Setup Maxwell 3D Model
 # ~~~~~~~~~~~~~~~~~~~~~~
 # Add analysis setup with frequency points at 50Hz and 200Hz
-Setup = M3D.create_setup(setupname = "Setup1")
+Setup = M3D.create_setup(setupname="Setup1")
 Setup.props["Frequency"] = "200Hz"
 Setup.props["HasSweepSetup"] = True
 Setup.props["StartValue"] = "50Hz"
@@ -87,22 +97,45 @@ Setup.update()
 ################################################################################
 # Adjust Eddy Effects for LadderPlate and SearchCoil
 # NB, Eddy effect setting is ignored for stranded conductor type used in Search Coil
-M3D.eddy_effects_on(['LadderPlate'],activate=True)
-M3D.eddy_effects_on(['SearchCoil'],activate=False)
+M3D.eddy_effects_on(["LadderPlate"], activate=True)
+M3D.eddy_effects_on(["SearchCoil"], activate=False)
 
 ################################################################################
 # Add a linear parametric sweep for the two coil positions
-sweepname = 'CoilSweep'
-M3D.opti_parametric.add_parametric_setup("Coil_Position", "LIN -20mm 0mm 20mm",parametricname=sweepname)
+sweepname = "CoilSweep"
+M3D.opti_parametric.add_parametric_setup("Coil_Position", "LIN -20mm 0mm 20mm", parametricname=sweepname)
 
 ################################################################################
 # For now we need to use Optimetrics module to tick 'Save Fields' box
-Opt=M3D.ooptimetrics
-Opt.EditSetup ("CoilSweep",["NAME:CoilSweep","IsEnabled:=",True,
-    ["NAME:ProdOptiSetupDataV2","SaveFields:=",True,"CopyMesh:=",False,"SolveWithCopiedMeshOnly:=",True],
-    ["NAME:StartingPoint"],"Sim. Setups:=",["Setup1"],["NAME:Sweeps",
-        ["NAME:SweepDefinition","Variable:=","Coil_Position","Data:=","LIN -20mm 0mm 20mm","OffsetF1:=",False,
-            "Synchronize:=",0]],["NAME:Sweep Operations"],["NAME:Goals"]])
+Opt = M3D.ooptimetrics
+Opt.EditSetup(
+    "CoilSweep",
+    [
+        "NAME:CoilSweep",
+        "IsEnabled:=",
+        True,
+        ["NAME:ProdOptiSetupDataV2", "SaveFields:=", True, "CopyMesh:=", False, "SolveWithCopiedMeshOnly:=", True],
+        ["NAME:StartingPoint"],
+        "Sim. Setups:=",
+        ["Setup1"],
+        [
+            "NAME:Sweeps",
+            [
+                "NAME:SweepDefinition",
+                "Variable:=",
+                "Coil_Position",
+                "Data:=",
+                "LIN -20mm 0mm 20mm",
+                "OffsetF1:=",
+                False,
+                "Synchronize:=",
+                0,
+            ],
+        ],
+        ["NAME:Sweep Operations"],
+        ["NAME:Goals"],
+    ],
+)
 
 # Solve the model, we solve the parametric sweep directly so results of all variations are available.
 M3D.analyze_setup(sweepname)
@@ -124,24 +157,32 @@ Fields.AddNamedExpression("Bz", "Fields")
 ###############################################################################
 # Plot mag(Bz) as a function of frequency for both coil positions
 Plot = M3D.odesign.GetModule("ReportSetup")
-Plot.CreateReport("mag(Bz) Along 'Line_AB' Offset Coil","Fields","Rectangular Plot","Setup1 : LastAdaptive",
-                     ["Context:=","Line_AB","PointCount:=",1001],
-                     ["Distance:=",["All"],"Freq:=",["All"],"Phase:=",["0deg"],"Coil_Position:=",['-20mm']],
-                     ["X Component:=","Distance-60mm","Y Component:=",["mag(Bz)"]])
+Plot.CreateReport(
+    "mag(Bz) Along 'Line_AB' Offset Coil",
+    "Fields",
+    "Rectangular Plot",
+    "Setup1 : LastAdaptive",
+    ["Context:=", "Line_AB", "PointCount:=", 1001],
+    ["Distance:=", ["All"], "Freq:=", ["All"], "Phase:=", ["0deg"], "Coil_Position:=", ["-20mm"]],
+    ["X Component:=", "Distance-60mm", "Y Component:=", ["mag(Bz)"]],
+)
 
-Plot.CreateReport("mag(Bz) Along 'Line_AB' Centred Coil","Fields","Rectangular Plot","Setup1 : LastAdaptive",
-                     ["Context:=","Line_AB","PointCount:=",1001],
-                     ["Distance:=",["All"],"Freq:=",["All"],"Phase:=",["0deg"],"Coil_Position:=",['0mm']],
-                     ["X Component:=","Distance-60mm","Y Component:=",["mag(Bz)"]])
+Plot.CreateReport(
+    "mag(Bz) Along 'Line_AB' Centred Coil",
+    "Fields",
+    "Rectangular Plot",
+    "Setup1 : LastAdaptive",
+    ["Context:=", "Line_AB", "PointCount:=", 1001],
+    ["Distance:=", ["All"], "Freq:=", ["All"], "Phase:=", ["0deg"], "Coil_Position:=", ["0mm"]],
+    ["X Component:=", "Distance-60mm", "Y Component:=", ["mag(Bz)"]],
+)
 
 ###############################################################################
 # Create a plot Mag_J, the induced current density on the surface of the ladder plate
 surflist = primitives.get_object_faces("LadderPlate")
 intrinsic_dict = {"Freq": "50Hz", "Phase": "0deg"}
-M3D.post.create_fieldplot_surface(surflist, "Mag_J",intrinsincDict=intrinsic_dict, plot_name='Mag_J')
+M3D.post.create_fieldplot_surface(surflist, "Mag_J", intrinsincDict=intrinsic_dict, plot_name="Mag_J")
 
 ###############################################################################
 # The electronics desktop is released from the script engine, we leave the desktop and project open.
 M3D.release_desktop(False, False)
-
-
