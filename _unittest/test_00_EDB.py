bd311fce5e61a6988064d76c76d4c5befdf62afa
svandenb-dev <simon.vandenbrouck@ansys.com>
2021-12-17 10:48:03 +0100

    create ports on component unit test fix

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -58,12 +58,6 @@ class TestClass:
         assert self.edbapp.core_padstack.get_via_instance_from_net("GND")
         assert not self.edbapp.core_padstack.get_via_instance_from_net(["GND2"])
 
-    def test_01C_create_ports_on_component(self):
-        net_list = ["M_DQ<1>", "M_DQ<2>", "M_DQ<3>", "M_DQ<4>", "M_DQ<5>"]
-        assert self.edbapp.core_components.create_port_on_component(
-            component="U2A5", net_list=net_list, port_type=SourceType.CoaxPort, do_pingroup=False, reference_net="GND"
-        )
-
     def test_02_get_properties(self):
         assert len(self.edbapp.core_components.components) > 0
         assert len(self.edbapp.core_components.inductors) > 0
