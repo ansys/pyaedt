18bea742bce8cbbcfdf07f7048ce1598537c9b03
Maxime Rey <maxime.rey@ansys.com>
2021-12-24 18:58:13 +0100

    Improve format and fix typo.

--- _unittest/test_15_ibs_reader.py
+++ _unittest/test_15_ibs_reader.py
@@ -3,10 +3,9 @@ from pyaedt.generic import ibis_reader
 
 
 class TestClass:
-
     def test_01_read_ibis(self):
         reader = ibis_reader.IbisReader()
-        ibis = reader.read_project(os.path.join(os.getcwd(), "_unittest", "example_models", "u26a_800_modified.ibs" ))
+        ibis = reader.read_project(os.path.join(os.getcwd(), "_unittest", "example_models", "u26a_800_modified.ibs"))
 
         ibis_components = ibis.components
         assert len(ibis_components) == 6
@@ -17,6 +16,6 @@ class TestClass:
 
         ibis_models = ibis.models
         assert len(ibis_models) == 17
-        assert ibis_models[0].name == 'DQ_FULL_800'
-        assert ibis_models[1].name == 'DQ_FULL_ODT50_800'
+        assert ibis_models[0].name == "DQ_FULL_800"
+        assert ibis_models[1].name == "DQ_FULL_ODT50_800"
         assert ibis_models[16].name == "NF_IN_800"
