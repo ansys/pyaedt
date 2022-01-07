0cb3cf393d4c92c87480e60579ad1c59e3321d93
Maxime Rey <maxime.rey@ansys.com>
2022-01-07 10:33:22 +0100

    Fix IronPython.

--- _unittest/test_01_Design.py
+++ _unittest/test_01_Design.py
@@ -236,4 +236,5 @@ class TestClass:
         assert self.aedtapp.omaterial_manager
 
     def test_27_odesktop(self):
+        print(type(self.aedtapp.odesktop))
         assert str(type(self.aedtapp.odesktop)) == "<class 'win32com.client.CDispatch'>"
