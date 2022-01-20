37fbef67de0c42ea5f70130b4e88d395c4bcf8e8
adimaria <alberto.dimaria@ansys.com>
2022-01-20 23:01:27 +0100

    changes suggested by black

--- _unittest/test_04_GeometryOperators.py
+++ _unittest/test_04_GeometryOperators.py
@@ -333,4 +333,4 @@ class TestClass:
         assert go.is_collinear([1.0, 0.0, 0.0], [-1.0, 0.0, 0.0])
         assert not go.is_collinear([1, 0, 0], [0, 1, 0])
         assert go.is_collinear([1, 1, 1], [-2, -2, -2])
-        assert not go.is_collinear([1, 2, 3], [3., 2., 1.])
+        assert not go.is_collinear([1, 2, 3], [3.0, 2.0, 1.0])
