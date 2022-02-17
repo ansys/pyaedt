bb52df5b30d702eff1085fc2a2ed176b51659a8f
jeremich41 <jeremy.michel@ansys.com>
2022-02-17 10:08:32 +0100

    fix ipy compatibility

--- pyaedt/modeler/Primitives3D.py
+++ pyaedt/modeler/Primitives3D.py
@@ -219,14 +219,14 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def create_polyhedron(
-        self,
-        cs_axis=None,
-        center_position=(0.0, 0.0, 0.0),
-        start_position=(0.0, 1.0, 0.0),
-        height=1.0,
-        num_sides=12,
-        name=None,
-        matname=None,
+            self,
+            cs_axis=None,
+            center_position=(0.0, 0.0, 0.0),
+            start_position=(0.0, 1.0, 0.0),
+            height=1.0,
+            num_sides=12,
+            name=None,
+            matname=None,
     ):
         """Create a regular polyhedron.
 
@@ -487,18 +487,18 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def create_bondwire(
-        self,
-        start_position,
-        end_position,
-        h1=0.2,
-        h2=0,
-        alpha=80,
-        beta=5,
-        bond_type=0,
-        diameter=0.025,
-        facets=6,
-        name=None,
-        matname=None,
+            self,
+            start_position,
+            end_position,
+            h1=0.2,
+            h2=0,
+            alpha=80,
+            beta=5,
+            bond_type=0,
+            diameter=0.025,
+            facets=6,
+            name=None,
+            matname=None,
     ):
         """Create a bondwire.
 
@@ -757,21 +757,21 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def create_equationbased_curve(
-        self,
-        x_t=0,
-        y_t=0,
-        z_t=0,
-        t_start=0,
-        t_end=1,
-        num_points=0,
-        name=None,
-        xsection_type=None,
-        xsection_orient=None,
-        xsection_width=1,
-        xsection_topwidth=1,
-        xsection_height=1,
-        xsection_num_seg=0,
-        xsection_bend_type=None,
+            self,
+            x_t=0,
+            y_t=0,
+            z_t=0,
+            t_start=0,
+            t_end=1,
+            num_points=0,
+            name=None,
+            xsection_type=None,
+            xsection_orient=None,
+            xsection_width=1,
+            xsection_topwidth=1,
+            xsection_height=1,
+            xsection_num_seg=0,
+            xsection_bend_type=None,
     ):
         """Create an equation-based curve.
 
@@ -1009,16 +1009,16 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def create_spiral(
-        self,
-        internal_radius=10,
-        spacing=1,
-        faces=8,
-        turns=10,
-        width=2,
-        thickness=1,
-        elevation=0,
-        material="copper",
-        name=None,
+            self,
+            internal_radius=10,
+            spacing=1,
+            faces=8,
+            turns=10,
+            width=2,
+            thickness=1,
+            elevation=0,
+            material="copper",
+            name=None,
     ):
         """Create a spiral inductor from a polyline.
 
@@ -1167,7 +1167,7 @@ class Primitives3D(Primitives, object):
             self.logger.error("Folder {} does not exist.".format(actor_folder))
             return False
         if not any(fname.endswith(".json") for fname in os.listdir(actor_folder)) or not any(
-            fname.endswith(".a3dcomp") for fname in os.listdir(actor_folder)
+                fname.endswith(".a3dcomp") for fname in os.listdir(actor_folder)
         ):
             self.logger.error("At least one json and one a3dcomp file is needed.")
             return False
@@ -1182,15 +1182,15 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def add_person(
-        self,
-        actor_folder,
-        speed=0.0,
-        global_offset=[0, 0, 0],
-        yaw=0,
-        pitch=0,
-        roll=0,
-        relative_cs_name=None,
-        actor_name=None,
+            self,
+            actor_folder,
+            speed=0.0,
+            global_offset=[0, 0, 0],
+            yaw=0,
+            pitch=0,
+            roll=0,
+            relative_cs_name=None,
+            actor_name=None,
     ):
         """Add a Walking Person Multipart from 3D Components.
 
@@ -1290,15 +1290,15 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def add_vehicle(
-        self,
-        actor_folder,
-        speed=0,
-        global_offset=[0, 0, 0],
-        yaw=0,
-        pitch=0,
-        roll=0,
-        relative_cs_name=None,
-        actor_name=None,
+            self,
+            actor_folder,
+            speed=0,
+            global_offset=[0, 0, 0],
+            yaw=0,
+            pitch=0,
+            roll=0,
+            relative_cs_name=None,
+            actor_name=None,
     ):
         """Add a Moving Vehicle Multipart from 3D Components.
 
@@ -1380,16 +1380,16 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def add_bird(
-        self,
-        actor_folder,
-        speed=0,
-        global_offset=[0, 0, 0],
-        yaw=0,
-        pitch=0,
-        roll=0,
-        flapping_rate=50,
-        relative_cs_name=None,
-        actor_name=None,
+            self,
+            actor_folder,
+            speed=0,
+            global_offset=[0, 0, 0],
+            yaw=0,
+            pitch=0,
+            roll=0,
+            flapping_rate=50,
+            relative_cs_name=None,
+            actor_name=None,
     ):
         """Add a Bird Multipart from 3D Components.
 
@@ -1496,7 +1496,8 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def add_environment(
-        self, env_folder, global_offset=[0, 0, 0], yaw=0, pitch=0, roll=0, relative_cs_name=None, environment_name=None
+            self, env_folder, global_offset=[0, 0, 0], yaw=0, pitch=0, roll=0, relative_cs_name=None,
+            environment_name=None
     ):
         """Add an Environment Multipart Component from Json file.
 
@@ -1812,21 +1813,21 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def _make_double_linked_winding(
-        self,
-        name,
-        color,
-        mat,
-        in_rad,
-        out_rad,
-        height,
-        w_dia,
-        teta,
-        teta_in_wind,
-        turns,
-        turns_in_wind,
-        chamfer,
-        chamf_in_wind,
-        sr,
+            self,
+            name,
+            color,
+            mat,
+            in_rad,
+            out_rad,
+            height,
+            w_dia,
+            teta,
+            teta_in_wind,
+            turns,
+            turns_in_wind,
+            chamfer,
+            chamf_in_wind,
+            sr,
     ):
         list_object = self._make_double_winding(
             name,
@@ -1873,23 +1874,23 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def _make_triple_linked_winding(
-        self,
-        name,
-        color,
-        mat,
-        in_rad,
-        out_rad,
-        height,
-        w_dia,
-        teta,
-        teta_mid_wind,
-        teta_in_wind,
-        turns,
-        turns_mid_wind,
-        turns_in_wind,
-        chamfer,
-        chamf_in_wind,
-        sr,
+            self,
+            name,
+            color,
+            mat,
+            in_rad,
+            out_rad,
+            height,
+            w_dia,
+            teta,
+            teta_mid_wind,
+            teta_in_wind,
+            turns,
+            turns_mid_wind,
+            turns_in_wind,
+            chamfer,
+            chamf_in_wind,
+            sr,
     ):
         list_object = self._make_triple_winding(
             name,
@@ -1950,22 +1951,22 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def _make_double_winding(
-        self,
-        name,
-        color,
-        mat,
-        in_rad,
-        out_rad,
-        height,
-        w_dia,
-        teta,
-        teta_in_wind,
-        turns,
-        turns_in_wind,
-        chamfer,
-        chamf_in_wind,
-        sr,
-        sep_layer,
+            self,
+            name,
+            color,
+            mat,
+            in_rad,
+            out_rad,
+            height,
+            w_dia,
+            teta,
+            teta_in_wind,
+            turns,
+            turns_in_wind,
+            chamfer,
+            chamf_in_wind,
+            sr,
+            sep_layer,
     ):
 
         chamf = self._make_winding_follow_chamfer(chamfer, sr, w_dia, 3)
@@ -1992,24 +1993,24 @@ class Primitives3D(Primitives, object):
 
     @aedt_exception_handler
     def _make_triple_winding(
-        self,
-        name,
-        color,
-        mat,
-        in_rad,
-        out_rad,
-        height,
-        w_dia,
-        teta,
-        teta_mid_wind,
-        teta_in_wind,
-        turns,
-        turns_mid_wind,
-        turns_in_wind,
-        chamfer,
-        chamf_in_wind,
-        sr,
-        sep_layer,
+            self,
+            name,
+            color,
+            mat,
+            in_rad,
+            out_rad,
+            height,
+            w_dia,
+            teta,
+            teta_mid_wind,
+            teta_in_wind,
+            turns,
+            turns_mid_wind,
+            turns_in_wind,
+            chamfer,
+            chamf_in_wind,
+            sr,
+            sep_layer,
     ):
 
         chamf = self._make_winding_follow_chamfer(chamfer, sr, w_dia, 5)
@@ -2104,12 +2105,12 @@ class Primitives3D(Primitives, object):
 
         """
         dictionary_model = {
-            "Number of Windings": {"1": True, "2": True, "3": False, "4": True},
-            "Layer": {"Simple": False, "Double": True, "Triple": True},
-            "Layer Type": {"Separate": False, "Linked": True},
-            "Similar Layer": {"Similar": False, "Different": True},
+            "Number of Windings": {"1": True, "2": False, "3": False, "4": False},
+            "Layer": {"Simple": True, "Double": False, "Triple": False},
+            "Layer Type": {"Separate": True, "Linked": False},
+            "Similar Layer": {"Similar": True, "Different": False},
             "Mode": {"Differential": True, "Common": False},
-            "Wire Section": {"None": True, "Hexagon": False, "Octagon": True, "Circle": False},
+            "Wire Section": {"None": False, "Hexagon": False, "Octagon": True, "Circle": False},
             "Core": {"Name": "Core", "Inner Radius": 11, "Outer Radius": 17, "Height": 7, "Chamfer": 0.8},
             "Outer Winding": {
                 "Name": "Winding",
@@ -2130,6 +2131,31 @@ class Primitives3D(Primitives, object):
         read_file = open(json_file, "r")
         values = json.load(read_file)
 
+        list_keys_values = []
+        for f_key in values.keys():
+            list_keys_values.append(f_key)
+            for s_key in values[f_key].keys():
+                list_keys_values.append(s_key)
+
+        list_keys_dictionary_model = []
+        for f_key in dictionary_model.keys():
+            list_keys_dictionary_model.append(f_key)
+            for s_key in dictionary_model[f_key].keys():
+                list_keys_dictionary_model.append(s_key)
+
+        if len(list_keys_values) != len(list_keys_dictionary_model):
+            self.logger.error("Number of keys of json file is incorrect. "
+                              "All the keys should be the same and at the same place as the example.")
+            return [False, values]
+        keys_are_correct = True
+        for i in range(len(list_keys_values)):
+            if list_keys_values[i] != list_keys_dictionary_model[i]:
+                self.logger.error("The key %s should be written like %s. "
+                                  % (list_keys_values[i], list_keys_dictionary_model[i]))
+                keys_are_correct = False
+        if not keys_are_correct:
+            return return [False, values]
+
         for f_key in values.keys():
             count_true = False
             for s_key in values[f_key].keys():
@@ -2140,7 +2166,7 @@ class Primitives3D(Primitives, object):
                         count_true = True
                 else:
                     self.logger.error(
-                        "A character entered is invalid. The values of the dictionary %s must be boolean", f_key
+                        "A character entered is invalid. The values of the dictionary %s must be boolean" % f_key
                     )
                     are_inequations_checkable = False
                     break
