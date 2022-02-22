3354df57f95e23d8207968fe791b9a0dddae96db
jeremich41 <jeremy.michel@ansys.com>
2022-02-22 10:05:48 +0100

    fix IronPython problems

--- _unittest/test_08_Primitives3D.py
+++ _unittest/test_08_Primitives3D.py
@@ -979,7 +979,7 @@ class TestClass(BasisTest):
 
     @pyaedt_unittest_check_desktop_error
     def test_73_make_winding(self):
-        chamfer = self.aedtapp.modeler._make_winding_follow_chamfer(0.8, 1.1, 2,1)
+        chamfer = self.aedtapp.modeler._make_winding_follow_chamfer(0.8, 1.1, 2, 1)
         winding_list = self.aedtapp.modeler._make_widning("Winding", "", "", 29.9, 52.1, 22.2, 2, 5, 15, chamfer, True)
         assert isinstance(winding_list, list)
         assert isinstance(winding_list[0], Object3d)
