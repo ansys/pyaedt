b2b94c74a13817ad10078d2cc25d6471ccec91e0
Maxime Rey <maxime.rey@ansys.com>
2022-02-24 10:22:47 +0100

    Add unit test for mechanical fixed support.

--- _unittest/test_29_Mechanical.py
+++ _unittest/test_29_Mechanical.py
@@ -90,6 +90,9 @@ class TestClass:
         assert mech.assign_fixed_support(mech.modeler["MyCylinder"].faces[0].id)
         assert mech.assign_frictionless_support(mech.modeler["MyCylinder"].faces[1].id)
 
+        mech = Mechanical(solution_type=self.aedtapp.SOLUTIONS.Mechanical.Thermal)
+        assert not mech.assign_fixed_support(mech.modeler["MyCylinder"].faces[0].id)
+
     def test_08_mesh_settings(self):
         assert self.aedtapp.mesh.initial_mesh_settings
         assert self.aedtapp.mesh.initial_mesh_settings.props
