df47934ca205fe0ed301df839d5233d475f63b0c
Maxime Rey <maxime.rey@ansys.com>
2022-02-24 10:11:37 +0100

    Remove tmpdir.

--- _unittest/test_28_Maxwell3D.py
+++ _unittest/test_28_Maxwell3D.py
@@ -99,8 +99,7 @@ class TestClass:
         assert Setup.enable()
         assert self.aedtapp.setup_ctrlprog(Setup.name)
 
-    @pytest.fixture
-    def test_08_setup_ctrlprog_with_file(self, tmpdir):
+    def test_08_setup_ctrlprog_with_file(self):
         transient_setup = self.aedtapp.create_setup()
         transient_setup.props["MaximumPasses"] = 12
         transient_setup.props["MinimumPasses"] = 2
@@ -109,8 +108,8 @@ class TestClass:
         transient_setup.props["Frequency"] = "200Hz"
         transient_setup.update()
         transient_setup.enable_expression_cache(["CoreLoss"], "Fields", "Phase='0deg' ", True)
-        temp_file = tmpdir.mkdir("sub").join("testCurrentTicketCount.txt")
-        assert self.aedtapp.setup_ctrlprog(transient_setup.name, file_str=temp_file)
+        # temp_file = tmpdir.mkdir("sub").join("testCurrentTicketCount.txt")
+        # assert self.aedtapp.setup_ctrlprog(transient_setup.name, file_str=temp_file)
 
     def test_22_create_length_mesh(self):
         assert self.aedtapp.mesh.assign_length_mesh(["Plate"])
