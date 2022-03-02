390a39c3b97594047ae0bc4dbcd78ca840840534
Maxime Rey <maxime.rey@ansys.com>
2022-03-02 17:48:07 +0100

    No quality check required for the conftest import.

--- pyaedt/conftest.py
+++ pyaedt/conftest.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import pytest
 
 from pyaedt import Desktop
-from pyaedt.doctest_fixtures import hfss_fixtures
-from pyaedt.doctest_fixtures import icepak_fixtures
+from pyaedt.doctest_fixtures import hfss_fixtures  # noqa: F401
+from pyaedt.doctest_fixtures import icepak_fixtures  # noqa: F401
 
 # Import fixtures from other files
 pytest_plugins = [
