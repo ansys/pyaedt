adeca2cbcd35c9ab9159f9b793ea6de87edf03ef
Maxime Rey <maxime.rey@ansys.com>
2022-03-03 14:57:00 +0100

    Add missing import.

--- pyaedt/application/Analysis3DLayout.py
+++ pyaedt/application/Analysis3DLayout.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 
+from pyaedt.application.Analysis import Analysis
 from pyaedt.generic.general_methods import is_ironpython
 from pyaedt.generic.general_methods import pyaedt_function_handler
 from pyaedt.modeler.Model3DLayout import Modeler3DLayout
