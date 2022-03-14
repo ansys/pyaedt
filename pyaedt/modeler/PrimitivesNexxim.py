4120bf4fe4c5b2390419ddb8369240a5a2f1ce92
maxcapodi78 <Shark78>
2022-03-14 12:43:48 +0100

    use AddDynamicLink instead of internal API commands

--- pyaedt/modeler/PrimitivesNexxim.py
+++ pyaedt/modeler/PrimitivesNexxim.py
@@ -1227,7 +1227,7 @@ class NexximComponents(CircuitComponents):
         solution_name : str, optional
             Name of the solution and sweep. The default is ``"Setup1 : Sweep"``.
         extrusion_length : float, str, optional
-            Extrusion length for 2D Models (q2d or Hfss). Default is `None`.
+            Extrusion length for 2D Models (q2d or Hfss) in model units. Default is `None`.
         enable_cable_modeling : bool, optional
             Either if the Hfss Cable modeling has to be enabled for 2D subcircuits.
         default_matrix : str, optional
@@ -1254,74 +1254,72 @@ class NexximComponents(CircuitComponents):
             comp_name = generate_unique_name(pyaedt_app.design_name)
         source_project_path = pyaedt_app.project_file
         source_design_name = pyaedt_app.design_name
-        matrix = None
-        if pyaedt_app.design_type == "HFSS":
-            pin_names = pyaedt_app.get_excitations_name()
-        elif pyaedt_app.design_type == "Q3D Extractor":
-            excts = list(pyaedt_app.oboundary.GetExcitations())
-            i = 0
-            sources = []
-            sinks = []
-            while i < len(excts):
-                if excts[i + 1] == "Source":
-                    sources.append(excts[i])
-                elif excts[i + 1] == "Sink":
-                    sinks.append(excts[i])
-                i += 2
-            pin_names = sources + sinks
-            matrix = ["NAME:Reduce Matrix Choices"] + list(pyaedt_app.omatrix.ListReduceMatrixes())
-        elif pyaedt_app.design_type == "2D Extractor":
-            excts = list(pyaedt_app.oboundary.GetExcitations())
-            pins = []
-            i = 0
-            while i < len(excts):
-                if excts[i + 1] != "ReferenceGround":
-                    pins.append(excts[i])
-                i += 2
-            pin_names = [i + "_in" for i in pins]
-            pin_names.append("Input_ref")
-            pin_names.extend([i + "_out" for i in pins])
-            pin_names.append("Output_ref")
-            matrix = ["NAME:Reduce Matrix Choices"] + list(pyaedt_app.omatrix.ListReduceMatrixes())
-        variables = {}
-        for k, v in pyaedt_app.variable_manager.variables.items():
-            variables[k] = v.string_value
+        # matrix = None
+        # if pyaedt_app.design_type == "HFSS":
+        #     pin_names = pyaedt_app.get_excitations_name()
+        # elif pyaedt_app.design_type == "Q3D Extractor":
+        #     excts = list(pyaedt_app.oboundary.GetExcitations())
+        #     i = 0
+        #     sources = []
+        #     sinks = []
+        #     while i < len(excts):
+        #         if excts[i + 1] == "Source":
+        #             sources.append(excts[i])
+        #         elif excts[i + 1] == "Sink":
+        #             sinks.append(excts[i])
+        #         i += 2
+        #     pin_names = sources + sinks
+        #     matrix = ["NAME:Reduce Matrix Choices"] + list(pyaedt_app.omatrix.ListReduceMatrixes())
+        # elif pyaedt_app.design_type == "2D Extractor":
+        #     excts = list(pyaedt_app.oboundary.GetExcitations())
+        #     pins = []
+        #     i = 0
+        #     while i < len(excts):
+        #         if excts[i + 1] != "ReferenceGround":
+        #             pins.append(excts[i])
+        #         i += 2
+        #     pin_names = [i + "_in" for i in pins]
+        #     pin_names.append("Input_ref")
+        #     pin_names.extend([i + "_out" for i in pins])
+        #     pin_names.append("Output_ref")
+        #     matrix = ["NAME:Reduce Matrix Choices"] + list(pyaedt_app.omatrix.ListReduceMatrixes())
+        # variables = {}
+        # for k, v in pyaedt_app.variable_manager.variables.items():
+        #     variables[k] = v.string_value
         if not solution_name:
             solution_name = pyaedt_app.nominal_sweep
-        comp = self._add_subcircuit_link(
-            comp_name=comp_name,
-            pin_names=pin_names,
-            source_project_path=source_project_path,
-            source_design_name=source_design_name,
-            solution_name=solution_name,
-            image_subcircuit_path="",
-            model_type=pyaedt_app.design_type,
-            variables=variables,
-            extrusion_length_q2d=extrusion_length,
-            matrix=matrix,
-            enable_cable_modeling=enable_cable_modeling,
-            default_matrix=default_matrix,
-        )
-
-        #
-        #
-        # self._app.odesign.AddDynamicLink(
-        #     source_design_name,
-        #     source_project_path,
-        #     comp_name,
-        #     solution_name,
-        #     tline_port,
-        #     default_matrix,
-        #     enable_cable_modeling,
-        #     "Pyaedt Dynamic Link",
+        # comp = self._add_subcircuit_link(
+        #     comp_name=comp_name,
+        #     pin_names=pin_names,
+        #     source_project_path=source_project_path,
+        #     source_design_name=source_design_name,
+        #     solution_name=solution_name,
+        #     image_subcircuit_path="",
+        #     model_type=pyaedt_app.design_type,
+        #     variables=variables,
+        #     extrusion_length_q2d=extrusion_length,
+        #     matrix=matrix,
+        #     enable_cable_modeling=enable_cable_modeling,
+        #     default_matrix=default_matrix,
         # )
+
+        self._app.odesign.AddDynamicLink(
+            source_design_name,
+            source_project_path,
+            comp_name,
+            solution_name,
+            tline_port,
+            default_matrix,
+            enable_cable_modeling,
+            "Pyaedt Dynamic Link",
+        )
         self.refresh_all_ids()
         for el in self.components:
             if comp_name in self.components[el].composed_name:
                 if extrusion_length:
-                    self.components[el].set_property("Length", extrusion_length)
+                    self.components[el].set_property("Length", self.arg_with_dim(extrusion_length))
                 if tline_port and extrusion_length:
-                    self.components[el].set_property("TLineLength", extrusion_length)
+                    self.components[el].set_property("TLineLength", self.arg_with_dim(extrusion_length))
                 return self.components[el]
         return False
 
