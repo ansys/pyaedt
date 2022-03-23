eb6b5d66953acb5e9ab5c1b7ef7f52f8077cf4e4
hzhou <ring630@gmail.com>
2022-03-23 09:34:37 +0100

    bug fix

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -952,5 +952,15 @@ class TestClass(BasisTest, object):
 
     def test_83_set_component_type(self):
         comp = self.edbapp.core_components.components["R2L18"]
+        comp.type = "Resistor"
+        assert comp.type == "Resistor"
+        comp.type = "Inductor"
+        assert comp.type == "Inductor"
         comp.type = "Capacitor"
         assert comp.type == "Capacitor"
+        comp.type = "IO"
+        assert comp.type == "IO"
+        comp.type = "IC"
+        assert comp.type == "IC"
+        comp.type = "Other"
+        assert comp.type == "Other"
