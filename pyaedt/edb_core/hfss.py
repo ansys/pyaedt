6c162bac0f1a5d2e5d8a84e71ec73b495293b8eb
hzhou <ring630@gmail.com>
2022-03-29 08:58:26 +0200

    new

--- pyaedt/edb_core/hfss.py
+++ pyaedt/edb_core/hfss.py
@@ -466,7 +466,7 @@ class EdbHfss(object):
         pin = comp.pins[pin_number]
         edb_net = pin.pin.GetNet()
         edb_pin = pin.pin
-        res, from_layer, to_layer = edb_pin.GetLayerRange(None, None)
+        _, from_layer, to_layer = edb_pin.GetLayerRange(None, None)
 
         if from_layer == comp.placement_layer:
             edb_layer = from_layer
