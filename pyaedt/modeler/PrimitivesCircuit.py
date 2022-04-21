f06056d98f440d108477519cc9dac08cd4fef31e
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2022-04-21 18:19:39 +0200

    fix id issue on Circuit components (#1072)
    Co-authored-by: maxcapodi78 <Shark78>

--- pyaedt/modeler/PrimitivesCircuit.py
+++ pyaedt/modeler/PrimitivesCircuit.py
@@ -977,10 +977,11 @@ class CircuitComponents(object):
                     o.name = name[0]
                     if len(name) == 2:
                         o.schematic_id = name[1]
+                        objID = int(o.schematic_id)
                     else:
                         o.id = int(name[1])
                         o.schematic_id = name[2]
-                    objID = o.id
+                        objID = o.id
                     self.components[objID] = o
         return len(self.components)
 
