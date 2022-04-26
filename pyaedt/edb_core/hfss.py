c39a2618e6cabff7eb75836cdc68f5c2db5a7bb5
svandenb-dev <simon.vandenbrouck@ansys.com>
2022-04-26 14:30:24 +0200

    hfss adaptive freq fixed

--- pyaedt/edb_core/hfss.py
+++ pyaedt/edb_core/hfss.py
@@ -928,21 +928,21 @@ class EdbHfss(object):
             simsetup_info.SimulationSettings.AdaptiveSettings.AdaptiveFrequencyDataList.Clear()
             simsetup_info.SimulationSettings.AdaptiveSettings.AdaptiveFrequencyDataList.Add(adapt)
         else:
-            list(simsetup_info.SimulationSettings.AdaptiveSettings.AdaptiveFrequencyDataList).clear()
-            list(simsetup_info.SimulationSettings.AdaptiveSettings.AdaptiveFrequencyDataList).append(adapt)
+            simsetup_info.SimulationSettings.AdaptiveSettings.AdaptiveFrequencyDataList = \
+                convert_py_list_to_net_list([adapt])
         simsetup_info.SimulationSettings.InitialMeshSettings.LambdaRefine = simulation_setup.do_lambda_refinement
         simsetup_info.SimulationSettings.InitialMeshSettings.UseDefaultLambda = True
         simsetup_info.SimulationSettings.AdaptiveSettings.MaxRefinePerPass = 30
-        simsetup_info.SimulationSettings.AdaptiveSettings.MinPasses = simulation_setup.min_num_passes  # 1
+        simsetup_info.SimulationSettings.AdaptiveSettings.MinPasses = simulation_setup.min_num_passes
         simsetup_info.SimulationSettings.AdaptiveSettings.MinConvergedPasses = 1
         simsetup_info.SimulationSettings.HFSSSolverSettings.OrderBasis = (
             simulation_setup.basis_order
-        )  # -1  # e.g. mixed
+        )
         simsetup_info.SimulationSettings.HFSSSolverSettings.UseHFSSIterativeSolver = False
         simsetup_info.SimulationSettings.DefeatureSettings.UseDefeature = False  # set True when using defeature ratio
         simsetup_info.SimulationSettings.DefeatureSettings.UseDefeatureAbsLength = (
             simulation_setup.defeature_layout
-        )  # True
+        )
         simsetup_info.SimulationSettings.DefeatureSettings.DefeatureAbsLength = simulation_setup.defeature_abs_length
 
         try:
