65df5be9d1cc97ec6adb8ad63988e5c2a39ba8a6
jeremich41 <jeremy.michel@ansys.com>
2022-05-03 12:10:16 +0200

    add in

--- pyaedt/modules/Boundary.py
+++ pyaedt/modules/Boundary.py
@@ -620,7 +620,9 @@ class FieldSetup(BoundaryCommon, object):
     >>> from pyaedt import Hfss
     >>> hfss = Hfss()
     >>> sphere1 = hfss.insert_infinite_sphere()
-    >>> sphere1.props["ThetaStart"] = 45
+    >>> sphere1.props["ThetaStart"] = "-90deg"
+    >>> sphere1.props["ThetaStop"] = "90deg"
+    >>> sphere1.props["ThetaStep"] = "2deg"
     >>> sphere1.delete()
     """
 
