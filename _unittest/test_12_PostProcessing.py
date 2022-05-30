89b402f6352128fd603e5ee6dc1c31b3f13ec287
Giulia Malinverno <giulia.malinverno@ansys.com>
2022-05-30 12:08:00 +0200

    fix test post process

--- _unittest/test_12_PostProcessing.py
+++ _unittest/test_12_PostProcessing.py
@@ -735,7 +735,7 @@ class TestClass(BasisTest, object):
         assert new_report.create()
         new_report = self.q3dtest.post.reports_by_category.dc_fields("Mag_VolumeJdc", polyline="Polyline1")
         assert new_report.create()
-        assert len(self.q3dtest.post.plots) == 4
+        assert len(self.q3dtest.post.plots) == 6
 
     def test_57_test_export_q2d_results(self):
         self.q2dtest.analyze_nominal()
