671158282838841f1ff117a88d0e20b1284ad28a
jeremich41 <98900773+jeremich41@users.noreply.github.com>
2022-06-13 09:40:42 +0200

    IronPython Problem + Heritage

--- pyaedt/modeler/stackup_3d.py
+++ pyaedt/modeler/stackup_3d.py
@@ -6,6 +6,7 @@ from pyaedt.generic.general_methods import generate_unique_name
 from pyaedt.generic.general_methods import pyaedt_function_handler
 from pyaedt.modules.MaterialLib import Material
 from pyaedt.generic.general_methods import is_ironpython
+
 if not is_ironpython:
     try:
         import joblib
