813cec6a4b39ef72ec2df14e020d0800ffe1b558
maxcapodi78 <Shark78>
2022-06-20 16:58:29 +0200

    Updated CICD to 22R2

--- _unittest_ironpython/run_unittests_batchmode.cmd
+++ _unittest_ironpython/run_unittests_batchmode.cmd
@@ -2,4 +2,4 @@ set ANSYSEM_FEATURE_SF6694_NON_GRAPHICAL_COMMAND_EXECUTION_ENABLE=1
 set ANSYSEM_FEATURE_SF159726_SCRIPTOBJECT_ENABLE=1
 set RUN_UNITTESTS_ARGS=%*
 set ANSYSEM_INSTALL_DIR=%ANSYSEM_ROOT221%
-"%ANSYSEM_ROOT221%\ansysedt.exe" -ng -RunScriptAndExit "_unittest_ironpython\run_unittests.py"
+"%ANSYSEM_ROOT222%\ansysedt.exe" -ng -RunScriptAndExit "_unittest_ironpython\run_unittests.py"
