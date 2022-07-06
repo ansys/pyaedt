5f43839df4c462343e97cc7df50fdf17417ca63c
maxcapodi78 <Shark78>
2022-07-06 12:11:03 +0200

    pythonnet linux fixed UT to support pythonnet 3.0

--- pyaedt/rpc/rpyc_services.py
+++ pyaedt/rpc/rpyc_services.py
@@ -815,7 +815,7 @@ class GlobalService(rpyc.Service):
                 for option in range(beta_options.__len__()):
                     if beta_options[option] not in ng_feature:
                         ng_feature += "," + beta_options[option]
-            command = [aedt_exe, "-grpcrv", str(port), ng_feature]
+            command = [aedt_exe, "-grpcsrv", str(port), ng_feature]
 
 
         print(command)
