3f00f63290bd0bd02cb8c2e11e0e4b18d464c94c
maxcapodi78 <Shark78>
2022-07-18 15:51:21 +0200

    Fixed Circuit Issue on linux

--- pyaedt/application/aedt_objects.py
+++ pyaedt/application/aedt_objects.py
@@ -16,8 +16,6 @@ class AedtObjects(object):
             else:
                 self.oproject = self._odesktop.GetActiveProject()
                 self.odesign = self.oproject.GetActiveDesign()
-        print(self.oproject.GetName())
-        print(self.odesign.GetName())
         self._oboundary = None
         self._oimport_export = None
         self._ooptimetrics = None
