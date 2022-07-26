88b15e569de69b5c2f49f9189d2b109bf76e5f79
maxcapodi78 <Shark78>
2022-07-26 14:38:57 +0200

    fixed black style issue

--- _unittest/conftest.py
+++ _unittest/conftest.py
@@ -10,7 +10,7 @@ This module contains the configuration and fixture for the pytest-based unit tes
 The default configuration can be changed by placing a file called local_config.json in the same
 directory as this module. An example of the contents of local_config.json
 {
-  "desktopVersion": "2022.1",
+  "desktopVersion": "2022.2",
   "NonGraphical": false,
   "NewThread": false,
   "test_desktops": true
