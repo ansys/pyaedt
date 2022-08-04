fe71a5df548429e2709c5b9649256870dace0a82
MaxJPRey <maxime.rey@ansys.com>
2022-08-04 17:21:52 +0200

    Remove OrderedDict.

--- pyaedt/modules/Material.py
+++ pyaedt/modules/Material.py
@@ -1778,22 +1778,9 @@ class Material(CommonMaterial, object):
         self._props["core_loss_curves"]["Frequency"] = "{}Hz".format(frequency)
         self._props["core_loss_curves"]["Thickness"] = thickness
         self._props["core_loss_curves"]["IsTemperatureDependent"] = False
+
         self._props["core_loss_curves"]["BPCoordinates"] = OrderedDict({})
-
         self._props["core_loss_curves"]["BPCoordinates"]["Point"] = []
-        # self._props["AttachedData"] = OrderedDict(
-        #     {
-        #         "MatAppearanceData": OrderedDict(
-        #             {
-        #                 "property_data": "appearance_data",
-        #                 "Red": self._material_appearance[0],
-        #                 "Green": self._material_appearance[1],
-        #                 "Blue": self._material_appearance[2],
-        #             }
-        #         )
-        #     }
-        # )
-
         for points in point_list:
             self._props["core_loss_curves"]["BPCoordinates"]["Point"].append(points)
         return self.update()
