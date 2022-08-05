cbf5eab7927efe15ecde1475bdebe04692509c4f
MaxJPRey <maxime.rey@ansys.com>
2022-08-05 16:30:22 +0200

    Test that an exception is raised when a layer does not exist.

--- _unittest/test_16_3d_stackup.py
+++ _unittest/test_16_3d_stackup.py
@@ -59,6 +59,10 @@ class TestClass(BasisTest, object):
     def test_03_padstackline(self):
         p1 = self.st.add_padstack("Massimo", material="aluminum")
         p1.plating_ratio = 0.7
+        try:
+            p1.set_start_layer("non_existing_layer")
+        except ValueError:
+            assert True
         assert p1.set_start_layer("lay1")
         assert p1.set_stop_layer("top")
         p1.set_all_pad_value(1)
