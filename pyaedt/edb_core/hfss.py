0be1ea9640b4d056fc1cf65fe3bb64bb01f761b9
Hui Zhou <ring630@gmail.com>
2022-08-22 15:11:43 +0200

    Update pyaedt/edb_core/hfss.py
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- pyaedt/edb_core/hfss.py
+++ pyaedt/edb_core/hfss.py
@@ -85,8 +85,8 @@ class EdbHfss(object):
             Primitive ID.
         point_on_edge : list
             Coordinate of the point to define the edge terminal.
-        The point must be on the target edge but not on the two
-        ends of the edge.
+            The point must be on the target edge but not on the two
+            ends of the edge.
         terminal_name : str, optional
             Name o the terminal.
         is_ref : bool, optional
