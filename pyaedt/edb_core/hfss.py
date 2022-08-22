b5b5590f4e6451898e602f05e59bf19449217c66
Hui Zhou <ring630@gmail.com>
2022-08-22 15:50:04 +0200

    Update pyaedt/edb_core/hfss.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/edb_core/hfss.py
+++ pyaedt/edb_core/hfss.py
@@ -90,7 +90,7 @@ class EdbHfss(object):
         terminal_name : str, optional
             Name o the terminal.
         is_ref : bool, optional
-            Whether it is a reference terminal.
+            Whether it is a reference terminal. The default is ``False``.
         Returns
         -------
         Edb.Cell.Terminal.EdgeTerminal
