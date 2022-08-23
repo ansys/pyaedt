a2ed80b6474be5d174897cb095c79c4594572342
gmalinve <103059376+gmalinve@users.noreply.github.com>
2022-08-23 12:24:19 +0200

    Update pyaedt/q3d.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/q3d.py
+++ pyaedt/q3d.py
@@ -518,7 +518,7 @@ class QExtractor(FieldAnalysis3D, object):
                                 self.logger.error("Spice can't be a matrix type if problem type is AC RL or DC RL.")
                                 return False
                 else:
-                    self.logger.error("Invalid problem type. Available values are C, AC RL, DC RL")
+                    self.logger.error("Invalid problem type. Possible values are C, AC RL, and DC RL.")
                     return False
 
         if variations is None:
