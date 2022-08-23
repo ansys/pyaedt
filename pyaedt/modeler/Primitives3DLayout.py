edebd291827cd8a551c60f523b5706d5fb5e3947
Hui Zhou <ring630@gmail.com>
2022-08-23 11:30:54 +0200

    Update pyaedt/modeler/Primitives3DLayout.py
    Co-authored-by: Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>

--- pyaedt/modeler/Primitives3DLayout.py
+++ pyaedt/modeler/Primitives3DLayout.py
@@ -904,10 +904,12 @@ class Primitives3DLayout(object):
 
         >>> oEditor.CreateLine
         """
-        warnings.warn(
-            "`netname` is deprecated. Use `net_name` instead.",
-            DeprecationWarning,
-        )
+        if "netname" in kwargs:
+            warnings.warn(
+                "`netname` is deprecated. Use `net_name` instead.",
+                DeprecationWarning,
+            )
+            net_name=kwargs["netname"]
         if not name:
             name = _uname()
         else:
