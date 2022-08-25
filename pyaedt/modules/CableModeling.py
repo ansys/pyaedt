131d09118070e706472f7fa120d20f638fc908de
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-08-25 09:37:09 +0200

    Update pyaedt/modules/CableModeling.py

--- pyaedt/modules/CableModeling.py
+++ pyaedt/modules/CableModeling.py
@@ -12,7 +12,7 @@ class Cable:
     Parameters
     ----------
     app : :class:`pyaedt.hfss.Hfss`
-    json_file_name : str , optional
+    json_file_name : str, optional
         Path of the json file where the cable information are saved.
     working_dir : str, optional
         Working directory.
