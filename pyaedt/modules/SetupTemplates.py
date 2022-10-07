44cb1f6a7961bfa8c4eae6ac439f8e17c5912485
hzhou <ring630@gmail.com>
2022-10-07 15:01:41 +0200

    minior change

--- pyaedt/modules/SetupTemplates.py
+++ pyaedt/modules/SetupTemplates.py
@@ -1,5 +1,6 @@
 import os.path
 from collections import OrderedDict
+import json
 
 from pyaedt.generic.DataHandlers import _dict2arg
 from pyaedt.generic.general_methods import pyaedt_function_handler
@@ -2172,3 +2173,37 @@ class SetupProps(OrderedDict):
 
     def _setitem_without_update(self, key, value):
         OrderedDict.__setitem__(self, key, value)
+
+    def export_properties_to_json(self, fpath):
+        """Export all setup properties into a json file.
+
+        Parameters
+        ----------
+        fpath : str
+            File path of the json file.
+        """
+        if not fpath.endswith(".json"):
+            fpath = fpath + ".json"
+        with open(fpath, "w", encoding="utf-8") as f:
+            f.write(json.dumps(self, indent=4, ensure_ascii=False))
+
+    def import_properties_from_json(self, fpath):
+        """Import setup properties from a json file.
+
+        Parameters
+        ----------
+        fpath : str
+            File path of the json file.
+        """
+        def set_props(target, source):
+            for k, v in source.items():
+                if k not in target:
+                    raise Exception("{} is not a valid property name.".format(k))
+                if not isinstance(v, dict):
+                    target[k] = v
+                else:
+                    set_props(target[k], v)
+        with open(fpath, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            set_props(self, data)
+
