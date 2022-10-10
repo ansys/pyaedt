8b483b8fe951486768a273f1ec2e19803fb4fcc5
Samuel Lopez <samuel.lopez@ansys.com>
2022-10-10 10:21:50 +0200

    Add Transient to AMI example

--- examples/03-Circuit/Circuit_AMI.py
+++ examples/03-Circuit/Circuit_AMI.py
@@ -58,7 +58,7 @@ NewThread = True
 # and starts the specified version in the specified mode.
 
 settings.enable_pandas_output = True
-cir = Circuit(projectname=os.path.join(project_path), non_graphical=non_graphical,
+cir = Circuit(projectname=os.path.join(project_path), non_graphical=True,
               specified_version=desktopVersion, new_desktop_session=NewThread)
 
 ###############################################################################
@@ -66,14 +66,14 @@ cir = Circuit(projectname=os.path.join(project_path), non_graphical=non_graphica
 # ~~~~~~~~~~~~~~~~~~~~~
 # Solve the transient setup.
 
-cir.analyze_setup("AMIAnalysis")
+cir.analyze_all()
 
 ###############################################################################
-# Create AMI report
+# Get AMI report
 # ~~~~~~~~~~~~~
-# Create a report that plots solution data.
+# Get AMI report data.
 
-plot_name = "WaveAfterSource<b_output4_42.int_ami_tx>"
+plot_name = "WaveAfterProbe<b_input_43.int_ami_rx>"
 cir.solution_type = "NexximAMI"
 original_data = cir.post.get_solution_data(expressions=plot_name,
                                            setup_sweep_name="AMIAnalysis", domain="Time",
@@ -89,20 +89,20 @@ print(original_data_value)
 fig = original_data.plot()
 
 ###############################################################################
-# Sample WaveAfterSource waveform using receiver clock
+# Sample WaveAfterProbe waveform using receiver clock
 # ~~~~~~~~~~~~~
 # Extract waveform at specific clock time plus half unit interval.
 
 probe_name = "b_input_43"
 source_name = "b_output4_42"
-plot_type = "WaveAfterSource"
+plot_type = "WaveAfterProbe"
 setup_name = "AMIAnalysis"
 ignore_bits = 100
 unit_interval = 0.1e-9
 sample_waveform = cir.post.sample_ami_waveform(setupname=setup_name, probe_name=probe_name, source_name=source_name,
                                                variation_list_w_value=cir.available_variations.nominal,
                                                unit_interval=unit_interval, ignore_bits=ignore_bits,
-                                               plot_type=None)
+                                               plot_type=plot_type)
 
 ###############################################################################
 # Plot waveform and samples
@@ -129,21 +129,21 @@ for time in original_data_value[plot_name][start_index_original_data:].index:
         break
 for time in sample_waveform[0].time:
     if tstart <= time:
-        start_index_waveform = sample_waveform[0].time.index[sample_waveform[1].time == time].tolist()[0]
+        start_index_waveform = sample_waveform[0].time.index[sample_waveform[0].time == time].tolist()[0]
         break
 for time in sample_waveform[0].time:
     if time >= tstop:
-        stop_index_waveform = sample_waveform[0].time.index[sample_waveform[1].time == time].tolist()[0]
+        stop_index_waveform = sample_waveform[0].time.index[sample_waveform[0].time == time].tolist()[0]
         break
 
 original_data_zoom = original_data_value[start_index_original_data:stop_index_original_data]
-sampled_data_zoom = sample_waveform[1].voltage[start_index_waveform:stop_index_waveform] * scale_data
-sampled_time_zoom = sample_waveform[1].time[start_index_waveform:stop_index_waveform] * scale_time
+sampled_data_zoom = sample_waveform[0].voltage[start_index_waveform:stop_index_waveform] * scale_data
+sampled_time_zoom = sample_waveform[0].time[start_index_waveform:stop_index_waveform] * scale_time
 
 fig, ax = plt.subplots()
 ax.plot(sampled_time_zoom, sampled_data_zoom, "r*")
 ax.plot(np.array(list(original_data_zoom.index.values)), original_data_zoom.values, color='blue')
-ax.set_title('WaveAfterSource')
+ax.set_title('WaveAfterProbe')
 ax.set_xlabel(original_data.units_sweeps["Time"])
 ax.set_ylabel(original_data.units_data[plot_name])
 plt.show()
@@ -154,17 +154,10 @@ plt.show()
 # Create the plot from a start time to stop time in seconds.
 
 fig, ax2 = plt.subplots()
-ax2.plot(sample_waveform[1].time, sample_waveform[1].voltage, "r*")
-ax2.set_title('Slicer Scatter: WaveAfterSource')
+ax2.plot(sample_waveform[0].time, sample_waveform[0].voltage, "r*")
+ax2.set_title('Slicer Scatter: WaveAfterProbe')
 ax2.set_xlabel("s")
 ax2.set_ylabel("V")
-plt.show()
-
-fig, ax3 = plt.subplots()
-ax3.set_title('Slicer Scatter: WaveAfterChannel')
-ax3.plot(sample_waveform[2].time, sample_waveform[2].voltage, "r*")
-ax3.set_xlabel("s")
-ax3.set_ylabel("V")
 plt.show()
 
 ###############################################################################
@@ -173,17 +166,107 @@ plt.show()
 # Create the plot from a start time to stop time in seconds.
 
 fig, ax4 = plt.subplots()
-ax4.set_title('Slicer Histogram: WaveAfterSource')
-ax4.hist(sample_waveform[1].voltage, orientation='horizontal')
+ax4.set_title('Slicer Histogram: WaveAfterProbe')
+ax4.hist(sample_waveform[0].voltage, orientation='horizontal')
 ax4.set_ylabel("V")
 ax4.grid()
 plt.show()
 
-fig, ax5 = plt.subplots()
-ax5.set_title('Slicer Histogram: WaveAfterChannel')
-ax5.hist(sample_waveform[2].voltage, orientation='horizontal')
-ax5.set_ylabel("V")
-ax5.grid()
+###############################################################################
+# Get Transient report
+# ~~~~~~~~~~~~~
+# Get Transient report data.
+
+plot_name = "V(b_input_43.int_ami_rx.eye_probe.out)"
+cir.solution_type = "NexximTransient"
+original_data = cir.post.get_solution_data(expressions=plot_name,
+                                           setup_sweep_name="NexximTransient", domain="Time",
+                                           variations=cir.available_variations.nominal)
+
+###############################################################################
+# Sample waveform using a user-defined clock
+# ~~~~~~~~~~~~~
+# Extract waveform at specific clock time plus half unit interval.
+
+original_data.enable_pandas_output = False
+original_data_value = original_data.data_real()
+original_data_sweep = original_data.primary_sweep_values
+waveform_unit = original_data.units_data[plot_name]
+waveform_sweep_unit = original_data.units_sweeps["Time"]
+tics = np.arange(20e-9, 100e-9, 1e-10, dtype=float)
+
+sample_waveform = cir.post.sample_waveform(
+                waveform_data=original_data_value,
+                waveform_sweep=original_data_sweep,
+                waveform_unit=waveform_unit,
+                waveform_sweep_unit=waveform_sweep_unit,
+                unit_interval=unit_interval,
+                clock_tics=tics,
+                pandas_enabled=False,
+            )
+
+###############################################################################
+# Plot waveform and samples
+# ~~~~~~~~~
+# Create the plot from a start time to stop time in seconds.
+
+tstop = 40.0e-9
+tstart = 25.0e-9
+scale_time = constants.unit_converter(1, unit_system="Time", input_units="s",
+                                 output_units=waveform_sweep_unit)
+scale_data = constants.unit_converter(1, unit_system="Voltage", input_units="V",
+                                 output_units=waveform_unit)
+
+tstop_ns = scale_time * tstop
+tstart_ns = scale_time * tstart
+
+for time in original_data_sweep:
+    if tstart_ns <= time:
+        start_index_original_data = original_data_sweep.index(time)
+        break
+for time in original_data_sweep[start_index_original_data:]:
+    if time >= tstop_ns:
+        stop_index_original_data = original_data_sweep.index(time)
+        break
+cont = 0
+for frame in sample_waveform:
+    if tstart <= frame[0]:
+        start_index_waveform = cont
+        break
+    cont += 1
+for frame in sample_waveform[start_index_waveform:]:
+    if frame[0] >= tstop:
+        stop_index_waveform = cont
+        break
+    cont += 1
+
+original_data_zoom = original_data_value[start_index_original_data:stop_index_original_data]
+original_sweep_zoom = original_data_sweep[start_index_original_data:stop_index_original_data]
+original_data_zoom_array = np.array(list(map(list, zip(original_sweep_zoom, original_data_zoom))))
+original_data_zoom_array[:, 0] *= 1
+sampled_data_zoom_array = np.array(sample_waveform[start_index_waveform:stop_index_waveform])
+sampled_data_zoom_array[:, 0] *= scale_time
+sampled_data_zoom_array[:, 1] *= scale_data
+
+fig, ax = plt.subplots()
+ax.plot(sampled_data_zoom_array[:, 0], sampled_data_zoom_array[:, 1], "r*")
+ax.plot(original_sweep_zoom, original_data_zoom_array[:, 1], color='blue')
+ax.set_title(plot_name)
+ax.set_xlabel(waveform_sweep_unit)
+ax.set_ylabel(waveform_unit)
+plt.show()
+
+###############################################################################
+# Plot Slicer Scatter
+# ~~~~~~~~~
+# Create the plot from a start time to stop time in seconds.
+
+sample_waveform_array = np.array(sample_waveform)
+fig, ax2 = plt.subplots()
+ax2.plot(sample_waveform_array[:, 0], sample_waveform_array[:, 1], "r*")
+ax2.set_title('Slicer Scatter: ' + plot_name)
+ax2.set_xlabel("s")
+ax2.set_ylabel("V")
 plt.show()
 
 ###############################################################################
