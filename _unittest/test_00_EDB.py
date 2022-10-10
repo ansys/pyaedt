ba8be8a67e91fdf7ac1ef1807fb8191b8392bc4d
hzhou <ring630@gmail.com>
2022-10-10 16:34:14 +0200

    minior change

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -2059,9 +2059,8 @@ if not config["skip_edb"]:
             assert isinstance(edbapp.stackup.stackup_layers, dict)
             assert isinstance(edbapp.stackup.non_stackup_layers, dict)
             assert not edbapp.stackup["Outline"].is_stackup_layer
-            top = edbapp.stackup["TOP"]
-            assert top.conducvitiy
-            assert top.permittivity
+            assert edbapp.stackup["TOP"].conductivity
+            assert edbapp.stackup["UNNAMED_002"].permittivity
             assert edbapp.stackup.add_layer("new_layer")
             new_layer = edbapp.stackup["new_layer"]
             assert new_layer.is_stackup_layer
