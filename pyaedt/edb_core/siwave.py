4cad2bf89fb5f57b5075374f605d730beb3ea4a9
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-10-11 20:23:04 +0200

    Update pyaedt/edb_core/siwave.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/edb_core/siwave.py
+++ pyaedt/edb_core/siwave.py
@@ -1,6 +1,6 @@
 """
-This module contains these classes: `CircuitPort`, `CurrentSource`, `EdbSiwave`,
-`PinGroup`, `ResistorSource`, `Source`, `SourceType`, and `VoltageSource`.
+This module contains these classes: ``CircuitPort``, ``CurrentSource``, ``EdbSiwave``,
+``PinGroup``, ``ResistorSource``, ``Source``, ``SourceType``, and ``VoltageSource``.
 """
 import os
 import time
