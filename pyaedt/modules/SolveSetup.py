e069d0029bda6e47fe41f0ea8ca879837a132d4e
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-10-24 17:40:32 +0200

    Improve the sweeps. (#1856)
    * Use the setup parameter for the sweep.
    
    * Add deprecation warning.
    
    * Fix tests.
    
    * Fix setup name.
    
    * Fix kwarg for IronPython.
    
    * Update pyaedt/modules/SetupTemplates.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Update pyaedt/modules/SetupTemplates.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Update pyaedt/modules/SetupTemplates.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Update pyaedt/modules/SetupTemplates.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/modules/SolveSetup.py
+++ pyaedt/modules/SolveSetup.py
@@ -70,13 +70,13 @@ class CommonSetup(PropsManager, object):
                             app.pop("MoveBackwards", None)
                             for el in app:
                                 if isinstance(app[el], (OrderedDict, dict)):
-                                    self.sweeps.append(SweepHFSS(self, self.name, el, props=app[el]))
+                                    self.sweeps.append(SweepHFSS(self, el, props=app[el]))
 
                         else:
                             app = setup_data["Sweeps"]
                             for el in app:
                                 if isinstance(app[el], (OrderedDict, dict)):
-                                    self.sweeps.append(SweepMatrix(self, self.name, el, props=app[el]))
+                                    self.sweeps.append(SweepMatrix(self, el, props=app[el]))
                         setup_data.pop("Sweeps", None)
                     self.props = SetupProps(self, OrderedDict(setup_data))
             except:
@@ -489,9 +489,9 @@ class Setup(CommonSetup):
             self._app.logger.warning("This method only applies to HFSS and Q3D. Use add_eddy_current_sweep method.")
             return False
         if self.setuptype <= 4:
-            sweep_n = SweepHFSS(self, self.name, sweepname, sweeptype)
+            sweep_n = SweepHFSS(self, sweepname=sweepname, sweeptype=sweeptype)
         elif self.setuptype in [14, 30, 31]:
-            sweep_n = SweepMatrix(self, self.name, sweepname, sweeptype)
+            sweep_n = SweepMatrix(self, sweepname=sweepname, sweeptype=sweeptype)
         else:
             self._app.logger.warning("This method only applies to HFSS, Q2D and Q3D.")
             return False
@@ -1496,7 +1496,7 @@ class Setup3DLayout(CommonSetup):
         """
         if not sweepname:
             sweepname = generate_unique_name("Sweep")
-        sweep_n = SweepHFSS3DLayout(self, self.name, sweepname, sweeptype)
+        sweep_n = SweepHFSS3DLayout(self, sweepname, sweeptype)
         if sweep_n.create():
             self.sweeps.append(sweep_n)
             return sweep_n
