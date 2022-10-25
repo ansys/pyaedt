063ca8b59aa31d3c340f45f1a005131c2280b2d5
MaxJPRey <maxime.rey@ansys.com>
2022-10-25 21:22:49 +0200

    Improve test for the heat sink in Icepak.

--- _unittest/test_98_Icepak.py
+++ _unittest/test_98_Icepak.py
@@ -452,7 +452,17 @@ class TestClass(BasisTest, object):
 
     def test_36_create_heat_sink(self):
         self.aedtapp.insert_design("HS")
-        assert self.aedtapp.create_parametric_fin_heat_sink()
+        assert self.aedtapp.create_parametric_fin_heat_sink(
+            draftangle=1.5,
+            patternangle=8,
+            numcolumn_perside=3,
+            vertical_separation=5.5,
+            matname="Copper",
+            center=[10, 0, 0],
+            plane_enum=self.aedtapp.PLANE.XY,
+            rotation=45,
+            tolerance=0.005,
+        )
 
     def test_37_check_bounding_box(self):
         self.aedtapp.insert_design("Bbox")
