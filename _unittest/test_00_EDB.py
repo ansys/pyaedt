102fd14471bbd85f77d38c817dc8a1e1c2ac6282
Hui Zhou <ring630@gmail.com>
2022-10-28 15:14:22 +0200

    add methods for flexibility (#1891)
    * exclude reference terminals
    
    * exclude reference terminals
    
    * exclude reference terminals
    
    * exclude reference terminals
    
    * exclude reference terminals
    
    * exclude reference terminals
    
    * Update pyaedt/edb_core/siwave.py
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    
    * Update pyaedt/edb_core/siwave.py
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -2236,3 +2236,10 @@ if not config["skip_edb"]:
             comp.type = "Inductor"
             comp.value = 10  # This command set the model back to ideal RLC
             assert comp.type == "Inductor" and comp.value == 10 and float(comp.ind_value) == 10
+
+            pg_name, _ = edbapp.core_siwave.create_pin_group("U3A1", 2)
+            assert edbapp.core_siwave.create_pin_group("U3A1", [5, 34, 35], "pos")
+            assert "pos" in edbapp.core_siwave.pin_groups
+            edbapp.core_siwave.create_pin_group_on_net("U3A1", "GND", "gnd")
+            edbapp.core_siwave.create_current_source_on_pin_group("pos", "gnd")
+            edbapp.core_siwave.create_voltage_source_on_pin_group(pg_name, "gnd")
