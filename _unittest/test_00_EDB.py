d2631ab03d47d6c8f35074f361b999ae4e41ad0f
svandenb-dev <simon.vandenbrouck@ansys.com>
2022-11-04 11:05:13 +0100

    coax ports extent and void removing with clipping

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -2284,10 +2284,10 @@ if not config["skip_edb"]:
                 return 0
 
             target_path = os.path.join(local_path, "example_models", test_subfolder, "Galileo.aedb")
-            edbapp = Edb(target_path, edbversion=desktop_version)
-            # self.local_scratch.copyfolder(target_path, out_edb)
+            out_edb = os.path.join(self.local_scratch.path, "Galileo_test.aedb")
+            self.local_scratch.copyfolder(target_path, out_edb)
             json_path = os.path.join(local_path, "example_models", test_subfolder, "test_mat.json")
-            # edbapp = Edb(out_edb, edbversion=desktop_version)
+            edbapp = Edb(out_edb, edbversion=desktop_version)
             edbapp.stackup._import_layer_stackup(json_path)
             edbapp.save_edb()
             delta = 1e-6
