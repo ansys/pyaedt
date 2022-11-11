ab27c92203758bf5a4ea588966493d0838ee92a6
svandenb-dev <simon.vandenbrouck@ansys.com>
2022-11-11 13:15:07 +0100

    unit test fixed

--- pyaedt/edb.py
+++ pyaedt/edb.py
@@ -1073,9 +1073,12 @@ class Edb(object):
             _success = db2.Save()
             _dbCells = convert_py_list_to_net_list(_dbCells)
             db2.CopyCells(_dbCells)  # Copies cutout cell/design to db2 project
-            for net in list(list(db2.CircuitCells)[0].GetLayout().Nets):
-                if not net.GetName() in included_nets_list:
-                    net.Delete()
+            try:
+                for net in list(list(db2.CircuitCells)[0].GetLayout().Nets):
+                    if not net.GetName() in included_nets_list:
+                        net.Delete()
+            except:
+                pass
             _success = db2.Save()
             for c in list(self.db.TopCircuitCells):
                 if c.GetName() == _cutout.GetName():
