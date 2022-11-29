e49cd2481960a06532ea234c71aff2b993977009
Hui <ring630@gmail.com>
2022-11-29 10:42:32 +0100

    draft

--- pyaedt/edb_core/simulation_setup.py
+++ pyaedt/edb_core/simulation_setup.py
@@ -1,5 +1,5 @@
 
-from pyaedt.edb_core.edb_data.simulation_setup_data import Hfss3dlSimulationSetup
+from pyaedt.edb_core.edb_data.simulation_setup_data import HfssSimulationSetup
 
 
 class SimulationSetups(object):
@@ -9,7 +9,7 @@ class SimulationSetups(object):
 
     @property
     def setups(self):
-        return {i.GetName(): Hfss3dlSimulationSetup(self._edb, i.GetName, i) for i in list(self._edb._active_layout.GetCell().SimulationSetups)}
+        return {i.GetName(): HfssSimulationSetup(self._edb, i.GetName, i) for i in list(self._edb._active_layout.GetCell().SimulationSetups)}
 
     @property
     def hfss_setups(self):
@@ -18,4 +18,4 @@ class SimulationSetups(object):
     def create_hfss_simulation_setup(self, name=None):
         if name in self.setups:
             return False
-        return Hfss3dlSimulationSetup(self._edb, name)
+        return HfssSimulationSetup(self._edb, name)
