a28b3bc591ed1ce20b7d00d7303ac2179d8e4e99
svandenb-dev <simon.vandenbrouck@ansys.com>
2022-12-07 11:01:39 +0100

    component with via wrong position

--- pyaedt/edb_core/IPC2581/ecad/cad_data/padstack_def/padstack_instance.py
+++ pyaedt/edb_core/IPC2581/ecad/cad_data/padstack_def/padstack_instance.py
@@ -18,27 +18,27 @@ class PadstackInstance(object):
 
     def write_xml(self, net_root):  # pragma no cover
         if self.isvia:
-            if not self.refdes:
-                net_root.set("testPoint", "false")
-                net_root.set("plate", "true")
-                net_root.set("padUsage", "VIA")
-                non_standard_attribute = ET.SubElement(net_root, "NonstandardAttribute")
-                non_standard_attribute.set("name", "PADSTACK_USAGE")
-                non_standard_attribute.set("value", "Through")
-                non_standard_attribute.set("type", "STRING")
-                pad = ET.SubElement(net_root, "Pad")
-                pad.set("padstackDefRef", self.padstack_def)
-                xform = ET.SubElement(pad, "Xform")
-                xform.set("rotation", "0")
-                xform.set("mirror", "false")
-                location = ET.SubElement(pad, "Location")
-                location.set("x", str(self.x))
-                location.set("y", str(self.y))
-                primitive_ref = ET.SubElement(pad, "StandardPrimitiveRef")
-                primitive_ref.set("id", self.standard_primimtive_ref)
-            else:
-                # must add component with via
-                pass
+            net_root.set("testPoint", "false")
+            net_root.set("plate", "true")
+            net_root.set("padUsage", "VIA")
+            non_standard_attribute = ET.SubElement(net_root, "NonstandardAttribute")
+            non_standard_attribute.set("name", "PADSTACK_USAGE")
+            non_standard_attribute.set("value", "Through")
+            non_standard_attribute.set("type", "STRING")
+            pad = ET.SubElement(net_root, "Pad")
+            pad.set("padstackDefRef", self.padstack_def)
+            xform = ET.SubElement(pad, "Xform")
+            xform.set("rotation", "0")
+            xform.set("mirror", "false")
+            location = ET.SubElement(pad, "Location")
+            location.set("x", str(self.x))
+            location.set("y", str(self.y))
+            primitive_ref = ET.SubElement(pad, "StandardPrimitiveRef")
+            primitive_ref.set("id", self.standard_primimtive_ref)
+            if self.refdes:
+                pin_ref = ET.SubElement(pad, "PinRef")
+                pin_ref.set("pin", self.pin)
+                pin_ref.set("componentRef", self.refdes)
         else:
             net_root.set("testPoint", "false")
             net_root.set("plate", "true")
