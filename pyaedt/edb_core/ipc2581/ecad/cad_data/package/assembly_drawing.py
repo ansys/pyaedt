d568713bfe37d99810bb3042d104597bdba35cca
svandenb-dev <74993647+svandenb-dev@users.noreply.github.com>
2022-12-12 08:22:42 +0100

    Update pyaedt/edb_core/ipc2581/ecad/cad_data/package/assembly_drawing.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/edb_core/ipc2581/ecad/cad_data/package/assembly_drawing.py
+++ pyaedt/edb_core/ipc2581/ecad/cad_data/package/assembly_drawing.py
@@ -4,7 +4,7 @@ from pyaedt.edb_core.ipc2581.ecad.cad_data.primitives.polygon import Polygon
 
 
 class AssemblyDrawing(object):
-    """Class describing ipc2581 assembly drawing."""
+    """Class describing an IPC2581 assembly drawing."""
 
     def __init__(self, ipc):
         self._ipc = ipc
