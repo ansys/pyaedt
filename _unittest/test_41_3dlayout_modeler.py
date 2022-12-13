6fb0ae3eb6f6752dfdf416512c4d4978210eb786
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2022-12-13 19:16:19 +0100

    Support for TECH files in GDS and DXF imports in HFSS 3D Layout (#2131)
    * Create new release v0.6.0. This release will be the milestone for AEDT 2023R1
    
    * Updated to v0.6.1
    
    * Updated to v0.6.2
    
    * Updated to v0.6.3
    
    * Updated to v0.6.4
    
    * Updated to v0.6.5
    
    * Update version to 0.6.10
    
    * Update to v0.6.11
    
    * Update to v0.6.12
    
    * Update to v0.6.14
    
    * Pyaedt New Version 0.6.15
    
    * Updated to v0.6.16
    
    * update to v0.6.18
    
    * Updated to v0.6.20
    
    * Updated to v0.6.21
    
    * Update to v0.6.22
    
    * update to v0.6.23
    
    * Updated newer version 0.6.24
    
    * update to v0.6.25
    
    * Update to v0.6.26
    
    * Release version 0.6.27
    
    * updated to v0.6.28
    
    * update to v0.6.30
    
    * Updated to v0.6.31
    
    * Updated to v0.6.32
    
    * Update to v0.6.33
    
    * Update to v0.6.34
    
    * update to v0.6.35
    
    * Update to v0.6.36
    
    * Added support for .tech file when import gds and dxf files.
    
    * Added support for .tech file when import gds and dxf files.
    
    * Added support for .tech file when import gds and dxf files.
    
    * Docstrings improvements
    
    Co-authored-by: maxcapodi78 <Shark78>

--- _unittest/test_41_3dlayout_modeler.py
+++ _unittest/test_41_3dlayout_modeler.py
@@ -614,7 +614,7 @@ class TestClass(BasisTest, object):
 
     def test_37_import_gds(self):
         gds_file = os.path.join(local_path, "example_models", "cad", "GDS", "gds1.gds")
-        control_file = ""
+        control_file = os.path.join(local_path, "example_models", "cad", "GDS", "gds1.tech")
         aedb_file = os.path.join(self.local_scratch.path, "gds_out.aedb")
         assert self.aedtapp.import_gds(gds_file, aedb_path=aedb_file, control_file=control_file)
 
