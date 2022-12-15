a986d9cd6274bfc4bdf5d3c0f09832ff63afa858
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2022-12-15 16:36:13 +0100

    Update pyaedt/modules/AdvancedPostProcessing.py

--- pyaedt/modules/AdvancedPostProcessing.py
+++ pyaedt/modules/AdvancedPostProcessing.py
@@ -608,7 +608,7 @@ class PostProcessor(Post):
            View to export. Options are ``"isometric"``, ``"xy"``, ``"xz"``, and ``"yz"``.
            The default is ``"isometric"``.
         zoom : float, optional
-            Default zoom.
+            Default zoom. Default Value is `2`.
 
         Returns
         -------
