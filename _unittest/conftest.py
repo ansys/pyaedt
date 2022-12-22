9459bb888f4331ee7189f6d68d2fea8b67746680
maxcapodi78 <Shark78>
2022-12-22 11:57:38 +0100

    added object to class

--- _unittest/conftest.py
+++ _unittest/conftest.py
@@ -136,7 +136,10 @@ class BasisTest(object):
 
     def add_app(self, project_name=None, design_name=None, solution_type=None, application=None, subfolder=""):
         if "oDesktop" not in dir(sys.modules["__main__"]):
-            desktop = Desktop(desktop_version, settings.non_graphical, new_thread)
+            try:
+                desktop = Desktop(desktop_version, settings.non_graphical, new_thread)
+            except:
+                desktop = Desktop(desktop_version, settings.non_graphical, new_thread)
             desktop.disable_autosave()
         if project_name:
             example_project = os.path.join(local_path, "example_models", subfolder, project_name + ".aedt")
