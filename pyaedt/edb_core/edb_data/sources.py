e66918b23e8680e97ad26924a561ea38c3b8f13b
ring630 <@gmail.com>
2023-01-05 14:58:23 +0100

    enhancement

--- pyaedt/edb_core/edb_data/sources.py
+++ pyaedt/edb_core/edb_data/sources.py
@@ -281,10 +281,13 @@ class PinGroup(object):
 
     def _create_terminal(self, is_reference=False):
         pg_term = self._edb_pin_group.GetPinGroupTerminal()
+        edb_net = self._edb_pin_group.GetNet()
+        if edb_net.IsNull():
+            edb_net = list(self._edb_pin_group.GetPins())[0].GetNet()
         if pg_term.IsNull():
             return self._pedb.edb.Cell.Terminal.PinGroupTerminal.Create(
                 self._active_layout,
-                self._edb_pin_group.GetNet(),
+                edb_net,
                 self.name,
                 self._edb_pin_group,
                 is_reference,
