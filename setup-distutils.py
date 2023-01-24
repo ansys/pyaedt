733f6e1b45278781e6e83aca8a036982752e71fb
MaxJPRey <maxime.rey@ansys.com>
2023-01-24 18:32:48 +0100

    Add backend.

--- setup-distutils.py
+++ setup-distutils.py
@@ -1,31 +1,31 @@
-from distutils.core import setup
+# from distutils.core import setup
 
-from _setup_common import (
-    name,
-    version,
-    author,
-    maintainer,
-    maintainer_email,
-    description,
-    long_description,
-    packages,
-    data_files,
-    license,
-    classifiers,
-    project_urls,
-)
+# from _setup_common import (
+#     name,
+#     version,
+#     author,
+#     maintainer,
+#     maintainer_email,
+#     description,
+#     long_description,
+#     packages,
+#     data_files,
+#     license,
+#     classifiers,
+#     project_urls,
+# )
 
-setup(
-    name=name,
-    version=version,
-    author=author,
-    maintainer=maintainer,
-    maintainer_email=maintainer_email,
-    description=description,
-    long_description=long_description,
-    packages=packages,
-    data_files=data_files,
-    license=license,
-    classifiers=classifiers,
-    project_urls=project_urls,
-)
+# setup(
+#     name=name,
+#     version=version,
+#     author=author,
+#     maintainer=maintainer,
+#     maintainer_email=maintainer_email,
+#     description=description,
+#     long_description=long_description,
+#     packages=packages,
+#     data_files=data_files,
+#     license=license,
+#     classifiers=classifiers,
+#     project_urls=project_urls,
+# )
