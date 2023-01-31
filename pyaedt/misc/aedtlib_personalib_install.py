add4b55602d3bad5f7329e0ce1b09b2ce53e207a
Karan Bhagat <karan.bhagat@ansys.com>
2023-01-30 22:28:25 -0500

    Toolkit Icons - defining functions in different order

--- pyaedt/misc/aedtlib_personalib_install.py
+++ pyaedt/misc/aedtlib_personalib_install.py
@@ -22,16 +22,79 @@ else:
 pid = 0
 
 
-def write_pretty_xml(root, file_path):
-    """Write the XML in a pretty format."""
-    # If we use the commented code below, then the previously existing lines will have double lines added. We need to
-    # split and ignore the double lines.
-    # xml_str = parseString(ET.tostring(root)).toprettyxml(indent=" " * 4)
-    lines = [line for line in parseString(ET.tostring(root)).toprettyxml(indent=" " * 4).split("\n") if line.strip()]
-    xml_str = "\n".join(lines)
+def main():
+    from pyaedt import Desktop
 
-    with open(file_path, "w") as f:
-        f.write(xml_str)
+    with Desktop(version, True, new_desktop_session=True) as d:
+        desktop = sys.modules["__main__"].oDesktop
+        pers1 = os.path.join(desktop.GetPersonalLibDirectory(), "pyaedt")
+        pid = desktop.GetProcessID()
+        if os.path.exists(pers1):
+            d.logger.info("PersonalLib already mapped")
+        else:
+            os.system('mklink /D "{}" "{}"'.format(pers1, pyaedt_path))
+
+        toolkits = [
+            "2DExtractor",
+            "CircuitDesign",
+            "Emit",
+            "HFSS",
+            "HFSS-IE",
+            "HFSS3DLayoutDesign",
+            "Icepak",
+            "Maxwell2D",
+            "Maxwell3D",
+            "Q3DExtractor",
+            "TwinBuilder",
+            "Mechanical",
+        ]
+
+        for product in toolkits:
+            try:
+                sys_dir = os.path.join(d.syslib, "Toolkits")
+                install_toolkit(sys_dir, product)
+                d.logger.info("Installed toolkit for {} in sys lib".format(product))
+
+            except IOError:
+                pers_dir = os.path.join(d.personallib, "Toolkits")
+                install_toolkit(pers_dir, product)
+                d.logger.info("Installed toolkit for {} in personal lib".format(product))
+    if pid:
+        try:
+            os.kill(pid, 9)
+        except:
+            pass
+
+
+def install_toolkit(toolkit_dir, product):
+    toolkit_rel_lib_dir = os.path.join("Lib", "PyAEDT")
+    lib_dir = os.path.join(toolkit_dir, toolkit_rel_lib_dir)
+    tool_dir = os.path.join(toolkit_dir, product, "PyAEDT")
+    os.makedirs(lib_dir, exist_ok=True)
+    os.makedirs(tool_dir, exist_ok=True)
+    files_to_copy = ["Console", "Run_PyAEDT_Script", "Jupyter"]
+    jupyter_executable = sys.executable.replace("python.exe", "jupyter.exe")
+    ipython_executable = sys.executable.replace("python.exe", "ipython.exe")
+    for file_name in files_to_copy:
+        with open(os.path.join(current_dir, file_name + ".py_build"), "r") as build_file:
+            file_name_dest = file_name.replace("_", " ") + ".py"
+            with open(os.path.join(tool_dir, file_name_dest), "w") as out_file:
+                print("Building to " + os.path.join(tool_dir, file_name_dest))
+                build_file_data = build_file.read()
+                build_file_data = (
+                    build_file_data.replace("##TOOLKIT_REL_LIB_DIR##", toolkit_rel_lib_dir)
+                    .replace("##PYTHON_EXE##", sys.executable)
+                    .replace("##IPYTHON_EXE##", ipython_executable)
+                    .replace("##JUPYTER_EXE##", jupyter_executable)
+                )
+                out_file.write(build_file_data)
+    shutil.copyfile(os.path.join(current_dir, "console_setup"), os.path.join(lib_dir, "console_setup.py"))
+    shutil.copyfile(
+        os.path.join(current_dir, "jupyter_template.ipynb"),
+        os.path.join(lib_dir, "jupyter_template.ipynb"),
+    )
+    if version >= "2023.2":
+        write_tab_config(os.path.join(toolkit_dir, product), lib_dir)
 
 
 def write_tab_config(product_toolkit_dir, pyaedt_lib_dir, force_write=False):
@@ -79,79 +142,16 @@ def write_tab_config(product_toolkit_dir, pyaedt_lib_dir, force_write=False):
         shutil.copy(os.path.normpath(os.path.join(current_dir, file_name)), dest_file)
 
 
-def install_toolkit(toolkit_dir, product):
-    toolkit_rel_lib_dir = os.path.join("Lib", "PyAEDT")
-    lib_dir = os.path.join(toolkit_dir, toolkit_rel_lib_dir)
-    tool_dir = os.path.join(toolkit_dir, product, "PyAEDT")
-    os.makedirs(lib_dir, exist_ok=True)
-    os.makedirs(tool_dir, exist_ok=True)
-    files_to_copy = ["Console", "Run_PyAEDT_Script", "Jupyter"]
-    jupyter_executable = sys.executable.replace("python.exe", "jupyter.exe")
-    ipython_executable = sys.executable.replace("python.exe", "ipython.exe")
-    for file_name in files_to_copy:
-        with open(os.path.join(current_dir, file_name + ".py_build"), "r") as build_file:
-            file_name_dest = file_name.replace("_", " ") + ".py"
-            with open(os.path.join(tool_dir, file_name_dest), "w") as out_file:
-                print("Building to " + os.path.join(tool_dir, file_name_dest))
-                build_file_data = build_file.read()
-                build_file_data = (
-                    build_file_data.replace("##TOOLKIT_REL_LIB_DIR##", toolkit_rel_lib_dir)
-                    .replace("##PYTHON_EXE##", sys.executable)
-                    .replace("##IPYTHON_EXE##", ipython_executable)
-                    .replace("##JUPYTER_EXE##", jupyter_executable)
-                )
-                out_file.write(build_file_data)
-    shutil.copyfile(os.path.join(current_dir, "console_setup"), os.path.join(lib_dir, "console_setup.py"))
-    shutil.copyfile(
-        os.path.join(current_dir, "jupyter_template.ipynb"),
-        os.path.join(lib_dir, "jupyter_template.ipynb"),
-    )
-    if version >= "2023.2":
-        write_tab_config(os.path.join(toolkit_dir, product), lib_dir)
+def write_pretty_xml(root, file_path):
+    """Write the XML in a pretty format."""
+    # If we use the commented code below, then the previously existing lines will have double lines added. We need to
+    # split and ignore the double lines.
+    # xml_str = parseString(ET.tostring(root)).toprettyxml(indent=" " * 4)
+    lines = [line for line in parseString(ET.tostring(root)).toprettyxml(indent=" " * 4).split("\n") if line.strip()]
+    xml_str = "\n".join(lines)
 
-
-def main():
-    from pyaedt import Desktop
-
-    with Desktop(version, True, new_desktop_session=True) as d:
-        desktop = sys.modules["__main__"].oDesktop
-        pers1 = os.path.join(desktop.GetPersonalLibDirectory(), "pyaedt")
-        pid = desktop.GetProcessID()
-        if os.path.exists(pers1):
-            d.logger.info("PersonalLib already mapped")
-        else:
-            os.system('mklink /D "{}" "{}"'.format(pers1, pyaedt_path))
-
-        toolkits = [
-            "2DExtractor",
-            "CircuitDesign",
-            "Emit",
-            "HFSS",
-            "HFSS-IE",
-            "HFSS3DLayoutDesign",
-            "Icepak",
-            "Maxwell2D",
-            "Maxwell3D",
-            "Q3DExtractor",
-            "TwinBuilder",
-            "Mechanical",
-        ]
-
-        for product in toolkits:
-            try:
-                sys_dir = os.path.join(d.syslib, "Toolkits")
-                install_toolkit(sys_dir, product)
-                d.logger.info("Installed toolkit for {} in sys lib".format(product))
-
-            except IOError:
-                pers_dir = os.path.join(d.personallib, "Toolkits")
-                install_toolkit(pers_dir, product)
-                d.logger.info("Installed toolkit for {} in personal lib".format(product))
-    if pid:
-        try:
-            os.kill(pid, 9)
-        except:
-            pass
+    with open(file_path, "w") as f:
+        f.write(xml_str)
 
 
 if __name__ == "__main__":
