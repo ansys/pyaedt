80c8631575c77edb809df3571c415a895d260e72
jsalant22 <josh.salant@ansys.com>
2023-02-07 16:00:45 -0500

    fix build

--- pyaedt/emit_core/__init__.py
+++ pyaedt/emit_core/__init__.py
@@ -6,7 +6,8 @@ delcross_python_path = os.environ.get("ANSYS_DELCROSS_PYTHON_PATH")
 if delcross_python_path:
     sys.path.append(delcross_python_path)
 desktop_path = os.environ.get("ANSYSEM_ROOT232")
-path = os.path.join(desktop_path, "Delcross")
-sys.path.append(path)
+if desktop_path:
+    path = os.path.join(desktop_path, "Delcross")
+    sys.path.append(path)
 
 EMIT_MODULE = import_module("EmitApiPython")
