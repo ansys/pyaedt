1c949270953a94c2e9edf0f5d40bfbb62d46d346
jsalant22 <josh.salant@ansys.com>
2023-02-08 06:56:15 -0500

    make sure EMIT_MODULE exists

--- pyaedt/emit_core/__init__.py
+++ pyaedt/emit_core/__init__.py
@@ -2,6 +2,8 @@ import os
 import sys
 from importlib import import_module
 
+EMIT_MODULE = None
+
 delcross_python_path = os.environ.get("ANSYS_DELCROSS_PYTHON_PATH")
 if delcross_python_path:
     sys.path.append(delcross_python_path)
