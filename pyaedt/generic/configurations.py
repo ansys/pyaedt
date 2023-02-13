41d3dfeef34f4785538594f897dc7a4c9e1ca004
Lorenzo Vecchietti <lorenzo.vecchietti.1997@gmail.com>
2023-02-13 15:53:58 +0100

    update imports

--- pyaedt/generic/configurations.py
+++ pyaedt/generic/configurations.py
@@ -4,14 +4,17 @@ import os
 from collections import OrderedDict
 from datetime import datetime
 
-from pyaedt import __version__
+from pyaedt import __version__, Icepak, get_pyaedt_app, generate_unique_folder_name
+from pyaedt.application.Variables import decompose_variable_value
 from pyaedt.generic.DataHandlers import _arg2dict
+from pyaedt.generic.LoadAEDTFile import load_keyword_in_aedt_file
 from pyaedt.generic.general_methods import _create_json_file
 from pyaedt.generic.general_methods import generate_unique_name
 from pyaedt.generic.general_methods import pyaedt_function_handler
 from pyaedt.modeler.cad.Modeler import CoordinateSystem
+from pyaedt.modeler.cad.components_3d import UserDefinedComponent
 from pyaedt.modeler.geometry_operators import GeometryOperators
-from pyaedt.modules.Boundary import BoundaryObject
+from pyaedt.modules.Boundary import BoundaryObject, NativeComponentObject
 from pyaedt.modules.Boundary import BoundaryProps
 from pyaedt.modules.DesignXPloration import SetupOpti
 from pyaedt.modules.DesignXPloration import SetupParam
