a23863303033accb6e2ebef3f451c3f6cf91a37c
svandenb <simon.vandenbrouck@ansys.com>
2023-02-17 09:24:37 +0100

    unit test added

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -2152,11 +2152,11 @@ class TestClass(BasisTest, object):
         setup_str = [t.strip("\n\t") for t in setup.ToString().split("\r")]
         assert [f for f in setup_str if "MeshFrequency" in f][0].split("=")[-1].strip("'") == simconfig.mesh_freq
 
-        def test_134_create_port_between_pin_and_layer(self):
-            source_path = os.path.join(local_path, "example_models", test_subfolder, "Galileo.aedb")
-            target_path = os.path.join(self.local_scratch.path, "test_0134.aedb")
-            self.local_scratch.copyfolder(source_path, target_path)
-            edbapp = Edb(target_path, edbversion=desktop_version)
-            edbapp.core_siwave.create_port_between_pin_and_layer(
-                component_name="U2A5", pins_name="AJ30", layer_name="BOTTOM", reference_net="GND"
-            )
+    def test_134_create_port_between_pin_and_layer(self):
+        source_path = os.path.join(local_path, "example_models", test_subfolder, "Galileo.aedb")
+        target_path = os.path.join(self.local_scratch.path, "test_0134.aedb")
+        self.local_scratch.copyfolder(source_path, target_path)
+        edbapp = Edb(target_path, edbversion=desktop_version)
+        edbapp.core_siwave.create_port_between_pin_and_layer(
+            component_name="U2A5", pins_name="AJ30", layer_name="BOTTOM", reference_net="GND"
+        )
