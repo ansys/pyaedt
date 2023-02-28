5c0baeddf78beaca5b1c3d791bb0e85ff6ac1c38
Hui Zhou <ring630@gmail.com>
2023-02-28 17:59:03 +0100

    Update examples/02-HFSS3DLayout/Dcir_in_3DLayout.py
    Co-authored-by: Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>

--- examples/02-HFSS3DLayout/Dcir_in_3DLayout.py
+++ examples/02-HFSS3DLayout/Dcir_in_3DLayout.py
@@ -34,7 +34,7 @@ appedb.core_siwave.create_pin_group_on_net(
     net_name="GND",
     group_name="U3A1-GND")
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#####################################################################################
 # Create voltage source between VRM positive and negative pin groups
 appedb.core_siwave.create_voltage_source_on_pin_group(
     pos_pin_group_name="U3A1-BST_V3P3_S5",
