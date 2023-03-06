80e356ab4384e17f0c62761ddb039e2b5c8e64cb
Hui Zhou <ring630@gmail.com>
2023-03-06 12:12:22 +0100

    Dcir element data (#2573)
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * doc update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * improved speed
    
    * improved open_file method
    
    * improved open_file method
    
    * improved open_file method
    
    * removed pandas from hfss3dlayout.py
    
    * Minor documentation fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    
    * coverage on missing part of the code
    
    ---------
    
    Co-authored-by: ring630 <@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: maxcapodi78 <Shark78>
    Co-authored-by: Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- examples/01-HFSS3DLayout/Dcir_in_3DLayout.py
+++ examples/01-HFSS3DLayout/Dcir_in_3DLayout.py
@@ -98,34 +98,35 @@ hfss3dl.analyze_all()
 hfss3dl.save_project()
 
 ###############################################################################
-# Get loop resistance
+# Get element data
 # ~~~~~~~~~~~~~~~~~~~
-# Get loop resistance from dcir result
-loop_resistance = hfss3dl.get_dcir_solution_data(
-    setup_name="my_setup",
-    show="RL",
-    category="Loop_Resistance")
-print({expression: loop_resistance.data_magnitude(expression) for expression in loop_resistance.expressions})
+# Get loop resistance
+
+loop_resistance = hfss3dl.get_dcir_element_data_loop_resistance(setup_name="my_setup")
+print(loop_resistance)
+
+# ~~~~~~~~~~~~~~~~~~~
+# Get current source
+
+current_source = hfss3dl.get_dcir_element_data_current_source(setup_name="my_setup")
+print(current_source)
+
+# ~~~~~~~~~~~~~~~~~~~
+# Get via information
+
+via = hfss3dl.get_dcir_element_data_via(setup_name="my_setup")
+print(via)
+
 
 ###############################################################################
 # Get voltage
 # ~~~~~~~~~~~
-# Get voltage from dcir result
+# Get voltage from dcir solution data
 voltage = hfss3dl.get_dcir_solution_data(
     setup_name="my_setup",
     show="Sources",
     category="Voltage")
-print({ expression: voltage.data_magnitude(expression) for  expression in voltage.expressions})
-
-###############################################################################
-# Get via
-# ~~~~~~~
-# Get via current from dcir result
-via_current = hfss3dl.get_dcir_solution_data(
-    setup_name="my_setup",
-    show="Vias",
-    category="Current")
-print({ expression: via_current.data_magnitude(expression) for  expression in via_current.expressions[100:105]})
+print({expression: voltage.data_magnitude(expression) for  expression in voltage.expressions})
 
 ###############################################################################
 # Close AEDT
