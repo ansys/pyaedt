a69fce5fe81301a1636f60d5937608580508edf7
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-03-21 16:29:43 +0100

    Fixed minor bugs in lsf batch syntax (#2662)
    * Fixed minor bugs in lsf batch syntax
    
    * Fixed units error in circuit
    
    * Fixed units error in circuit
    
    * improved precommit
    
    * Update pyaedt/generic/general_methods.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * fixed lsf queue usage
    
    ---------
    
    Co-authored-by: maxcapodi78 <Shark78>
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/modeler/circuits/PrimitivesCircuit.py
+++ pyaedt/modeler/circuits/PrimitivesCircuit.py
@@ -1159,20 +1159,19 @@ class CircuitComponents(object):
 
     @pyaedt_function_handler()
     def number_with_units(self, value, units=None):
-        """Convert a number to a string with units. If value is a string it's returned as is.
+        """Convert a number to a string with units. If value is a string, it's returned as is.
 
         Parameters
         ----------
         value : float, int, str
             Input  number or string.
         units : optional
-            Units for formatting. The default is ``None`` which uses ``"meter"``.
+            Units for formatting. The default is ``None``, which uses ``"meter"``.
 
         Returns
         -------
         str
            String concatenating the value and unit.
-
         """
         return self._app.number_with_units(value, units)
 
