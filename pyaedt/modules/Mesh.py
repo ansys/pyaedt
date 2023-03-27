762ea6a9619f36e52079f8382c1839eaa12009c6
gmalinve <103059376+gmalinve@users.noreply.github.com>
2023-03-27 15:20:51 +0200

    Update pyaedt/modules/Mesh.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/modules/Mesh.py
+++ pyaedt/modules/Mesh.py
@@ -1370,14 +1370,14 @@ class Mesh(object):
             The default is ``None``.
         moving_side_layers : int, optional
             Number of mesh layers on the moving side.
-            The valid ranges are integers >= 1.
-            Option valid only for 3D design.
-            The default values is ``1``.
+            The valid ranges are integers greater or equal to 1.
+            This parameter is valid only for a 3D design.
+            The default is ``1``.
         static_side_layers : int, optional
             Number of mesh layers on the static side.
-            The valid ranges are integers >= 1.
-            Option valid only for 3D design.
-            The default values is ``1``.
+            The valid ranges are integers greater than or equal to 1.
+            This parameter is valid only for a 3D design.
+            The default is ``1``.
 
         Returns
         -------
