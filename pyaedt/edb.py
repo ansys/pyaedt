267e7bddb123636f687a12c81e1bcf4be8598df1
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-04-07 16:17:48 +0200

    fixed documentations (#2799)
    Co-authored-by: maxcapodi78 <Shark78>

--- pyaedt/edb.py
+++ pyaedt/edb.py
@@ -2815,6 +2815,7 @@ class Edb(object):
     @pyaedt_function_handler()
     def change_design_variable_value(self, variable_name, variable_value):
         """Change a variable value.
+
         ..note::
             User can use also the getitem to read the variable value. See example below.
 
