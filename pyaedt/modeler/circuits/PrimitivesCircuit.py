303be75863ca04bb8b7960d1ba245fa016850415
maxcapodi78 <Shark78>
2023-04-07 14:39:23 +0200

    fixed refresh_all_ids

--- pyaedt/modeler/circuits/PrimitivesCircuit.py
+++ pyaedt/modeler/circuits/PrimitivesCircuit.py
@@ -916,7 +916,7 @@ class CircuitComponents(object):
         ----------
 
         >>> oEditor.GetAllElements()"""
-        obj = self.oeditor.GetAllElements()
+        obj = list(self.oeditor.GetAllElements())
         obj = [i for i in obj if "Wire" not in i[:4]]
         for el in obj:
             if not self.get_obj_id(el):
