a58be946f2d1a18f3f55d2692d6569039ed76747
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2023-04-20 21:20:24 +0200

    Update pyaedt/edb_core/components.py
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>

--- pyaedt/edb_core/components.py
+++ pyaedt/edb_core/components.py
@@ -1007,7 +1007,7 @@ class Components(object):
                 self._get_edb_value(neg_pin_loc[0]), self._get_edb_value(neg_pin_loc[1])
             )
             neg_pin_term = self._pedb.edb.Cell.Terminal.PointTerminal.Create(
-                self._active_layout, pins[1].GetNet(), f"{component.refdes}_{pins[1].GetName()}_ref", pt, pin_layers[0]
+                self._active_layout, pins[1].GetNet(), "{}_{}_ref".format(component.refdes, pins[1].GetName()), pt, pin_layers[0]
             )
             if not neg_pin_term:  # pragma: no cover
                 return False
