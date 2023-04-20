52fe049dcfaadf001c26fc07d09dbdba40ef9aa7
jsalant <josh.salant@ansys.com>
2023-04-20 13:49:31 -0400

    fix version check for importing EmitApi

--- pyaedt/emit_core/__init__.py
+++ pyaedt/emit_core/__init__.py
@@ -13,7 +13,8 @@ if delcross_python_path:
 # the list is reverse sorted, so newest version is first
 latest_version = list_installed_ansysem()[0]
 desktop_path = os.environ.get(latest_version)
-if desktop_path:
+numeric_version = int(latest_version[-3:])
+if desktop_path and numeric_version > 231:
     path = os.path.join(desktop_path, "Delcross")
     sys.path.append(path)
     EMIT_MODULE = import_module("EmitApiPython")
