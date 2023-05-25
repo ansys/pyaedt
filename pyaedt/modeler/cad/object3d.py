5270baa915fa51eeadd4f7e6e538423cbde29d77
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-05-25 17:57:03 +0200

    Fix/property class (#3055)
    * fix crash happening in debug mode
    
    * fix crash happening in debug mode
    
    * fix crash happening in debug mode
    
    * fix crash happening in debug mode
    
    ---------
    
    Co-authored-by: maxcapodi78 <Shark78>
    Co-authored-by: Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>

--- pyaedt/modeler/cad/object3d.py
+++ pyaedt/modeler/cad/object3d.py
@@ -1877,26 +1877,9 @@ class Object3d(object):
 
     def __str__(self):
         return """
-         {}
          name: {}    id: {}    object_type: {}
-         --- read/write properties  ----
-         solve_inside: {}
-         model: {}
-         material_name: {}
-         color: {}
-         transparency: {}
-         display_wireframe {}
-         part_coordinate_system: {}
          """.format(
-            type(self),
             self.name,
             self.id,
-            self.object_type,
-            self.solve_inside,
-            self.model,
-            self.material_name,
-            self.color,
-            self.transparency,
-            self.display_wireframe,
-            self.part_coordinate_system,
+            self._object_type,
         )
