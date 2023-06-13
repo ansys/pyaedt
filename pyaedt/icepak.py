70200a6853dac473659f28599f5a3a0c895aa5c8
lorenzovecchietti <lorenzo.vecchietti@ansys.com>
2023-06-13 10:07:08 +0200

    refactoring for python 2.7 compatibility

--- pyaedt/icepak.py
+++ pyaedt/icepak.py
@@ -23,8 +23,6 @@ from pyaedt.application.Analysis3D import FieldAnalysis3D
 from pyaedt.generic.DataHandlers import _arg2dict
 from pyaedt.generic.DataHandlers import random_string
 from pyaedt.generic.configurations import ConfigurationsIcepak
-
-# from pyaedt.generic.general_methods import property
 from pyaedt.generic.general_methods import generate_unique_name
 from pyaedt.generic.general_methods import open_file
 from pyaedt.generic.general_methods import pyaedt_function_handler
