8e9b924e22b1f05bf0b71a63af0e2875643557ab
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-06-21 15:02:16 +0000

    Replace galileo in unittest with Ansys edb (#3159)
    * Removal of Galileo board from unit tests
    
    * additional fixes
    
    * fix numpy
    
    * fix numpy
    
    * fixed Edb files.
    Missing Circuit
    
    * Improved desktop launcher
    
    ---------
    
    Co-authored-by: maxcapodi78 <Shark78>

--- pyaedt/generic/general_methods.py
+++ pyaedt/generic/general_methods.py
@@ -651,7 +651,8 @@ def _retry_ntimes(n, function, *args, **kwargs):
     while retry < n:
         try:
             ret_val = function(*args, **kwargs)
-            if not ret_val and type(ret_val) is not float and type(ret_val) is not int:
+            if ret_val is None:
+                # if not ret_val and type(ret_val) not in [float, int, str, tuple, list]:
                 ret_val = True
         except:
             retry += 1
