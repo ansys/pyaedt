3d4023a64d3b713335043602202a6afe0620a59e
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-06-22 17:55:22 +0200

    Update pyaedt/edb.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/edb.py
+++ pyaedt/edb.py
@@ -1437,7 +1437,7 @@ class Edb(Database):
         preserve_components_with_model=False,
     ):
         """Create a cutout using an approach entirely based on PyAEDT.
-        This new method replaces all legacy cutout methods in pyaedt.
+        This method replaces all legacy cutout methods in PyAEDT.
         It does in sequence:
         - delete all nets not in list,
         - create a extent of the nets,
