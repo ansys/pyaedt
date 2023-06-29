e2e3e99482d27b366a7aeb4b972d1ff8cbb1a50a
Samuel Lopez <samuel.lopez@ansys.com>
2023-06-29 12:37:37 +0200

    Allow parameters in bondwire definition

--- pyaedt/modeler/cad/Primitives3D.py
+++ pyaedt/modeler/cad/Primitives3D.py
@@ -518,9 +518,29 @@ class Primitives3D(Primitives, object):
         ...                                                     bond_type=0, name="mybox", matname="copper")
         """
         x_position, y_position, z_position = self._pos_with_arg(start_position)
+        x_position_end, y_position_end, z_position_end = self._pos_with_arg(end_position)
+
         if x_position is None or y_position is None or z_position is None:
             raise AttributeError("Position Argument must be a valid 3 Element List")
-        x_length, y_length, z_length = self._pos_with_arg([n - m for m, n in zip(start_position, end_position)])
+
+        cont = 0
+        x_length = None
+        y_length = None
+        z_length = None
+
+        for m, n in zip(start_position, end_position):
+            if not isinstance(m, str):
+                m = self._arg_with_dim(m)
+            if not isinstance(n, str):
+                n = self._arg_with_dim(n)
+            if cont == 0:
+                x_length = "(" + str(n) + ") - (" + str(m) + ")"
+            elif cont == 1:
+                y_length = "(" + str(n) + ") - (" + str(m) + ")"
+            elif cont == 2:
+                z_length = "(" + str(n) + ") - (" + str(m) + ")"
+            cont += 1
+
         if x_length is None or y_length is None or z_length is None:
             raise AttributeError("Dimension Argument must be a valid 3 Element List")
         if bond_type == 0:
@@ -542,9 +562,24 @@ class Primitives3D(Primitives, object):
         first_argument.append("XDir:="), first_argument.append(x_length)
         first_argument.append("YDir:="), first_argument.append(y_length)
         first_argument.append("ZDir:="), first_argument.append(z_length)
-        first_argument.append("Distance:="), first_argument.append(
-            self._arg_with_dim(GeometryOperators.points_distance(start_position, end_position))
+        distance = (
+            "sqrt(("
+            + str(x_position_end)
+            + "-("
+            + str(x_position)
+            + ")) ** 2 + ("
+            + str(y_position_end)
+            + "-("
+            + str(y_position)
+            + ")) ** 2 + ( "
+            + str(z_position_end)
+            + "-("
+            + str(z_position)
+            + ")) ** 2) meter"
         )
+
+        first_argument.append("Distance:="), first_argument.append(distance)
+
         first_argument.append("h1:="), first_argument.append(self._arg_with_dim(h1))
         first_argument.append("h2:="), first_argument.append(self._arg_with_dim(h2))
         first_argument.append("alpha:="), first_argument.append(self._arg_with_dim(alpha, "deg"))
