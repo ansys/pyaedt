e009cb4d42a312f2fe5671f54294778e35913559
adimaria <alberto.dimaria@ansys.com>
2023-07-19 18:44:15 +0200

    fix in SBR+ test to avoid to invalidate solutions

--- _unittest/test_17_SBR.py
+++ _unittest/test_17_SBR.py
@@ -196,7 +196,7 @@ class TestClass(BasisTest, object):
 
     @pytest.mark.skipif(is_linux or sys.version_info < (3, 8), reason="Not supported.")
     def test_13_link_array(self):
-        self.array.setups[0].props["MaximumPasses"] = 1
+        # self.array.setups[0].props["MaximumPasses"] = 1
         assert self.sbr_platform.create_sbr_linked_antenna(self.array, target_cs="antenna_CS", fieldtype="farfield")
         self.sbr_platform.analyze()
         ffdata = self.sbr_platform.get_antenna_ffd_solution_data(frequencies=12e9, sphere_name="3D")
