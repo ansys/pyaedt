2fefcbdbdb152e20103e118f88ab9eb6f273a9f5
madelinecoleman1 <madeline.coleman@ansys.com>
2023-07-25 17:54:46 -0400

    Revert "reconfigured emit tests to use example model"
    This reverts commit ed846840bb9c0329fd528ac24d41032b69aa0512.

--- _unittest/test_26_emit.py
+++ _unittest/test_26_emit.py
@@ -931,11 +931,68 @@ class TestClass(BasisTest, object):
         config["desktopVersion"] <= "2023.1" or is_ironpython,
         reason="Skipped on versions earlier than 2023.2",
     )
-    def test_interference_scripts_no_filter(self):
-        self.aedtapp = BasisTest.add_app(self, project_name="interference", application=Emit, subfolder=test_subfolder)
+    def test_interference_script(self):
+        sys.path.append("examples/07-EMIT")
+        from interference_classification import interference_type_classification
+        from interference_classification import protection_level_classification
+
+        self.aedtapp = BasisTest.add_app(self, application=Emit)
+        # place components
+        rad1 = self.aedtapp.modeler.components.create_component("Bluetooth Low Energy (LE)")
+        ant1 = self.aedtapp.modeler.components.create_component("Antenna")
+        if rad1 and ant1:
+            ant1.move_and_connect_to(rad1)
+        rad2 = self.aedtapp.modeler.components.create_component("GPS Receiver")
+        ant2 = self.aedtapp.modeler.components.create_component("Antenna")
+        if rad2 and ant2:
+            ant2.move_and_connect_to(rad2)
+        rad3 = self.aedtapp.modeler.components.create_component("WiFi - 802.11-2012")
+        ant3 = self.aedtapp.modeler.components.create_component("Antenna")
+        if rad3 and ant3:
+            ant3.move_and_connect_to(rad3)
+
+        # Reduce the bluetooth transmit power
+        bands = rad1.bands()
+        for band in bands:
+            band.set_band_power_level(-20)
+
+        # Enable L2 P(Y) GPS Band
+        bands = rad2.bands()
+        for band in bands:
+            for child in band.children:
+                if "L2 P(Y)" in band.node_name:
+                    band.enabled = True
+                else:
+                    band.enabled = False
+
+        # Enable HR-DSSS Ch 1-13 Wifi band
+        bands = rad3.bands()
+        for band in bands:
+            if "HR-DSSS" in band.node_name:
+                if "Ch 1-13" in band.node_name:
+                    band.enabled = True
+                    band.set_band_power_level(-20)
 
         # Generate a revision
+        assert len(self.aedtapp.results.revisions) == 0
         rev = self.aedtapp.results.analyze()
+        assert len(self.aedtapp.results.revisions) == 1
+
+        # Get list of RX and TX radios
+        radiosRX = rev.get_receiver_names()
+        radiosTX = rev.get_interferer_names(InterfererType.TRANSMITTERS)
+
+        assert len(radiosRX) == 3
+        assert len(radiosTX) == 2
+
+        rx_bands = ["Band", "L2 P(Y)", "HR-DSSS Rx - Ch 1-13"]
+        tx_bands = ["Band", "HR-DSSS Tx - Ch 1-13"]
+
+        for i in range(len(radiosRX)):
+            assert rx_bands[i] == rev.get_band_names(radiosRX[i], TxRxMode.RX)[0]
+
+        for i in range(len(radiosTX)):
+            assert tx_bands[i] == rev.get_band_names(radiosTX[i], TxRxMode.TX)[0]
 
         # Test with no filtering
         expected_interference_colors = [["white", "green", "yellow"], ["red", "green", "white"]]
@@ -943,14 +1000,13 @@ class TestClass(BasisTest, object):
         expected_protection_colors = [["white", "yellow", "yellow"], ["yellow", "yellow", "white"]]
         expected_protection_power = [["N/A", -20.0, -20.0], [-20.0, -20.0, "N/A"]]
 
-        domain = self.aedtapp.results.interaction_domain()
         interference_colors = []
         interference_power_matrix = []
         protection_colors = []
         protection_power_matrix = []
-        interference_colors, interference_power_matrix = rev.interference_type_classification(domain)
-        protection_colors, protection_power_matrix = rev.protection_level_classification(
-            domain, global_protection_level=True, global_levels=[30, -4, -30, -104]
+        interference_colors, interference_power_matrix = interference_type_classification(self.aedtapp)
+        protection_colors, protection_power_matrix = protection_level_classification(
+            self.aedtapp, global_protection_level=True, global_levels=[30, -4, -30, -104]
         )
 
         assert interference_colors == expected_interference_colors
@@ -970,25 +1026,14 @@ class TestClass(BasisTest, object):
 
         protection_colors = []
         protection_power_matrix = []
-        protection_colors, protection_power_matrix = rev.protection_level_classification(
-            domain, global_protection_level=False, protection_levels=protection_levels
+        protection_colors, protection_power_matrix = protection_level_classification(
+            self.aedtapp, global_protection_level=False, protection_levels=protection_levels
         )
 
         assert protection_colors == expected_protection_colors
         assert protection_power_matrix == expected_protection_power
 
-    @pytest.mark.skipif(
-        config["desktopVersion"] <= "2023.1" or is_ironpython,
-        reason="Skipped on versions earlier than 2023.2",
-    )
-    def test_interference_scripts_fitlering(self):
-        self.aedtapp = BasisTest.add_app(self, project_name="interference", application=Emit, subfolder=test_subfolder)
-
-        # Generate a revision
-        rev = self.aedtapp.results.analyze()
-
         # Test with active filtering
-        domain = self.aedtapp.results.interaction_domain()
         interference_colors = []
         interference_power_matrix = []
         protection_colors = []
@@ -1033,12 +1078,12 @@ class TestClass(BasisTest, object):
             interference_filter = interference_filters[:ind] + interference_filters[ind + 1 :]
             protection_filter = protection_filters[:ind] + protection_filters[ind + 1 :]
 
-            interference_colors, interference_power_matrix = rev.interference_type_classification(
-                domain, use_filter=True, filter_list=interference_filter
+            interference_colors, interference_power_matrix = interference_type_classification(
+                self.aedtapp, use_filter=True, filter_list=interference_filter
             )
 
-            protection_colors, protection_power_matrix = rev.protection_level_classification(
-                domain,
+            protection_colors, protection_power_matrix = protection_level_classification(
+                self.aedtapp,
                 global_protection_level=True,
                 global_levels=[30, -4, -30, -104],
                 use_filter=True,
