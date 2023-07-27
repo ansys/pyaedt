c42654490efe6a1be438d642d0c1ac5fbf3018ed
adimaria <alberto.dimaria@ansys.com>
2023-07-28 00:53:26 +0200

    refactored test_15_ibs_reader.py with fixtures.

--- _unittest/test_15_ibs_reader.py
+++ _unittest/test_15_ibs_reader.py
@@ -1,6 +1,7 @@
 import os
+import pytest
 
-from _unittest.conftest import BasisTest
+# from _unittest.conftest import BasisTest
 from _unittest.conftest import local_path
 
 from pyaedt import Circuit
@@ -8,14 +9,24 @@ from pyaedt.generic import ibis_reader
 
 test_subfolder = "T15"
 
+@pytest.fixture(scope="class")
+def aedtapp(add_app):
+    app = add_app(application=Circuit)
+    return app
 
-class TestClass(BasisTest, object):
-    def setup_class(self):
-        BasisTest.my_setup(self)
-        self.aedtapp = BasisTest.add_app(self, application=Circuit)
 
-    def teardown_class(self):
-        BasisTest.my_teardown(self)
+class TestClass:
+    # def setup_class(self):
+    #     BasisTest.my_setup(self)
+    #     self.aedtapp = BasisTest.add_app(self, application=Circuit)
+    #
+    # def teardown_class(self):
+    #     BasisTest.my_teardown(self)
+
+    @pytest.fixture(autouse=True)
+    def init(self, aedtapp, local_scratch):
+        self.aedtapp = aedtapp
+        self.local_scratch = local_scratch
 
     def test_01_read_ibis(self):
         reader = ibis_reader.IbisReader(
