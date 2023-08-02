717a417eb3995521dce3808d6d91645a13b52cbb
adimaria <alberto.dimaria@ansys.com>
2023-08-02 15:21:53 +0200

    improved IronPython tests

--- _unittest_ironpython/run_unittests.py
+++ _unittest_ironpython/run_unittests.py
@@ -47,59 +47,96 @@ class TestIronPython(unittest.TestCase):
         aedtapp.solution_type = "Modal"
         self.assertTrue(aedtapp.solution_type == "Modal")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_circuit(self):
         aedtapp = Circuit()
         self.assertTrue(aedtapp.design_type == "Circuit Design")
         self.assertTrue(aedtapp.solution_type == "NexximLNA")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_icepak(self):
         aedtapp = Icepak()
         self.assertTrue(aedtapp.design_type == "Icepak")
         self.assertTrue(aedtapp.solution_type == "SteadyState")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_hfss3dlayout(self):
         aedtapp = Hfss3dLayout()
         self.assertTrue(aedtapp.design_type == "HFSS 3D Layout Design")
         self.assertTrue(aedtapp.solution_type == "HFSS3DLayout")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_twinbuilder(self):
         aedtapp = TwinBuilder()
         self.assertTrue(aedtapp.design_type == "Twin Builder")
         self.assertTrue(aedtapp.solution_type == "TR")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_q2d(self):
         aedtapp = Q2d()
         self.assertTrue(aedtapp.design_type == "2D Extractor")
         self.assertTrue(aedtapp.solution_type == "Open")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_q3d(self):
         aedtapp = Q3d()
         self.assertTrue(aedtapp.design_type == "Q3D Extractor")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_maxwell2d(self):
         aedtapp = Maxwell2d()
         self.assertTrue(aedtapp.design_type == "Maxwell 2D")
         self.assertTrue(aedtapp.solution_type == "Magnetostatic")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_hfss(self):
         aedtapp = Hfss()
         self.assertTrue(aedtapp.design_type == "HFSS")
         self.assertTrue("Modal" in aedtapp.solution_type)
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
     def test_run_desktop_maxwell3d(self):
         aedtapp = Maxwell3d()
         self.assertTrue(aedtapp.design_type == "Maxwell 3D")
         self.assertTrue(aedtapp.solution_type == "Magnetostatic")
         self.assertTrue(aedtapp.modeler)
+        self.assertTrue(aedtapp.post)
+        self.assertTrue(aedtapp.materials)
+        self.assertTrue(aedtapp.mesh)
+        self.assertTrue(aedtapp.variable_manager)
 
 
 if __name__ == '__main__':
