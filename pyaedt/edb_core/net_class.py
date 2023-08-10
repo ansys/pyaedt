d5f659dc58182d75519b519127fec5929fb2283a
ring630 <@gmail.com>
2023-08-10 12:36:23 +0200

    fix

--- pyaedt/edb_core/net_class.py
+++ pyaedt/edb_core/net_class.py
@@ -287,7 +287,7 @@ class EdbDifferentialPairs(EdbCommon, object):
 
     @pyaedt_function_handler
     def auto_identify(self, positive_differentiator="_P", negative_differentiator="_N"):
-        """Generate differential pairs.
+        """Auto identify differential pairs by naming conversion.
 
         Parameters
         ----------
