4c1b58cbb2d0ff133bd3bbe0e2ba5feb883045b0
svandenb-dev <74993647+svandenb-dev@users.noreply.github.com>
2023-08-23 13:16:51 +0200

    Update pyaedt/edb_core/padstack.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/edb_core/padstack.py
+++ pyaedt/edb_core/padstack.py
@@ -1333,7 +1333,7 @@ class EdbPadstacks(object):
     def get_reference_pins(
         self, positive_pin, reference_net="gnd", search_radius=5e-3, max_limit=0, component_only=True
     ):
-        """Search for reference pins with several criteria.
+        """Search for reference pins using given criteria.
         Parameters
         ----------
         positive_pin : EDBPadstackInstance
