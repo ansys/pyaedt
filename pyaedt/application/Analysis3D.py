34e0a7e190febf433f3506bcfc8ff7fa5cb86b2f
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2023-09-06 14:47:07 +0200

    Added additional control to rpc connection to avoid errors in PyAEDT (#3518)
    * Added additional control to rpc connection to avoid errors in PyAEDT
    
    * Added additional control to rpc connection to avoid errors in PyAEDT
    
    * Added additional control to rpc connection to avoid errors in PyAEDT
    
    * Added additional control to rpc connection to avoid errors in PyAEDT
    
    * Added additional control to rpc connection to avoid errors in PyAEDT
    
    * kept for back compatibility
    
    * kept for back compatibility
    
    ---------
    
    Co-authored-by: maxcapodi78 <Shark78>

--- pyaedt/application/Analysis3D.py
+++ pyaedt/application/Analysis3D.py
@@ -668,7 +668,7 @@ class FieldAnalysis3D(Analysis, object):
 
         if mat.lower() in self.materials.material_keys:
             matobj = self.materials.material_keys[mat.lower()]
-        elif self.materials._get_aedt_case_name(mat) or settings.remote_api:
+        elif self.materials._get_aedt_case_name(mat) or settings.remote_api or settings.remote_rpc_session:
             matobj = self.materials._aedmattolibrary(mat)
         if matobj:
             if self.design_type == "HFSS":
