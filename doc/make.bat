673ea88398aa46bfc38f04653f9407e50e55e2ea
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-09-18 14:26:18 +0200

    fix: add pdf comments

--- doc/make.bat
+++ doc/make.bat
@@ -31,5 +31,11 @@ goto end
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
+:pdf
+%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+cd "%BUILDDIR%\latex"
+for %%f in (*.tex) do (
+pdflatex "%%f" --interaction=nonstopmode)
+
 :end
 popd
