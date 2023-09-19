5f6c5fa54f243fdb6448732b65377bb72663b28b
maxcapodi78 <Shark78>
2023-09-19 16:18:08 +0200

    Various improvements to add new MRI Example

--- examples/06-Multiphysics/MRI.py
+++ examples/06-Multiphysics/MRI.py
@@ -21,7 +21,13 @@ import os.path
 
 from pyaedt import Hfss, Mechanical, Icepak, downloads
 
+###############################################################################
+# Set non-graphical mode
+# ~~~~~~~~~~~~~~~~~~~~~~
+# Set non-graphical mode. `
+# You can set ``non_graphical`` either to ``True`` or ``False``.
 
+non_graphical = False
 
 ###############################################################################
 # Project load
@@ -33,7 +39,8 @@ from pyaedt import Hfss, Mechanical, Icepak, downloads
 # Separate objects are used to selectively assign mesh operations
 # Material properties defined in  this project already contain #electrical and thermal properties.
 project_path = downloads.download_file(directory="mri")
-hfss = Hfss(os.path.join(project_path, "background_SAR.aedt"), specified_version="2023.2")
+hfss = Hfss(os.path.join(project_path, "background_SAR.aedt"), specified_version="2023.2", non_graphical=non_graphical,
+            new_desktop_session=True)
 ###############################################################################
 # Insert 3D component
 # ~~~~~~~~~~~~~~~~~~~
@@ -112,7 +119,6 @@ sol_data.data_real()
 
 hfss["input_scale"] = 1 / sol_data.data_real()[0]
 
-
 ###############################################################################
 # Phantom with Implant
 # ~~~~~~~~~~~~~~~~~~~~
@@ -127,7 +133,6 @@ hfss.modeler["implant_box"].subtract("rod", keep_originals=True)
 hfss.modeler["rod"].material_name = "titanium"
 hfss.analyze(num_cores=6)
 hfss.save_project()
-
 
 ###############################################################################
 # Thermal Simulation
@@ -167,8 +172,8 @@ mech.assign_uniform_convection(mech.modeler["Region"].faces, convection_value=1)
 
 setup = mech.create_setup()
 # setup.add_mesh_link("backgroundSAR")
-#mech.create_dataset1d_design("PowerMap", [0, 239, 240, 360], [1, 1, 0, 0])
-#exc.props["LossMultiplier"] = "pwl(PowerMap,Time)"
+# mech.create_dataset1d_design("PowerMap", [0, 239, 240, 360], [1, 1, 0, 0])
+# exc.props["LossMultiplier"] = "pwl(PowerMap,Time)"
 
 mech.modeler.set_working_coordinate_system("implant")
 mech.modeler.create_point([0, 0, 0], name="Point1")
@@ -191,10 +196,12 @@ mech.analyze(num_cores=6)
 # Plot Temperature on point.
 
 
-mech.post.create_fieldplot_cutplane("implant:YZ", "Temperature", filter_objects=["implant_box"], intrinsincDict={"Time":"10s"})
+mech.post.create_fieldplot_cutplane("implant:YZ", "Temperature", filter_objects=["implant_box"],
+                                    intrinsincDict={"Time": "10s"})
 mech.save_project()
 
-data = mech.post.get_solution_data("Temperature", primary_sweep_variable="Time", context="Point1", report_category="Fields")
+data = mech.post.get_solution_data("Temperature", primary_sweep_variable="Time", context="Point1",
+                                   report_category="Fields")
 data.plot()
 
 ###############################################################################
@@ -238,7 +245,7 @@ setup = ipk.create_setup()
 setup.props["Stop Time"] = 60
 setup.props["N Steps"] = 2
 setup.props["Time Step"] = 5
-setup.props['Convergence Criteria - Energy']= 1e-12
+setup.props['Convergence Criteria - Energy'] = 1e-12
 
 ################################################################################
 # Mesh Region
@@ -247,13 +254,12 @@ setup.props['Convergence Criteria - Energy']= 1e-12
 
 
 bound = ipk.modeler["implant_box"].bounding_box
-mesh_box = ipk.modeler.create_box(bound[:3], [bound[3]-bound[0], bound[4]-bound[1], bound[5]-bound[2]])
+mesh_box = ipk.modeler.create_box(bound[:3], [bound[3] - bound[0], bound[4] - bound[1], bound[5] - bound[2]])
 mesh_box.model = False
 mesh_region = ipk.mesh.assign_mesh_region([mesh_box.name])
 mesh_region.UserSpecifiedSettings = False
 mesh_region.Level = 4
 mesh_region.update()
-
 
 ################################################################################
 # Point Monitor
@@ -261,9 +267,8 @@ mesh_region.update()
 # Create a new point monitor.
 
 ipk.modeler.set_working_coordinate_system("implant")
-ipk.monitor.assign_point_monitor([0,0,0], monitor_name="Point1")
+ipk.monitor.assign_point_monitor([0, 0, 0], monitor_name="Point1")
 ipk.assign_openings(ipk.modeler["Region"].top_face_z)
-
 
 ###############################################################################
 # Analyze and plot fields
@@ -273,11 +278,11 @@ ipk.assign_openings(ipk.modeler["Region"].top_face_z)
 # Plot Temperature on monitor point.
 
 ipk.analyze(num_cores=6)
-ipk.post.create_fieldplot_cutplane("implant:YZ", "Temperature", filter_objects=["implant_box"], intrinsincDict={"Time":"0s"})
+ipk.post.create_fieldplot_cutplane("implant:YZ", "Temperature", filter_objects=["implant_box"],
+                                   intrinsincDict={"Time": "0s"})
 ipk.save_project()
 
 data = ipk.post.get_solution_data("Point1.Temperature", primary_sweep_variable="Time", report_category="Monitor")
 data.plot()
-
 
 ipk.release_desktop(False)
