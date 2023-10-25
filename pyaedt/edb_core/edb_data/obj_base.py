38a3e4f38aa86f77bae659c978502571b31cb041
ring630 <@gmail.com>
2023-10-25 16:00:41 +0200

    fix

--- pyaedt/edb_core/edb_data/obj_base.py
+++ pyaedt/edb_core/edb_data/obj_base.py
@@ -1,9 +1,9 @@
 class ObjBase(object):
     """Manages EDB functionalities for a base object."""
 
-    def __init__(self, pedb, model):
+    def __init__(self, pedb, edb_object):
         self._pedb = pedb
-        self._edb_object = model
+        self._edb_object = edb_object
 
     @property
     def is_null(self):
--- pyaedt/edb_core/edb_data/ports_sources.py
+++ pyaedt/edb_core/edb_data/ports_sources.py
@@ -176,30 +176,6 @@ class ExcitationSources(Terminal):
         self._edb_object.SetSourcePhase(self._edb.utility.value(value))
 
 
-class ExcitationProbes(Terminal):
-    """Manage probes properties.
-
-    Parameters
-    ----------
-    pedb : pyaedt.edb.Edb
-        Edb object from Edblib.
-    edb_terminal : Ansys.Ansoft.Edb.Cell.Terminal.EdgeTerminal
-        Edge terminal instance from Edb.
-
-
-    Examples
-    --------
-    This example shows how to access this class.
-    >>> from pyaedt import Edb
-    >>> edb = Edb("myaedb.aedb")
-    >>> probes = edb.probes
-    >>> print(probes["Probe1"].name)
-    """
-
-    def __init__(self, pedb, edb_terminal):
-        Terminal.__init__(self, pedb, edb_terminal)
-
-
 class BundleWavePort(BundleTerminal):
     """Manages bundle wave port properties.
 
@@ -290,3 +266,27 @@ class CoaxPort(PadstackInstanceTerminal):
         p = self._hfss_port_property
         p["Radial Extent Factor"] = value
         self._hfss_port_property = p
+
+
+class VoltageProbe:
+    """Manage probes properties.
+
+    Parameters
+    ----------
+    pedb : pyaedt.edb.Edb
+        Edb object from Edblib.
+    edb_terminal : Ansys.Ansoft.Edb.Cell.Terminal.EdgeTerminal
+        Edge terminal instance from Edb.
+
+
+    Examples
+    --------
+    This example shows how to access this class.
+    >>> from pyaedt import Edb
+    >>> edb = Edb("myaedb.aedb")
+    >>> probes = edb.probes
+    >>> print(probes["Probe1"].name)
+    """
+
+    def __init__(self, pedb, edb_object):
+        Terminal.__init__(self, pedb, edb_object)
