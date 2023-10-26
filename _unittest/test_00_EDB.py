3d08a06409398e7f93443b049392458762e8e5f0
Sebastien Morais <sebastien.morais@ansys.com>
2023-10-26 17:23:15 +0200

    TEST: add read materials system tests

--- _unittest/test_00_EDB.py
+++ _unittest/test_00_EDB.py
@@ -3029,8 +3029,37 @@ class TestClass:
         assert mats == expected_res
 
     def test_150_material_load_syslib_amat(self):
+        """Load material from an amat file."""
+        assert self.edbapp.materials.load_syslib_amat()
         material_list = list(self.edbapp.materials.materials.keys())
         assert material_list
         assert len(material_list) > 0
         assert self.edbapp.materials.materials["Rogers RO3003 (tm)"].loss_tangent == 0.0013
         assert self.edbapp.materials.materials["Rogers RO3003 (tm)"].permittivity == 3.0
+
+    def test_151_materials_read_materials(self):
+        """Read materials."""
+        path = os.path.join(local_path, "example_models", "syslib", "Materials.amat")
+        mats = self.edbapp.materials.read_materials(path)
+        key = "FC-78"
+        assert key in mats
+        assert mats[key]["thermal_conductivity"] == 0.062
+        assert mats[key]["mass_density"] == 1700
+        assert mats[key]["specific_heat"] == 1050
+        assert mats[key]["thermal_expansion_coeffcient"] == 0.0016
+        key = "Polyflon CuFlon (tm)"
+        assert key in mats
+        assert mats[key]["permittivity"] == 2.1
+        assert mats[key]["tangent_delta"] == 0.00045
+        key = "Water(@360K)"
+        assert key in mats
+        assert mats[key]["thermal_conductivity"] == 0.6743
+        assert mats[key]["mass_density"] == 967.4
+        assert mats[key]["specific_heat"] == 4206
+        assert mats[key]["thermal_expansion_coeffcient"] == 0.0006979
+        key = "steel_stainless"
+        assert mats[key]["conductivity"] == 1100000
+        assert mats[key]["thermal_conductivity"] == 13.8
+        assert mats[key]["mass_density"] == 8055
+        assert mats[key]["specific_heat"] == 480
+        assert mats[key]["thermal_expansion_coeffcient"] == 1.08e-005
