de15d37d9f81fa845db34189471ef190867ea0ec
ring630 <@gmail.com>
2023-11-02 15:22:55 +0100

    fix

--- pyaedt/edb_core/edb_data/simulation_setup.py
+++ pyaedt/edb_core/edb_data/simulation_setup.py
@@ -1,4 +1,3 @@
-from pyaedt.edb_core.general import convert_netdict_to_pydict
 from pyaedt.edb_core.general import convert_pydict_to_netdict
 from pyaedt.generic.general_methods import generate_unique_name
 from pyaedt.generic.general_methods import is_linux
