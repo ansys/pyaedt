2fcf737aa0da5eb06fafaa7a788665de9d1e2ef0
lorenzovecchietti <lorenzo.vecchietti@ansys.com>
2023-11-14 10:58:24 +0100

    add no cover to GrpcApiError

--- pyaedt/icepak.py
+++ pyaedt/icepak.py
@@ -5047,7 +5047,7 @@ class Icepak(FieldAnalysis3D):
                 return bound
             else:  # pragma : no cover
                 raise SystemExit
-        except (GrpcApiError, SystemExit):
+        except (GrpcApiError, SystemExit):  # pragma : no cover
             return None
 
     @pyaedt_function_handler()
