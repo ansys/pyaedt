97dfcab55f811a096ab5b6d2e57db7ff73a816ae
Samuelopez-ansys <samuel.lopez@ansys.com>
2023-12-01 16:18:32 +0100

    Allow µ Allow parameter with and without =

--- pyaedt/circuit.py
+++ pyaedt/circuit.py
@@ -159,6 +159,7 @@ class Circuit(FieldAnalysisCircuit, object):
 
     def _get_number_from_string(self, stringval):
         value = stringval[stringval.find("=") + 1 :].strip().replace("{", "").replace("}", "").replace(",", ".")
+        value = value.replace("µ", "u")
         try:
             float(value)
             return value
@@ -206,8 +207,11 @@ class Circuit(FieldAnalysisCircuit, object):
                 line = line.decode("utf-8")
                 if ".param" in line[:7].lower():
                     try:
-                        ppar = line[7:].split("=")[0]
-                        pval = line[7:].split("=")[1]
+                        param_line = " ".join(line[7:].split())
+                        param_re = re.split("[ =]", param_line)
+
+                        ppar = param_re[0]
+                        pval = param_re[1]
                         self[ppar] = pval
                         xpos = 0.0254
                     except:
@@ -371,7 +375,7 @@ class Circuit(FieldAnalysisCircuit, object):
                         mycomp = self.modeler.schematic.create_voltage_pulse(
                             name, value, [xpos, ypos], use_instance_id_netlist=use_instance
                         )
-                elif fields[0][0] == "K":
+                elif fields[0][0].lower() == "k":
                     value = self._get_number_from_string(fields[3])
                     mycomp = self.modeler.schematic.create_coupling_inductors(
                         name, fields[1], fields[2], value, [xpos, ypos], use_instance_id_netlist=use_instance
