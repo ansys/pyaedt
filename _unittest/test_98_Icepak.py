bf1a275d99c7bdefdbc525ac3232bb0fe05b5157
Lorenzo Vecchietti <58366962+lorenzovecchietti@users.noreply.github.com>
2023-12-15 11:04:03 +0100

    New conducting plate function (#3980)
    * add plate_bc
    
    * add deprecation
    
    * fix docstrings
    
    * improve coverage
    
    * improve coverage
    
    * improve coverage
    
    * improve docstring
    
    * Update pyaedt/icepak.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Update pyaedt/icepak.py
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * improve docstring
    
    ---------
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- _unittest/test_98_Icepak.py
+++ _unittest/test_98_Icepak.py
@@ -1427,3 +1427,31 @@ class TestClass:
             power_law_constant=1.5,
             power_law_exponent="3",
         )
+
+    def test_73_conducting_plate(self):
+        box = self.aedtapp.modeler.create_box([5, 5, 5], [1, 2, 3], "ResistanceBox", "copper")
+        box_face = box.top_face_x
+        assert self.aedtapp.assign_conducting_plate_with_thickness(
+            box_face.id, total_power=1, high_side_rad_material="Steel-oxidised-surface"
+        )
+        assert self.aedtapp.assign_conducting_plate_with_resistance(
+            box_face.id, low_side_rad_material="Steel-oxidised-surface"
+        )
+        self.aedtapp.modeler.create_rectangle(self.aedtapp.PLANE.XY, [0, 0, 0], [10, 20], name="surfPlateTest")
+        assert self.aedtapp.assign_conducting_plate_with_impedance("surfPlateTest")
+        x = [1, 2, 3]
+        y = [3, 4, 5]
+        self.aedtapp.create_dataset1d_design("Test_DataSet_Plate", x, y)
+        assert self.aedtapp.assign_conducting_plate_with_conductance(
+            "surfPlateTest",
+            total_power={
+                "Type": "Temp Dep",
+                "Function": "Piecewise Linear",
+                "Values": "Test_DataSet_Plate",
+            },
+        )
+        try:
+            self.aedtapp.assign_conducting_plate_with_conductance([box_face.id, "surfPlateTest"])
+            assert False
+        except AttributeError:
+            assert True
