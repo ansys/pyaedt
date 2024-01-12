a070bac39ed77b88d0e8846b78bcf8bb732123db
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-01-12 16:53:15 +0100

    improve test

--- _unittest/test_34_TwinBuilder.py
+++ _unittest/test_34_TwinBuilder.py
@@ -130,6 +130,9 @@ class TestClass:
         assert tb.add_q3d_dynamic_component(
             self.dynamic_link, "2D_Extractor_Cable", "MySetupAuto", "sweep1", "Original", model_depth="100mm"
         )
+        assert tb.add_q3d_dynamic_component(self.dynamic_link, "Q3D_MSbend", "Setup1GHz", "MSbX_021GHz", "Original")
+        with pytest.raises(ValueError):
+            tb.add_q3d_dynamic_component(self.dynamic_link, "Q3D_MSbend", "Setup1GHz", "sweep1", "Original")
         assert tb.add_q3d_dynamic_component(
             self.dynamic_link_copy_import,
             "2D_Extractor_Cable",
