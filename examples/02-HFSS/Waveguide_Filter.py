3ee03690b36f58ef3819bb6782f48968af705484
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2024-01-15 15:41:01 +0100

    Fix examples (#4076)
    * Fix material override Mechanical example
    
    * fixed bug in ClearMessages
    
    * fixed bug in ClearMessages
    
    ---------
    
    Co-authored-by: maxcapodi78 <Shark78>

--- examples/02-HFSS/Waveguide_Filter.py
+++ examples/02-HFSS/Waveguide_Filter.py
@@ -90,6 +90,7 @@ else:
     zstart = "l1/2 - t/2"  # Odd number of cavities, even number of irises.
     is_even = False
 
+
 ###############################################################################
 # Draw parametric waveguide filter
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,14 +100,15 @@ else:
 
 def place_iris(zpos, dz, n):
     w_str = "w" + str(n)  # Iris width parameter as a string.
-    this_name = "iris_a_"+str(n)  # Iris object name in the HFSS project.
+    this_name = "iris_a_" + str(n)  # Iris object name in the HFSS project.
     iris = []  # Return a list of the two objects that make up the iris.
     if this_name in hfss.modeler.object_names:
         this_name = this_name.replace("a", "c")
-    iris.append(hfss.modeler.create_box(['-b/2', '-a/2', zpos], ['(b - ' + w_str + ')/2', 'a',  dz],
-                                                    name=this_name, matname="silver"))
+    iris.append(hfss.modeler.create_box(['-b/2', '-a/2', zpos], ['(b - ' + w_str + ')/2', 'a', dz],
+                                        name=this_name, matname="silver"))
     iris.append(iris[0].mirror([0, 0, 0], [1, 0, 0], duplicate=True))
     return iris
+
 
 ###############################################################################
 # Place irises
@@ -164,7 +166,7 @@ count = 0
 ports = []
 for n, z in enumerate(wg_z):
     face_id = hfss.modeler.get_faceid_from_position([0, 0, z], obj_name="waveguide")
-    u_start = [0, hfss.variable_manager["u_start"].evaluated_value,  z]
+    u_start = [0, hfss.variable_manager["u_start"].evaluated_value, z]
     u_end = [0, hfss.variable_manager["u_end"].evaluated_value, z]
 
     ports.append(hfss.wave_port(face_id, integration_line=[u_start, u_end], name="P" + str(n + 1), renormalize=False))
@@ -178,8 +180,8 @@ for n, z in enumerate(wg_z):
 # through the resonant structure while the mesh is refined.
 
 setup = hfss.create_setup("Setup1", setuptype="HFSSDriven",
-                           MultipleAdaptiveFreqsSetup=['9.8GHz', '10.2GHz'],
-                           MaximumPasses=5)
+                          MultipleAdaptiveFreqsSetup=['9.8GHz', '10.2GHz'],
+                          MaximumPasses=5)
 
 setup.create_frequency_sweep(
     unit="GHz",
@@ -187,8 +189,7 @@ setup.create_frequency_sweep(
     freqstart=9.5,
     freqstop=10.5,
     sweep_type="Interpolating",
-    )
-
+)
 
 #################################################################################
 #  Solve the project with two tasks.
@@ -217,13 +218,13 @@ plt = solution.plot(solution.expressions)  # Matplotlib axes object.
 #  The following command generates a field plot in HFSS and uses PyVista
 #  to plot the field in Jupyter.
 
-plot=hfss.post.plot_field(quantity="Mag_E",
-                          objects_list=["Global:XZ"],
-                          plot_type="CutPlane",
-                          setup_name=hfss.nominal_adaptive,
-                          intrinsics={"Freq":"9.8GHz", "Phase":"0deg"},
-                          export_path=hfss.working_directory,
-                          show=False)
+plot = hfss.post.plot_field(quantity="Mag_E",
+                            object_list=["Global:XZ"],
+                            plot_type="CutPlane",
+                            setup_name=hfss.nominal_adaptive,
+                            intrinsics={"Freq": "9.8GHz", "Phase": "0deg"},
+                            export_path=hfss.working_directory,
+                            show=False)
 
 ###############################################################################
 # Save and close the desktop
