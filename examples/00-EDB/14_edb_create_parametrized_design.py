b656357c3373e7f2538d8effa666ca145c7f5665
Devin <devin.crawford@ansys.com>
2024-01-21 09:00:31 -0500

    Update Example
    14_edb_create_component.py

--- examples/00-EDB/14_edb_create_parametrized_design.py
+++ examples/00-EDB/14_edb_create_parametrized_design.py
@@ -6,35 +6,35 @@ This example shows how to
 2, Create automatically parametrized design.
 """
 ######################################################################
+# The following layout will be created in this example
 #
-# Final expected project
-# ~~~~~~~~~~~~~~~~~~~~~~
+# <img src="_static\parameterized_design.png" width="600">
 #
-# .. image:: ../../_static/parametrized_design.png
-#  :width: 600
-#  :alt: Fully automated parametrization.
-######################################################################
+#
 
 ######################################################################
-# Create HFSS simulatio project
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Load an existing EDB folder.
-######################################################################
+# Create HFSS simulation project from an existing layout.
 
 import os
 import pyaedt
+import tempfile
 
-project_path = pyaedt.generate_unique_folder_name()
-target_aedb = pyaedt.downloads.download_file('edb/ANSYS-HSD_V1.aedb', destination=project_path)
-print("Project folder will be", target_aedb)
+temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
+target_aedb = pyaedt.downloads.download_file('edb/ANSYS-HSD_V1.aedb', destination=temp_dir.name)
+print("Project will be located in ", target_aedb)
 
 aedt_version = "2023.2"
 edb = pyaedt.Edb(edbpath=target_aedb, edbversion=aedt_version)
 print("EDB is located at {}".format(target_aedb))
 
 ########################################################################
-# Create SimulationConfiguration object and define simulation parameters
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ### Prepare the Layout for Simulation
+#
+# The ``new_simulation_configuration()`` method creates an instance of 
+# the ``SimulationConfiguration`` class. This class helps define all pre-processing steps
+# required to set up the PCB for simulation. After the simulation configuration has been defined, 
+# they are applied to the EDB using the method
+# ``Edb.build_simulation()``.
 
 simulation_configuration = edb.new_simulation_configuration()
 simulation_configuration.signal_nets = ["PCIe_Gen4_RX0_P", "PCIe_Gen4_RX0_N",
@@ -47,23 +47,59 @@ simulation_configuration.stop_freq = "20GHz"
 simulation_configuration.step_freq = "10MHz"
 
 ##########################
-# Build simulation project
-# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Now apply the simulation setup to the EDB.
 
 edb.build_simulation_project(simulation_configuration)
 
 #############################
-# Generated design parameters
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ### Parameterization
+#
+# The layout can automatically be set up to enable parametric studies. For example, the
+# impact of antipad diameter or trace width on signal integrity performance may be invested parametrically.
 
 edb.auto_parametrize_design(layers=True, materials=True, via_holes=True, pads=True, antipads=True, traces=True)
 edb.save_edb()
 edb.close_edb()
 
 ######################
-# Open project in AEDT
-# ~~~~~~~~~~~~~~~~~~~~
+# ## Open project in AEDT
+#
+# All manipulations thus far have been executed using the EDB API which provides fast, streamlined processing of
+# layout data in non-graphical mode. The layout and simulation setup can be visualized by opening it using the
+# 3D Layout editor in Electronics Desktop.
+#
+# Note that there may be some delay while AEDT is being launched.
 
-# Uncomment the following line to open the design in HFSS 3D Layout
-# hfss = pyaedt.Hfss3dLayout(projectname=target_aedb, specified_version=aedt_version, new_desktop_session=True)
-# hfss.release_desktop()
+hfss = pyaedt.Hfss3dLayout(projectname=target_aedb, 
+                           specified_version=aedt_version, 
+                           non_graphical=False,
+                           new_desktop_session=True)
+
+###############################################################################
+# The following cell can be used to ensure that the design is valid for simulation.
+
+validation_info = hfss.validate_full_design()
+is_ready_to_simulate = True
+
+for s in validation_info[0]:
+    if "error" in s:
+        print(s)
+        is_ready_to_simulate = False
+
+if is_ready_to_simulate:
+    print("The model is ready for simulation.")
+else:
+    print("There are errors in the model that need to be fixed.")
+
+###############################################################################
+# ### Release the application from the Python kernel
+#
+# It is important to release the application from the Python kernel after 
+# execution of the script. The default behavior of the ``release_desktop()`` method closes all open
+# projects and closes the application.
+#
+# If you want to conintue working on the project in graphical mode
+# after script execution, call the following method with both arguments set to ``False``.
+
+hfss.release_desktop(close_projects=True, close_desktop=True)
+temp_dir.cleanup()  # Remove the temporary folder and files. All data will be removd!
