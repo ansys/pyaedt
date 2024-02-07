0032bb40262df959ed6475c2d775bd5ac5513833
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2024-02-07 13:30:03 +0100

    Move virtual compliance and pdf test to _unittest_solvers (#4195)

--- _unittest_solvers/test_01_pdf.py
+++ _unittest_solvers/test_01_pdf.py
@@ -1,7 +1,7 @@
 import os
 
-from _unittest.conftest import desktop_version
-from _unittest.conftest import local_path
+from _unittest_solvers.conftest import desktop_version
+from _unittest_solvers.conftest import local_path
 import pytest
 
 from pyaedt import Circuit
@@ -10,17 +10,12 @@ from pyaedt.generic.pdf import AnsysReport
 
 tol = 1e-12
 test_project_name = "ANSYS-HSD_V1_0_test"
-test_subfolder = "compliance"
-
-
-@pytest.fixture(scope="module", autouse=True)
-def desktop():
-    return
+test_subfolder = "T01"
 
 
 @pytest.fixture(scope="class")
 def aedtapp(add_app):
-    app = add_app(test_project_name, application=Circuit, subfolder=test_subfolder)
+    app = add_app(test_project_name, application=Circuit, subfolder=os.path.join(test_subfolder, "compliance"))
     return app
 
 
@@ -41,10 +36,10 @@ class TestClass(object):
         report.add_chapter("Chapter 1")
         report.add_sub_chapter("C1")
         report.add_text("ciao")
-        report.add_text("ciao2", True, True)
+        report.add_text("hola", True, True)
         report.add_empty_line(2)
         report.add_page_break()
-        report.add_image(os.path.join(local_path, "example_models", "Coax_HFSS.jpg"), "Coaxial Cable")
+        report.add_image(os.path.join(local_path, "example_models", test_subfolder, "Coax_HFSS.jpg"), "Coaxial Cable")
         report.add_section(portrait=False, page_format="a3")
         report.add_table("MyTable", [["x", "y"], ["0", "1"], ["2", "3"], ["10", "20"]])
         report.add_section()
@@ -53,19 +48,23 @@ class TestClass(object):
         assert os.path.exists(report.save_pdf(local_scratch.path, "my_firstpdf.pdf"))
 
     def test_virtual_compliance(self, local_scratch, aedtapp):
-        template = os.path.join(local_path, "example_models", test_subfolder, "general_compliance_template.json")
+        template = os.path.join(local_path, "example_models", test_subfolder, "compliance",
+                                "general_compliance_template.json")
         template = local_scratch.copyfile(template)
         local_scratch.copyfile(
-            os.path.join(local_path, "example_models", test_subfolder, "ContourEyeDiagram_Custom.json")
+            os.path.join(local_path, "example_models", test_subfolder, "compliance", "ContourEyeDiagram_Custom.json")
         )
-        local_scratch.copyfile(os.path.join(local_path, "example_models", test_subfolder, "Sparameter_Custom.json"))
+        local_scratch.copyfile(os.path.join(local_path, "example_models", test_subfolder, "compliance",
+                                            "Sparameter_Custom.json"))
         local_scratch.copyfile(
-            os.path.join(local_path, "example_models", test_subfolder, "Sparameter_Insertion_Custom.json")
+            os.path.join(local_path, "example_models", test_subfolder, "compliance", "Sparameter_Insertion_Custom.json")
         )
         local_scratch.copyfile(
-            os.path.join(local_path, "example_models", test_subfolder, "StatisticalEyeDiagram_Custom.json")
+            os.path.join(local_path, "example_models", test_subfolder, "compliance",
+                         "StatisticalEyeDiagram_Custom.json")
         )
-        local_scratch.copyfile(os.path.join(local_path, "example_models", test_subfolder, "EyeDiagram_Custom.json"))
+        local_scratch.copyfile(os.path.join(local_path, "example_models", test_subfolder, "compliance",
+                                            "EyeDiagram_Custom.json"))
 
         import json
 
