66b547d1c29ecb4d6ba1a4a3f2132fce67b8d9c4
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2024-02-08 16:51:39 +0100

    Update pyaedt/generic/configurations.py
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- pyaedt/generic/configurations.py
+++ pyaedt/generic/configurations.py
@@ -1431,7 +1431,7 @@ class Configurations(object):
     @pyaedt_function_handler()
     def export_config(self, config_file=None, overwrite=False):
         """Export current design properties to json or toml file.
-        The section to be exported are defined with ``configuration.options`` class.
+        The sections to be exported are defined with ``configuration.options`` class.
 
 
         Parameters
