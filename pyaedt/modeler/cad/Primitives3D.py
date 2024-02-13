56e9c15e19cfadc8ec6b741d5a881461e968f088
Sebastien Morais <sebastien.morais@ansys.com>
2024-02-13 11:12:12 +0100

    MISC: format docstring

--- pyaedt/modeler/cad/Primitives3D.py
+++ pyaedt/modeler/cad/Primitives3D.py
@@ -1662,6 +1662,7 @@ class Primitives3D(GeometryModeler):
             Coordinate system to import. The default is all available coordinate systems.
         reference_coordinate_system : str, optional
             Coordinate system to use as reference. The default is ``Global``.
+
         Returns
         -------
         :class:`pyaedt.modeler.components_3d.UserDefinedComponent`
