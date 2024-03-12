dd80d063cbe4827ba758eb19322ba1a172e1a4ad
Sebastien Morais <sebastien.morais@ansys.com>
2024-03-12 14:06:27 +0100

    MISC: Improve Makefile and make.bat
    Changes include:
    - reworking the way HTML pages are created (with/without examples)
    - adding clean target
    - refact pdf target

--- doc/make.bat
+++ doc/make.bat
@@ -4,13 +4,21 @@ pushd %~dp0
 
 REM Command file for Sphinx documentation
 
+if "%SPHINXOPTS%" == "" (
+	set SPHINXOPTS=-j auto --color -w build_errors.txt
+)
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
 set BUILDDIR=_build
+set LINKCHECKDIR=\%BUILDDIR%\linkcheck
+set LINKCHECKOPTS=-d %BUILDDIR%\.doctrees -W --keep-going --color
 
 if "%1" == "" goto help
+if "%1" == "clean" goto clean
+if "%1" == "html-noexamples" goto html-noexamples
+if "%1" == "html" goto html
 if "%1" == "pdf" goto pdf
 
 %SPHINXBUILD% >NUL 2>NUL
@@ -31,12 +39,45 @@ goto end
 
 :help
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:clean
+echo Cleaning everything
+rmdir /s /q %SOURCEDIR%\examples > /NUL 2>&1 
+rmdir /s /q %BUILDDIR% > /NUL 2>&1 
+for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
+goto end
+
+:html-noexamples:
+echo Building HTML pages without running examples
+set PYAEDT_DOC_RUN_EXAMPLES=0
+if not exist "source\examples" mkdir "source\examples"  
+echo Examples> source\examples\index.rst  
+echo ========> source\examples\index.rst  
+::FIXME: currently linkcheck freezes and further investigation must be performed
+::%SPHINXBUILD% -M linkcheck %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %LINKCHECKOPTS% %O%
+%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+echo
+echo "Build finished. The HTML pages are in %BUILDDIR%."
+goto end
+
+:html:
+echo Building HTML pages with running examples
+set PYAEDT_DOC_RUN_EXAMPLES=1
+::FIXME: currently linkcheck freezes and further investigation must be performed
+::%SPHINXBUILD% -M linkcheck %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %LINKCHECKOPTS% %O%
+%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+echo
+echo "Build finished. The HTML pages are in %BUILDDIR%."
+goto end
 
 :pdf
 %SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 cd "%BUILDDIR%\latex"
 for %%f in (*.tex) do (
 pdflatex "%%f" --interaction=nonstopmode)
+echo "Build finished. The PDF pages are in %BUILDDIR%."
+goto end
 
 :end
 popd
