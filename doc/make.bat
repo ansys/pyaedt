ed2b2fbad47c466a35dc6b531057f0faf9c63cef
Sebastien Morais <sebastien.morais@ansys.com>
2024-04-21 18:21:14 +0200

    DOC: Fix make.bat target

--- doc/make.bat
+++ doc/make.bat
@@ -88,7 +88,7 @@ echo Examples> source\examples\index.rst
 echo ========> source\examples\index.rst
 ::FIXME: currently linkcheck freezes and further investigation must be performed
 ::%SPHINXBUILD% -M linkcheck %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %LINKCHECKOPTS% %O%
-%SPHINXBUILD% -M html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M latex %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 echo
 echo "Build finished. The HTML pages are in %BUILDDIR%."
 goto end
