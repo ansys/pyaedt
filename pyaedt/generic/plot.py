15fe8d0d65db1d1a3dc4e4433f09389f51c8f026
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-05-08 12:50:06 +0200

    Replace logger with warning

--- pyaedt/generic/plot.py
+++ pyaedt/generic/plot.py
@@ -124,7 +124,7 @@ def is_float(istring):
     try:
         return float(istring.strip())
     except Exception:
-        settings.logger.error("Unable to convert '" + isring.strip() + "' to a float.")
+        warnings.warn("Unable to convert '" + isring.strip() + "' to a float.")
         return 0
 
 
