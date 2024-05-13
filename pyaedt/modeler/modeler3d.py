ac7e902537264b1fe8671093c2fc7a2b4410e917
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-05-13 11:09:49 +0200

    Add nosec

--- pyaedt/modeler/modeler3d.py
+++ pyaedt/modeler/modeler3d.py
@@ -985,7 +985,7 @@ class Modeler3D(Primitives3D):
                     if line_type == "GRID*":
                         try:
                             nas_to_dict["Points"][grid_id] = [float(n1), float(n2), float(n3)]
-                        except Exception:
+                        except Exception:  # nosec
                             continue
                         nas_to_dict["PointsId"][grid_id] = id
                         id += 1
