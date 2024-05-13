5cd6970158be9d64667c062a9c3b264793d0d755
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2024-05-13 13:03:48 +0200

    Cherry pick

--- _unittest/test_21_Circuit.py
+++ _unittest/test_21_Circuit.py
@@ -898,3 +898,18 @@ class TestClass:
             design_name="AMI",
         )
         assert result
+
+    def test_50_enforce_touchstone_passive(self):
+        self.aedtapp.insert_design("Touchstone_passive")
+        self.aedtapp.modeler.schematic_units = "mil"
+        s_parameter_component = self.aedtapp.modeler.schematic.create_touchstone_component(self.touchstone_file)
+        assert s_parameter_component.enforce_touchstone_model_passive()
+        nexxim_customization = s_parameter_component.model_data.props["NexximCustomization"]
+        assert -1 == nexxim_customization["DCOption"]
+        assert 1 == nexxim_customization["InterpOption"]
+        assert 3 == nexxim_customization["ExtrapOption"]
+        assert 0 == nexxim_customization["Convolution"]
+        assert 6 == nexxim_customization["Passivity"]
+        assert not nexxim_customization["Reciprocal"]
+        assert "" == nexxim_customization["ModelOption"]
+        assert 2 == nexxim_customization["DataType"]
