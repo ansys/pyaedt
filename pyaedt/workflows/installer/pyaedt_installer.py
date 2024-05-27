cfcffaf1b5271c4ba17d1308e601bfacc5df6c0f
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-05-27 15:00:30 +0200

    Fix typo

--- pyaedt/workflows/installer/pyaedt_installer.py
+++ pyaedt/workflows/installer/pyaedt_installer.py
@@ -60,7 +60,7 @@ def add_pyaedt_to_aedt(
     os.makedirs(extensions_dir, exist_ok=True)
 
     templates_dir = os.path.dirname(pyaedt.workflows.templates.__file__)
-    script_file = os.path.join(templates_dir, "pyaedt_utils.py.py")
+    script_file = os.path.join(templates_dir, "pyaedt_utils.py")
     dest_script_path = os.path.join(extensions_dir, "pyaedt_utils.py.py")
     shutil.copy2(script_file, dest_script_path)
 
