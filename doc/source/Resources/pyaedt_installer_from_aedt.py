a57fd2facc5c0d1279d664a2f4954c2209a69bb3
Sebastien Morais <sebastien.morais@ansys.com>
2024-05-30 13:36:57 +0200

    REFACTOR: Use installer target in script

--- doc/source/Resources/pyaedt_installer_from_aedt.py
+++ doc/source/Resources/pyaedt_installer_from_aedt.py
@@ -213,10 +213,7 @@ def install_pyaedt():
             run_command('"{}" --default-timeout=1000 install wheel'.format(pip_exe))
             # run_command(
             # '"{}" --default-timeout=1000 install git+https://github.com/ansys/pyaedt.git@main'.format(pip_exe))
-            run_command('"{}" --default-timeout=1000 install pyaedt[all]'.format(pip_exe))
-            run_command('"{}" --default-timeout=1000 install jupyterlab'.format(pip_exe))
-            run_command('"{}" --default-timeout=1000 install ipython -U'.format(pip_exe))
-            run_command('"{}" --default-timeout=1000 install ipyvtklink'.format(pip_exe))
+            run_command('"{}" --default-timeout=1000 install pyaedt[installer]'.format(pip_exe))
 
         if args.version == "231":
             run_command('"{}" uninstall -y pywin32'.format(pip_exe))
