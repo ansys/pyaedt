5ca09d2cd3ca68b5be19010170ee2ff323bf46eb
Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>
2024-06-05 15:38:23 +0200

    REFACTOR: Encryption password handling (#4771)

--- pyaedt/application/Analysis3D.py
+++ pyaedt/application/Analysis3D.py
@@ -1023,7 +1023,7 @@ class FieldAnalysis3D(Analysis, object):
         bool
             ``True`` when successful, ``False`` when failed.
         """
-        if not password:
+        if password is None:
             password = os.getenv("PYAEDT_ENCRYPTED_PASSWORD", "")
         native_comp_names = [nc.component_name for _, nc in self.native_components.items()]
         if not components:
