43f14e894119828ca1d6199a8517f6541f05927d
Sebastien Morais <sebastien.morais@ansys.com>
2024-06-25 17:14:49 +0200

    REFACTOR: Add futur warning

--- src/pyaedt/__init__.py
+++ src/pyaedt/__init__.py
@@ -2,3 +2,30 @@ from ansys.aedt.api import __version__
 from ansys.aedt.api import *
 
 __version__ = __version__
+
+import warnings
+
+WARNING_MESSAGE = """Module 'pyedt' has become an alias to the new package structure.
+Please update you imports to use the new architecture based on 'ansys.aedt.core'.
+The old structure will be deprecated in future versions, see ...
+"""
+
+def alias_deprecation_warning():
+    """Warning message informing users of architecture deprecation."""
+    # Store warnings showwarning
+    existing_showwarning = warnings.showwarning
+
+    # Define and use custom showwarning
+    def custom_show_warning(message, category, filename, lineno, file=None, line=None):
+        """Custom warning used to remove <stdin>:loc: pattern."""
+        print("{}: {}".format(category.__name__, message))
+
+    warnings.showwarning = custom_show_warning
+
+    warnings.warn(WARNING_MESSAGE, FutureWarning)
+
+    # Restore warnings showwarning
+    warnings.showwarning = existing_showwarning
+
+
+alias_deprecation_warning()
