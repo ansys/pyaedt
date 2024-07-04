f3af3f94c88757bea2a6b4b4a17eb4585955fa98
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2024-07-04 08:05:32 +0200

    Apply suggestions from code review
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>

--- examples/07-Circuit/Reports.py
+++ examples/07-Circuit/Reports.py
@@ -87,10 +87,9 @@ Image(out)
 ###############################################################################
 # Create transient report
 # ~~~~~~~~~~~~~~~~~~~~~~~
-# Property dictionary can be customized in any aspect and new report can be created easily.
-# In this example the curve name is customized.
-# Expressions key can be a list of expressions or a dictionary containing expression to plot and line properties.
-# For example:
+# You can customize any aspect of the property dictionary and easily create a new report.
+# The following code customizes the curve name.
+# The expressions key can be a list of expressions as follows or a dictionary containing the expressions to plot and line properties.
 # props["expressions"] = { "V(Battery)" :
 #                           {"color": [0, 255, 0], "trace_style": "Solid", "width": 1, "trace_type": "Continuous"}}
 
