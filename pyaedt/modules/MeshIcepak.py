a020f20bcdbdbc9aab5eab1fbf1f9fa905b4010c
Lorenzo Vecchietti <lorenzo.vecchietti@ansys.com>
2024-07-04 11:40:34 +0200

    exclude from test cov stats, lines for version 23R2 or lower

--- pyaedt/modules/MeshIcepak.py
+++ pyaedt/modules/MeshIcepak.py
@@ -1104,7 +1104,7 @@ class IcepakMesh(object):
                                     "Icepak",
                                 )
                             )
-            else:
+            else:  # pragma : no cover
                 for ds in dp["MeshRegion"]["MeshSetup"]["MeshOperations"]:
                     if isinstance(
                         dp["MeshRegion"]["MeshSetup"]["MeshOperations"][ds],
@@ -1147,7 +1147,7 @@ class IcepakMesh(object):
                                 if el in meshop.__dict__:
                                     meshop.__dict__[el] = dict_prop[el]
                             meshops.append(meshop)
-            else:
+            else:  # pragma : no cover
                 for ds in dp["MeshRegion"]["MeshSetup"]["MeshRegions"]:
                     if isinstance(dp["MeshRegion"]["MeshSetup"]["MeshRegions"][ds], (OrderedDict, dict)):
                         dict_prop = dp["MeshRegion"]["MeshSetup"]["MeshRegions"][ds]
