8d44e6d0c6d7f2333dbe5a3a1064a5e420991aff
Massimo Capodiferro <77293250+maxcapodi78@users.noreply.github.com>
2024-07-19 13:51:32 +0200

    Update pyaedt/circuit.py
    Co-authored-by: gmalinve <103059376+gmalinve@users.noreply.github.com>

--- pyaedt/circuit.py
+++ pyaedt/circuit.py
@@ -2145,7 +2145,7 @@ class Circuit(FieldAnalysisCircuit, ScatteringMethods):
         rx_buffer_name : str
             Receiver buffer name.
         tx_pins : list
-            Pins to assign the transmitter IBIS.
+            Pins to assign to the transmitter IBIS.
         rx_pins : list, optional
             Pins to assign the receiver IBIS.
         tx_refs : list, optional
