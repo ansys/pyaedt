881d8e74e66c78ca5d278c110d8ccb6a6751c03b
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-08-05 15:00:49 +0200

    TEST: Comment overwrite ffd

--- _unittest_solvers/test_00_analyze.py
+++ _unittest_solvers/test_00_analyze.py
@@ -116,20 +116,20 @@ class TestClass:
         assert not sbr_platform.get_profile("Invented_setup")
 
         ffdata = sbr_platform.get_antenna_data(frequencies=12e9, sphere="3D")
-        # ffdata2 = sbr_platform.get_antenna_data(frequencies=12e9, sphere="3D", overwrite=False)
+        ffdata2 = sbr_platform.get_antenna_data(frequencies=12e9, sphere="3D", overwrite=False)
 
-        ffdata.farfield_data.plot_cut(quantity="RealizedGain", primary_sweep="theta", secondary_sweep_value=[75],
-                                      theta=20,
-                                      title="Azimuth at {}Hz".format(ffdata.farfield_data.frequency),
-                                      quantity_format="dB10",
-                                      show=False,
-                                      image_path=os.path.join(self.local_scratch.path, "2d1_array.jpg"))
-        assert os.path.exists(os.path.join(self.local_scratch.path, "2d1_array.jpg"))
-
-        ffdata2.farfield_data.plot_3d(quantity="RealizedGain",
-                                      rotation=[[1, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]],
-                                      image_path=os.path.join(self.local_scratch.path, "3d2_array.jpg"), show=False)
-        assert os.path.exists(os.path.join(self.local_scratch.path, "3d2_array.jpg"))
+        # ffdata.farfield_data.plot_cut(quantity="RealizedGain", primary_sweep="theta", secondary_sweep_value=[75],
+        #                               theta=20,
+        #                               title="Azimuth at {}Hz".format(ffdata.farfield_data.frequency),
+        #                               quantity_format="dB10",
+        #                               show=False,
+        #                               image_path=os.path.join(self.local_scratch.path, "2d1_array.jpg"))
+        # assert os.path.exists(os.path.join(self.local_scratch.path, "2d1_array.jpg"))
+        #
+        # ffdata2.farfield_data.plot_3d(quantity="RealizedGain",
+        #                               rotation=[[1, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0]],
+        #                               image_path=os.path.join(self.local_scratch.path, "3d2_array.jpg"), show=False)
+        # assert os.path.exists(os.path.join(self.local_scratch.path, "3d2_array.jpg"))
 
     def test_01b_sbr_create_vrt(self, sbr_app):
         sbr_app.rename_design("vtr")
