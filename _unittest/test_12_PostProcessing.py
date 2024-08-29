13e8c3b5913a582b61a7a855ceb53f8356d4de4b
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-08-29 10:57:49 +0200

    Add UT

--- _unittest/test_12_PostProcessing.py
+++ _unittest/test_12_PostProcessing.py
@@ -194,15 +194,18 @@ class TestClass:
         )
         new_report4.report_type = "Data Table"
         assert new_report4.create()
+
+        local_path = os.path.dirname(os.path.realpath(__file__))
+        template = os.path.join(local_path, "example_models", test_subfolder, "template.rpt")
         if not config["NonGraphical"]:
-            local_path = os.path.dirname(os.path.realpath(__file__))
-            template = os.path.join(local_path, "example_models", test_subfolder, "template.rpt")
             assert new_report4.apply_report_template(template)
-            template = os.path.join(local_path, "example_models", test_subfolder, "template_invented.rpt")
-            assert not new_report4.apply_report_template(template)
-            template = os.path.join(local_path, "example_models", test_subfolder, "template.csv")
-            assert not new_report4.apply_report_template(template)
-            assert not new_report4.apply_report_template(template, property_type="Dummy")
+            template2 = os.path.join(local_path, "example_models", test_subfolder, "template_invented.rpt")
+            assert not new_report4.apply_report_template(template2)
+            template3 = os.path.join(local_path, "example_models", test_subfolder, "template.csv")
+            assert not new_report4.apply_report_template(template3)
+            assert not new_report4.apply_report_template(template3, property_type="Dummy")
+
+        assert field_test.post.create_report_from_configuration(template)
 
     def test_09_manipulate_report_C(self, field_test):
         variations = field_test.available_variations.nominal_w_values_dict
