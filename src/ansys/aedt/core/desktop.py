686cff1cb894094d290b37e06b39763a3ca4ddc8
Sebastien Morais <sebastien.morais@ansys.com>
2024-09-26 15:45:12 +0200

    FIX: Call to closed/deleted log handler

--- src/ansys/aedt/core/desktop.py
+++ src/ansys/aedt/core/desktop.py
@@ -1685,7 +1685,6 @@ class Desktop(object):
 
         """
         if self.__closed is True:  # pragma: no cover
-            self.logger.debug("Connection is already closed. Ignoring request.")
             return
 
         return self.release_desktop(close_projects=True, close_on_exit=True)
