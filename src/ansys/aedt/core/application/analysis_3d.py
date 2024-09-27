f9a391a89f6eae2b5f2f2f98925ae019cd741199
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-09-27 17:03:36 +0200

    Fix flatten

--- src/ansys/aedt/core/application/analysis_3d.py
+++ src/ansys/aedt/core/application/analysis_3d.py
@@ -1089,7 +1089,7 @@ class FieldAnalysis3D(Analysis, object):
         """
         if password is None:
             password = os.getenv("PYAEDT_ENCRYPTED_PASSWORD", "")
-        native_comp_names = [nc for nc in self.native_components.keys()]
+        native_comp_names = [nc.definition_name for nc in self.native_components.values()]
         if not components:
             components = [
                 key
