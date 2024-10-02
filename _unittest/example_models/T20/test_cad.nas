153b13bec36dd52ded483f62a69c0b6242281d54
ring630 <@gmail.com>
2024-10-02 12:30:08 +0200

    test

--- _unittest/example_models/T20/test_cad.nas
+++
@@ -1,5 +0,0 @@
-CEND
-BEGIN BULK
-INCLUDE 'assembly1.key'
-INCLUDE 'assembly2.key'
-ENDDATA
--- _unittest/example_models/TMaxwell/timestep_only.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    ctl = open("user.ctl", "w")
-    ctl.write("begin_data\n")
-    ctl.write("timeStep ")
-    timeStep = 5e-4
-    ctl.write("%e\n" % timeStep)
-    ctl.write("end_data\n")
-    ctl.close()
