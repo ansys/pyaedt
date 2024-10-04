c2db213f8d3a7a3d6e6d4c998e0ac635f2a2fe8a
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-10-04 09:28:01 +0200

    fix export fields

--- src/ansys/aedt/core/visualization/post/post_common_3d.py
+++ src/ansys/aedt/core/visualization/post/post_common_3d.py
@@ -490,7 +490,7 @@ class PostProcessor3D(PostProcessorCommon):
             self.ofieldsreporter.CalcOp(scalar_function)
 
         if not variations:
-            variations = self._app.available_variations.nominal_w_values_dict_w_dependent
+            variations = self._app.available_variations.nominal_w_values_dict
 
         variation = []
         for el, value in variations.items():
@@ -671,7 +671,7 @@ class PostProcessor3D(PostProcessorCommon):
             return False
 
         if not variations:
-            variations = self._app.available_variations.nominal_w_values_dict_w_dependent
+            variations = self._app.available_variations.nominal_w_values_dict
 
         variation = []
         for el, value in variations.items():
@@ -840,7 +840,7 @@ class PostProcessor3D(PostProcessorCommon):
             self.ofieldsreporter.CopyNamedExprToStack(quantity)
 
         if not variations:
-            variations = self._app.available_variations.nominal_w_values_dict_w_dependent
+            variations = self._app.available_variations.nominal_w_values_dict
 
         variation = []
         for el, value in variations.items():
