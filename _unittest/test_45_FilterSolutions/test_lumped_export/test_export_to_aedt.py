7528ace1c460019d1c6b4ed7a5faccf48f45544e
raghajaf <ramin.aghajafari@ansys.com>
2024-10-09 10:59:00 -0400

    FIX:_FilterSolutions_unittest_typo_update

--- _unittest/test_45_FilterSolutions/test_lumped_export/test_export_to_aedt.py
+++ _unittest/test_45_FilterSolutions/test_lumped_export/test_export_to_aedt.py
@@ -147,7 +147,7 @@ class TestClass:
             lumpdesign.export_to_aedt.modelithics_capacitor_list_count
         assert info.value.args[0] == "The part library is not set to Modelithics"
         lumpdesign.export_to_aedt.part_libraries = PartLibraries.MODELITHICS
-        assert lumpdesign.export_to_aedt.modelithics_capacitor_list_count == 143
+        assert lumpdesign.export_to_aedt.modelithics_capacitor_list_count == 140
 
     def test_modelithics_capacitor_list(self):
         lumpdesign = ansys.aedt.core.FilterSolutions(implementation_type=FilterImplementation.LUMPED)
@@ -450,7 +450,7 @@ class TestClass:
         lumpdesign = ansys.aedt.core.FilterSolutions(implementation_type=FilterImplementation.LUMPED)
         lumpdesign.export_to_aedt._open_aedt_export()
         lumpdesign.export_to_aedt.load_library_parts_config(resource_path("library_parts.cfg"))
-        assert lumpdesign.export_to_aedt.part_libraries == PartLibraries.MODELITHICS
+        lumpdesign.export_to_aedt.part_libraries == PartLibraries.MODELITHICS
         assert lumpdesign.export_to_aedt.substrate_er == "4.5"
         assert lumpdesign.export_to_aedt.substrate_resistivity == "5.8E+07 "
         assert lumpdesign.export_to_aedt.substrate_conductor_thickness == "500 nm"
