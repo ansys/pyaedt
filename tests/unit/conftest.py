bf4c4da0c789fa41bf200c0a13f993857dbd2207
Sebastien Morais <sebastien.morais@ansys.com>
2024-10-20 18:40:16 +0200

    TESTS: Add correct marker

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,4 +4,4 @@ import pytest
 def pytest_collection_modifyitems(config: pytest.Config, items: List[pytest.Item]):
     """Hook used to apply marker on unit tests."""
     for item in items:
-        item.add_marker(pytest.mark.system)
+        item.add_marker(pytest.mark.unit)
