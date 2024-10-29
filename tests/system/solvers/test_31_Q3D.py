3429084ec3712d91d612684fae1d2ef74e877da3
Sebastien Morais <sebastien.morais@ansys.com>
2024-10-29 09:42:35 +0100

    TEST: Add forgotten import

--- tests/system/solvers/test_31_Q3D.py
+++ tests/system/solvers/test_31_Q3D.py
@@ -27,6 +27,7 @@ import os
 from ansys.aedt.core import Q3d
 import pytest
 
+from tests import TESTS_SOLVERS_PATH
 from tests.system.solvers.conftest import desktop_version
 
 test_project_name = "coax_Q3D"
