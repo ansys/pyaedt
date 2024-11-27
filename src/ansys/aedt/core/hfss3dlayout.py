58872b5336eb48b600dfe08e5f7b35bfd3561cb1
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-11-27 10:40:30 +0100

    Add icon

--- src/ansys/aedt/core/hfss3dlayout.py
+++ src/ansys/aedt/core/hfss3dlayout.py
@@ -2292,7 +2292,7 @@ class Hfss3dLayout(FieldAnalysis3DLayout, ScatteringMethods):
                 self.oboundary.EditExcitations(
                     [
                         "NAME:Excitations",
-                        [source, f"pwl({ds_name_mag}, Freq)", f"pwl({ds_name_phase}, Freq)"],
+                        [source, f"pwl({ds_name_phase}, Freq)", f"pwl({ds_name_mag}, Freq)"],
                     ],
                     ["NAME:Terminations", [source, False, str(impedance) + "ohm", "0ohm"]],
                     ",".join(str_val),
