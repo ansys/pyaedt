d9b98a7560350cf137549eb338dc67137e933f75
Sebastien Morais <sebastien.morais@ansys.com>
2024-12-09 11:15:52 +0100

    TESTS: Extend testing on settings

--- tests/system/general/test_utils.py
+++ tests/system/general/test_utils.py
@@ -31,23 +31,31 @@ from pathlib import Path
 from ansys.aedt.core.generic.settings import Settings
 
 
-def test_settings_load_default_yaml(tmp_path):
+def test_settings_load_default_yaml():
     """Test loading the default YAML file in docs/source/Resources."""
     default_settings = Settings()
 
-    settings = Settings()
+    local_settings = Settings()
     project_root = Path(__file__).resolve().parents[3]
     pyaedt_settings_path = project_root / "doc" / "source" / "Resources" / "pyaedt_settings.yaml"
-    settings.load_yaml_configuration(str(pyaedt_settings_path))
+    local_settings.load_yaml_configuration(str(pyaedt_settings_path))
 
     # Compare except for keys where it does not make sense, e.g. log filename, time_tick
     default_settings_attributes = default_settings.__dict__
     del default_settings_attributes["_Settings__global_log_file_name"]
     del default_settings_attributes["_Settings__time_tick"]
-    if os.name == "posix":
-        del default_settings_attributes["_Settings__aedt_environment_variables"]["ANS_NODEPCHECK"]
-    settings_attributes = settings.__dict__
-    del settings_attributes["_Settings__global_log_file_name"]
-    del settings_attributes["_Settings__time_tick"]
+    local_settings_attributes = local_settings.__dict__
+    del local_settings_attributes["_Settings__global_log_file_name"]
+    del local_settings_attributes["_Settings__time_tick"]
+    if os.name != "posix":
+        del local_settings_attributes["_Settings__aedt_environment_variables"]["ANS_NODEPCHECK"]
 
-    assert default_settings_attributes == settings_attributes
+    assert default_settings_attributes == local_settings_attributes
+
+
+def test_settings_writte_default_yaml(tmp_path):
+    default_settings = Settings()
+    path = str("pyaedt_settings.yaml")
+    default_settings.writte_yaml_configuration(path)
+
+    assert os.path.exists(path)
