933286dcd90339f50476fc64f51c1f6e04724ee9
Devin <devin.crawford@ansys.com>
2025-01-07 10:11:47 +0100

    Fix error in Hfss.get_antenna_data
    When numeric frequency values were passed, they were
    not corrected to consider frequency units.

--- src/ansys/aedt/core/hfss.py
+++ src/ansys/aedt/core/hfss.py
@@ -5850,15 +5850,16 @@ class Hfss(FieldAnalysis3D, ScatteringMethods):
             )
             self.logger.info("Far field sphere %s is created.", setup)
 
+        frequency_units = self.odesktop.GetDefaultUnit("Frequency")
         if setup in self.existing_analysis_sweeps and not frequencies:
             trace_name = "mag(rETheta)"
             farfield_data = self.post.get_far_field_data(expressions=trace_name, setup_sweep_name=setup, domain=sphere)
             if farfield_data and getattr(farfield_data, "primary_sweep_values", None) is not None:
                 frequencies = farfield_data.primary_sweep_values
-                frequency_units = self.odesktop.GetDefaultUnit("Frequency")
-                frequencies = [str(freq) + frequency_units for freq in frequencies]
 
-        if not frequencies:  # pragma: no cover
+        if frequencies:
+            frequencies = [str(freq) + frequency_units for freq in frequencies if is_number(freq)]
+        else:  # pragma: no cover
             self.logger.info("Frequencies could not be obtained.")
             return False
 
