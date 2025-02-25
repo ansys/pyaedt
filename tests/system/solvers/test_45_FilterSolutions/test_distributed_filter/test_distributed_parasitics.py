8e67b587eb76cae65ab943f9a706f590241e2b21
Pumichat (Kla) <101877396+anspraksaph@users.noreply.github.com>
2025-02-24 21:57:14 -0600

    Update test_distributed_parasitics.py

--- tests/system/solvers/test_45_FilterSolutions/test_distributed_filter/test_distributed_parasitics.py
+++ tests/system/solvers/test_45_FilterSolutions/test_distributed_filter/test_distributed_parasitics.py
@@ -23,10 +23,10 @@
 # SOFTWARE.
 
 from ansys.aedt.core.filtersolutions_core.distributed_topology import TopologyType
-from ansys.aedt.core.solvers.general_methods import is_linux
+from ansys.aedt.core.generic.general_methods import is_linux
 import pytest
 
-from tests.system.general.conftest import config
+from tests.system.solvers.conftest import config
 
 from ..resources import read_resource_file
 
