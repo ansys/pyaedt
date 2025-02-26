5c8c49627ff35c7b0dcc34790415634e2084b2c3
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-02-26 14:58:38 +0100

    Not used library

--- src/ansys/aedt/core/application/analysis_3d_layout.py
+++ src/ansys/aedt/core/application/analysis_3d_layout.py
@@ -22,7 +22,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
 import warnings
 
 from ansys.aedt.core.application.analysis import Analysis
