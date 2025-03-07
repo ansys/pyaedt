39a570a5634c493900fa585bcbd17348d5885f3b
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-03-07 12:51:14 +0100

    Fix template

--- tests/system/visualization/test_45_workflows.py
+++ tests/system/visualization/test_45_workflows.py
@@ -59,11 +59,18 @@ class TestClass:
         assert main({"is_test": True, "origin_x": 2})
         assert len(aedtapp.modeler.object_list) == 1
 
-        file_path = os.path.join(solvers_local_path, "example_models", "T00", "test_solve.aedt")
+        aedtapp2 = add_app(application=ansys.aedt.core.Hfss, project_name="workflow_test2")
+        aedtapp2.save_project()
+        file_path = aedtapp2.project_file
+        project_name = aedtapp2.project_name
+        aedtapp2.close_project()
+        del aedtapp2
+
         assert main({"is_test": True, "file_path": file_path})
         assert len(aedtapp.project_list) == 2
 
         aedtapp.close_project(aedtapp.project_name)
+        aedtapp.close_project(project_name)
 
     def test_02_hfss_push(self, add_app):
         aedtapp = add_app(project_name=push_project, subfolder=test_subfolder)
