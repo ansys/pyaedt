5b7727c7220aa39de25b6a252483c6fc016fb6f9
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-03-13 20:36:32 +0100

    Fix test

--- tests/system/general/test_02_2D_modeler.py
+++ tests/system/general/test_02_2D_modeler.py
@@ -223,7 +223,7 @@ class TestClass:
         assert os.path.exists(obj2.image_file)
         obj3 = self.aedtapp.plot(show=False, output_file=os.path.join(self.local_scratch.path, "image.jpg"), view="xy1")
         assert filecmp.cmp(obj.image_file, obj3.image_file)
-        assert isinstance(obj.point_cloud(), list)
+        assert isinstance(obj.point_cloud(), dict)
 
     def test_10_edit_menu_commands(self):
         rect1 = self.aedtapp.modeler.create_rectangle([1, 0, -2], [8, 3])
