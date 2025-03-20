19729ff76842f5e8900e97f749dbc5cf39eea7cc
drioskel <dulce.rioskeller@ansys.com>
2025-03-19 17:41:26 -0700

    DOCS: Documentation updates in FilterSolutions

--- src/ansys/aedt/core/filtersolutions_core/optimization_goals_table.py
+++ src/ansys/aedt/core/filtersolutions_core/optimization_goals_table.py
@@ -108,7 +108,7 @@ class OptimizationGoalsTable:
 
     @property
     def row_count(self) -> int:
-        """Number of golas in the optimization goals table.
+        """Number of goals in the optimization goals table.
 
         The default is `0`.
 
