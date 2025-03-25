f0187bf07ee296631cf7b33b2414386298f750b1
Jonathan Velasco <jonathan.velasco@ansys.com>
2025-03-25 14:22:14 +0200

    [refactor][pathlib] doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information -----------------------------------------------------
 import datetime
 import os
-import pathlib
+from pathlib import Path
 import sys
 import warnings
 
@@ -81,7 +81,7 @@ def directory_size(directory_path):
     res = 0
     for path, _, files in os.walk(directory_path):
         for f in files:
-            fp = os.path.join(path, f)
+            fp = Path(path) / f
             res += os.stat(fp).st_size
     # Convert in megabytes
     res /= 1e6
@@ -106,15 +106,15 @@ def setup(app):
     app.connect('build-finished', remove_doctree)
 
 
-local_path = os.path.dirname(os.path.realpath(__file__))
-module_path = pathlib.Path(local_path)
+local_path = Path(__file__).parent
+module_path = Path(local_path)
 root_path = module_path.parent.parent
 try:
     from ansys.aedt.core import __version__
 except ImportError:
 
-    sys.path.append(os.path.abspath(os.path.join(local_path)))
-    sys.path.append(os.path.join(root_path))
+    sys.path.append(str(Path(local_path)))
+    sys.path.append(str(Path(root_path)))
     from ansys.aedt.core import __version__
 
 from ansys.aedt.core import is_windows
