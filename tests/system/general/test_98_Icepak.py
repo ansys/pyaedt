c2b3bc6479747eb1e9d0c49f70f7d976c74e0e07
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-05-28 08:24:11 +0200

    Export Icepak 3d component

--- tests/system/general/test_98_Icepak.py
+++ tests/system/general/test_98_Icepak.py
@@ -31,7 +31,6 @@ from ansys.aedt.core import Icepak
 from ansys.aedt.core.generic.settings import settings
 from ansys.aedt.core.internal.errors import AEDTRuntimeError
 from ansys.aedt.core.modules.boundary.icepak_boundary import NetworkObject
-from ansys.aedt.core.modules.boundary.layout_boundary import NativeComponentObject
 from ansys.aedt.core.modules.boundary.layout_boundary import PCBSettingsDeviceParts
 from ansys.aedt.core.modules.boundary.layout_boundary import PCBSettingsPackageParts
 from ansys.aedt.core.modules.mesh_icepak import MeshRegion
@@ -863,86 +862,6 @@ class TestClass:
             mon_obj.delete()
         assert ipk.monitor.all_monitors == {}
         assert not ipk.monitor.delete_monitor("Test")
-
-    @pytest.mark.skipif(not config["use_grpc"], reason="Not running in COM mode")
-    @pytest.mark.parametrize("ipk", [board_ipk], indirect=True)
-    def test047__advanced3dcomp_export(self, ipk, local_scratch):
-        surf1 = ipk.modeler.create_rectangle(ipk.PLANE.XY, [0, 0, 0], [10, 20], name="surf1")
-        box1 = ipk.modeler.create_box([20, 20, 2], [10, 10, 3], "box1", "copper")
-        fan = ipk.create_fan("Fan", cross_section="YZ", radius="1mm", hub_radius="0mm")
-        cs0 = ipk.modeler.create_coordinate_system(name="CS0")
-        cs0.props["OriginX"] = 10
-        cs0.props["OriginY"] = 10
-        cs0.props["OriginZ"] = 10
-        cs1 = ipk.modeler.create_coordinate_system(name="CS1", reference_cs="CS0")
-        cs1.props["OriginX"] = 10
-        cs1.props["OriginY"] = 10
-        cs1.props["OriginZ"] = 10
-        fan2_prop = dict(fan.props).copy()
-        fan2_prop["TargetCS"] = "CS1"
-        fan2 = NativeComponentObject(ipk, "Fan", "Fan2", fan2_prop)
-        fan2.create()
-        ipk.create_ipk_3dcomponent_pcb(
-            "Board", link_data, solution_freq, resolution, custom_x_resolution=400, custom_y_resolution=500
-        )
-        ipk.monitor.assign_face_monitor(box1.faces[0].id, "Temperature", "FaceMonitor")
-        ipk.monitor.assign_point_monitor_in_object(box1.name, "Temperature", "BoxMonitor")
-        ipk.monitor.assign_surface_monitor(surf1.name, "Temperature", "SurfaceMonitor")
-        ipk.create_dataset(
-            "test_dataset",
-            [1, 2, 3, 4],
-            [1, 2, 3, 4],
-            z=None,
-            v=None,
-            is_project_dataset=False,
-            x_unit="cel",
-            y_unit="W",
-            v_unit="",
-        )
-        file_path = local_scratch.path
-        file_name = "Advanced3DComp.a3dcomp"
-        fan_obj = ipk.create_fan(is_2d=True)
-        ipk.monitor.assign_surface_monitor(
-            list(ipk.modeler.user_defined_components[fan_obj.name].parts.values())[0].name
-        )
-        ipk.monitor.assign_face_monitor(
-            list(ipk.modeler.user_defined_components[fan_obj.name].parts.values())[0].faces[0].id
-        )
-        fan_obj_3d = ipk.create_fan(is_2d=False)
-        ipk.monitor.assign_point_monitor_in_object(
-            list(ipk.modeler.user_defined_components[fan_obj_3d.name].parts.values())[0].name
-        )
-        assert ipk.modeler.create_3dcomponent(
-            os.path.join(file_path, file_name),
-            name="board_assembly",
-            coordinate_systems=["Global"],
-            export_auxiliary=True,
-        )
-        ipk.create_dataset(
-            "test_ignore",
-            [1, 2, 3, 4],
-            [1, 2, 3, 4],
-            z=None,
-            v=None,
-            is_project_dataset=False,
-            x_unit="cel",
-            y_unit="W",
-            v_unit="",
-        )
-        file_name = "Advanced3DComp1.a3dcomp"
-        mon_list = list(ipk.monitor.all_monitors.keys())
-        ipk.monitor.assign_point_monitor([0, 0, 0])
-        cs_list = [cs.name for cs in ipk.modeler.coordinate_systems if cs.name != "CS0"]
-        ipk.modeler.create_coordinate_system()
-        assert ipk.modeler.create_3dcomponent(
-            os.path.join(file_path, file_name),
-            name="board_assembly",
-            coordinate_systems=cs_list,
-            reference_coordinate_system="CS1",
-            export_auxiliary=True,
-            monitor_objects=mon_list,
-            datasets=["test_dataset"],
-        )
 
     @pytest.mark.skipif(not config["use_grpc"], reason="Not running in COM mode")
     @pytest.mark.parametrize("ipk", [board_ipk], indirect=True)
