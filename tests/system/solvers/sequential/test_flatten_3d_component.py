9743461ae1eab345a9f5aba1f41c86db9fba6958
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-05-28 12:09:23 +0200

    Move flatten component

---
+++ tests/system/solvers/sequential/test_flatten_3d_component.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from pathlib import Path
+
+import pytest
+
+from ansys.aedt.core import Icepak
+from ansys.aedt.core.modules.boundary.layout_boundary import NativeComponentObject
+from tests.system.general.conftest import config
+
+components_flatten = "components_flatten_231"
+test_subfolder = "flatten_component"
+
+
+@pytest.fixture(scope="class")
+def flatten(add_app):
+    app = add_app(project_name=components_flatten, subfolder=test_subfolder)
+    return app
+
+
+class TestClass:
+    def test_flatten_3d_components(self, flatten):
+        assert flatten.flatten_3d_components()
