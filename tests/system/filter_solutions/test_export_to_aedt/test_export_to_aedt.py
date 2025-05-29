3fcf2b5609708bcd339522801db6286d7f10ec0e
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-05-29 16:01:27 +0200

    Enable COM

--- tests/system/filter_solutions/test_export_to_aedt/test_export_to_aedt.py
+++ tests/system/filter_solutions/test_export_to_aedt/test_export_to_aedt.py
@@ -468,15 +468,16 @@ class TestClass:
         assert lumped_design.export_to_aedt.substrate_dielectric_height == "3 mm"
         assert lumped_design.export_to_aedt.substrate_loss_tangent == "0.065 "
 
-    @pytest.mark.skipif(config["desktopVersion"] == "2025.2", reason="WAITING")
+    # @pytest.mark.skipif(config["desktopVersion"] == "2025.2", reason="WAITING")
     def test_import_tuned_variables(self, lumped_design):
         lumped_design.export_to_aedt.simulate_after_export_enabled = True
         lumped_design.export_to_aedt.optimize_after_export_enabled = True
         lumped_design.export_to_aedt.part_libraries = PartLibraries.LUMPED
-        lumped_design.export_to_aedt.export_design()
+        app = lumped_design.export_to_aedt.export_design()
         assert lumped_design.export_to_aedt.import_tuned_variables().splitlines() == read_resource_file(
             "imported_netlist.ckt", "Lumped"
         )
+        app.release_desktop()
 
     def test_part_libraries(self, lumped_design):
         assert lumped_design.export_to_aedt.part_libraries == PartLibraries.LUMPED
