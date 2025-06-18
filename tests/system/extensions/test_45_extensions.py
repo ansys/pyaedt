e9cd849c05289dc8f38a13fdefd409ea7cf842f6
Sébastien Morais <sebastien.morais@ansys.com>
2025-06-18 15:25:39 +0200

    CHORE: Remove previous test approach

--- tests/system/extensions/test_45_extensions.py
+++ tests/system/extensions/test_45_extensions.py
@@ -591,12 +591,6 @@ class TestClass:
         app_microvia.create(selection=["v40h20-1"], signal_only=True, angle=75)
         h3d.close_project()
 
-    def test_via_design(self, local_scratch):
-        from ansys.aedt.core.extensions.project.via_design import ViaDesignFrontend
-        from ansys.aedt.core.extensions.project.via_design import main
-
-        main(is_test=True, file_path=ViaDesignFrontend.TabRF.fpath_config, output_dir=local_scratch.path)
-
     def test_citcuit_configuration(self, local_scratch):
         from ansys.aedt.core.extensions.circuit.circuit_configuration import main
 
