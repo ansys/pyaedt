4b00de5367ab199b72d044d303809d182b671259
Edouard Coussoux <edouard.coussoux@ansys.com>
2025-06-23 11:41:50 +0200

    chore: Reinstate deleted files with unused imports for backward compatibility

---
+++ src/pyaedt/generic/near_field_import.py
@@ -0,0 +1,1 @@
+from ansys.aedt.core.visualization.advanced.misc import convert_nearfield_data  # noqa: F401
---
+++ src/pyaedt/generic/report_file_parser.py
@@ -0,0 +1,1 @@
+from ansys.aedt.core.visualization.advanced.misc import parse_rdat_file  # noqa: F401
