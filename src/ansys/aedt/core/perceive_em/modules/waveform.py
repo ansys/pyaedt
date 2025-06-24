191873cd641dc5aa7f6195be7ff007807d1de30c
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-06-24 08:31:41 +0200

    Waveform

--- src/ansys/aedt/core/perceive_em/modules/waveform.py
+++ src/ansys/aedt/core/perceive_em/modules/waveform.py
@@ -28,7 +28,22 @@ from ansys.aedt.core.generic.constants import SpeedOfLight
 
 
 class Waveform:
+    """
+    Base class representing a generic waveform configuration.
+
+    This class provides basic transmitter and receiver settings,
+    along with specifications for signal processing windows.
+    """
+
     def __init__(self):
+        """
+        Initialize the Waveform with default parameters.
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        """
         self.__tx_incident_power = 1.0
         self.__rx_noise_db = None
         self.__rx_gain_db = None
@@ -44,28 +59,97 @@ class Waveform:
         self.output = "default"
 
     @property
-    def mode_delay(self):
+    def mode_delay(self) -> str:
+        """Mode delay.
+
+        Returns
+        -------
+        str
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        >>> waveform.mode_delay
+        """
         return self.__mode_delay
 
     @property
     def tx_multiplex(self):
+        """Transmission multiplexing mode.
+
+        Returns
+        -------
+        str
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        >>> waveform.tx_multiplex
+        """
         return self.__tx_multiplex
 
     @property
     def tx_incident_power(self):
+        """Transmitted signal power.
+
+        Returns
+        -------
+        str
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        >>> waveform.tx_incident_power
+        """
         return self.__tx_incident_power
 
     @property
     def rx_noise_db(self):
+        """Receiver noise level in decibels.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        >>> waveform.rx_noise_db
+        """
         return self.__rx_noise_db
 
     @property
     def rx_gain_db(self):
+        """Receiver gain level in decibels.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import Waveform
+        >>> waveform = Waveform()
+        >>> waveform.rx_gain_db
+        """
         return self.__rx_gain_db
 
 
 class RangeDopplerWaveform(Waveform):
+    """Waveform for range-Doppler radar processing."""
+
     def __init__(self):
+        """Initialize the range-Doppler waveform with default parameters.
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        """
         super().__init__()
 
         self.mode = "pulse_doppler"
@@ -101,7 +185,19 @@ class RangeDopplerWaveform(Waveform):
         )
 
     @property
-    def bandwidth(self):
+    def bandwidth(self) -> float:
+        """System bandwidth in Hz.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.bandwidth
+        """
         return self.__bandwidth
 
     @bandwidth.setter
@@ -110,7 +206,19 @@ class RangeDopplerWaveform(Waveform):
         self.system_to_performance(bandwidth=value)
 
     @property
-    def frequency_samples(self):
+    def frequency_samples(self) -> int:
+        """Number of frequency domain samples.
+
+        Returns
+        -------
+        int
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.frequency_samples
+        """
         return self.__frequency_samples
 
     @frequency_samples.setter
@@ -119,7 +227,19 @@ class RangeDopplerWaveform(Waveform):
         self.system_to_performance(frequency_samples=value)
 
     @property
-    def cpi_duration(self):
+    def cpi_duration(self) -> float:
+        """Coherent processing interval in seconds.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.cpi_duration
+        """
         return self.__cpi_duration
 
     @cpi_duration.setter
@@ -128,7 +248,19 @@ class RangeDopplerWaveform(Waveform):
         self.system_to_performance(cpi_duration=value)
 
     @property
-    def pulse_cpi(self):
+    def pulse_cpi(self) -> int:
+        """Number of pulses per coherent processing interval.
+
+        Returns
+        -------
+        int
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.pulse_cpi
+        """
         return self.__pulse_cpi
 
     @pulse_cpi.setter
@@ -137,7 +269,19 @@ class RangeDopplerWaveform(Waveform):
         self.system_to_performance(pulse_cpi=value)
 
     @property
-    def range_resolution(self):
+    def range_resolution(self) -> float:
+        """Range resolution in meters.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.range_resolution
+        """
         return self.__range_resolution
 
     @range_resolution.setter
@@ -146,7 +290,19 @@ class RangeDopplerWaveform(Waveform):
         self.performance_to_system(range_resolution=value)
 
     @property
-    def range_period(self):
+    def range_period(self) -> float:
+        """Maximum measurable range period.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.range_period
+        """
         return self.__range_period
 
     @range_period.setter
@@ -155,7 +311,19 @@ class RangeDopplerWaveform(Waveform):
         self.performance_to_system(range_period=value)
 
     @property
-    def velocity_resolution(self):
+    def velocity_resolution(self) -> float:
+        """Velocity resolution in m/s.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.velocity_resolution
+        """
         return self.__velocity_resolution
 
     @velocity_resolution.setter
@@ -164,7 +332,19 @@ class RangeDopplerWaveform(Waveform):
         self.performance_to_system(velocity_resolution=value)
 
     @property
-    def velocity_period(self):
+    def velocity_period(self) -> float:
+        """Maximum measurable velocity period.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.velocity_period
+        """
         return self.__velocity_period
 
     @velocity_period.setter
@@ -173,14 +353,61 @@ class RangeDopplerWaveform(Waveform):
         self.performance_to_system(velocity_period=value)
 
     @property
-    def wavelength(self):
+    def wavelength(self) -> float:
+        """Wavelength derived from center frequency.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.wavelength
+        """
         return SpeedOfLight / self.center_frequency
 
     @property
-    def pulse_interval(self):
+    def pulse_interval(self) -> float:
+        """Interval between radar pulses.
+
+        Returns
+        -------
+        float
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> waveform.pulse_interval
+        """
         return self.__pulse_interval
 
-    def system_to_performance(self, bandwidth=None, frequency_samples=None, cpi_duration=None, pulse_cpi=None):
+    def system_to_performance(
+        self,
+        bandwidth: float = None,
+        frequency_samples: float = None,
+        cpi_duration: float = None,
+        pulse_cpi: int = None,
+    ):
+        """Convert system parameters to performance characteristics.
+
+        Parameters
+        ----------
+        bandwidth : float, optional
+        frequency_samples : int, optional
+        cpi_duration : float, optional
+        pulse_cpi : int, optional
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> old_range = waveform.range_resolution
+        >>> waveform.system_to_performance(bandwidth=1e6)
+        >>> new_range = waveform.range_resolution
+        """
         if bandwidth is not None:
             self.__bandwidth = bandwidth
         if frequency_samples is not None:
@@ -201,6 +428,23 @@ class RangeDopplerWaveform(Waveform):
     def performance_to_system(
         self, range_resolution=None, range_period=None, velocity_resolution=None, velocity_period=None
     ):
+        """Convert performance characteristics to system parameters.
+
+        Parameters
+        ----------
+        range_resolution : float, optional
+        range_period : float, optional
+        velocity_resolution : float, optional
+        velocity_period : float, optional
+
+        Examples
+        --------
+        >>> from ansys.aedt.core.perceive_em.modules.waveform import RangeDopplerWaveform
+        >>> waveform = RangeDopplerWaveform()
+        >>> old_bandwidth = waveform.bandwidth
+        >>> waveform.performance_to_system(range_resolution=0.1)
+        >>> new_bandwidth = waveform.bandwidth
+        """
         if range_resolution is not None:
             self.__range_resolution = range_resolution
         if range_period is not None:
