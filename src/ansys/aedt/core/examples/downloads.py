a21830f49b68268023d37a4157349fe408b79bb3
Sébastien Morais <sebastien.morais@ansys.com>
2025-07-06 09:52:29 +0200

    FIX: Add nosec for controled URL

--- src/ansys/aedt/core/examples/downloads.py
+++ src/ansys/aedt/core/examples/downloads.py
@@ -101,10 +101,8 @@ def _download_file(
             ssl_context = ssl.create_default_context()
             pyaedt_logger.debug(f"Downloading file from URL {url}")
             with (
-                urllib.request.urlopen(url, context=ssl_context) as response,
-                open(  # nosec
-                    local_path, "wb"
-                ) as out_file,
+                urllib.request.urlopen(url, context=ssl_context) as response,  # nosec
+                open(local_path, "wb") as out_file,
             ):
                 shutil.copyfileobj(response, out_file)
         else:
