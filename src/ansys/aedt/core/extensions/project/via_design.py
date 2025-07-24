73f5795d086f8879efb90ac49772168cd8d3e758
ring630 <@gmail.com>
2025-07-24 11:43:55 +0200

    fix

--- src/ansys/aedt/core/extensions/project/via_design.py
+++ src/ansys/aedt/core/extensions/project/via_design.py
@@ -189,6 +189,7 @@ class ViaDesignExtension(ExtensionProjectCommon):
             stacked_vias_name = param_value["stacked_vias"]
             dict_config["differential_signals"][param_name]["stacked_vias"] = stacked_vias[stacked_vias_name]
 
+        dict_config["general"]["version"] = VERSION
         backend = ViaDesignBackend(dict_config)
         hfss_3d = Hfss3dLayout(
             project=backend.app.edbpath,
