72b50d369beebcc3a46e2d4eeb5e1b83c1a143ee
hui-zhou-a <hui.zhou@ansys.com>
2025-08-27 08:04:05 +0200

    FIX: configure layout test

--- src/ansys/aedt/core/extensions/project/resources/configure_layout/template.py
+++ src/ansys/aedt/core/extensions/project/resources/configure_layout/template.py
@@ -135,21 +135,7 @@ SERDES_CONFIG = {
                 "hole_range": "upper_pad_to_lower_pad",
                 "hole_parameters": {"shape": "circle", "diameter": "0.25mm"},
             },
-        ],
-        "instances": [
-            {
-                "name": "Via313",
-                "backdrill_parameters": {
-                    "from_bottom": {"drill_to_layer": "Inner3", "diameter": "1mm", "stub_length": "0.2mm"}
-                },
-            },
-            {
-                "name": "Via314",
-                "backdrill_parameters": {
-                    "from_bottom": {"drill_to_layer": "Inner3", "diameter": "1mm", "stub_length": "0.2mm"}
-                },
-            },
-        ],
+        ]
     },
     "ports": [
         {
