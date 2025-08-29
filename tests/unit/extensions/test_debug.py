b4f2ffdfa7176ef3d19086e196eb40667d649f5f
hui-zhou-a <hui.zhou@ansys.com>
2025-08-29 12:14:14 +0200

    fix

--- tests/unit/extensions/test_debug.py
+++ tests/unit/extensions/test_debug.py
@@ -57,4 +57,5 @@ def test_export_template():
     b1.grid(row=row, column=0, padx=5, pady=5, sticky="w")
     assert var_active_design.get() == 0
     b1.invoke()
+    assert var_active_design.get() == 1
     assert root.getvar("var1") == 1
