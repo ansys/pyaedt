540991b161887ea8eb9e1d4595d26495d4db1eff
jsalant <josh.salant@ansys.com>
2025-09-08 11:08:47 -0400

    add copyright to init
    fix some typos

--- src/ansys/aedt/core/emit_core/nodes/generated/emi_plot_marker_node.py
+++ src/ansys/aedt/core/emit_core/nodes/generated/emi_plot_marker_node.py
@@ -5,18 +5,18 @@
 #
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files(the "Software"), to deal
+# of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 # copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions :
+# furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -195,7 +195,7 @@ class EmiPlotMarkerNode(EmitNode):
     def font(self):
         """Specify font used for the label.
 
-        Value formated like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
+        Value formatted like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
         """
         val = self._get_property("Font")
         return val
--- src/ansys/aedt/core/emit_core/nodes/generated/result_plot_node.py
+++ src/ansys/aedt/core/emit_core/nodes/generated/result_plot_node.py
@@ -5,18 +5,18 @@
 #
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files(the "Software"), to deal
+# of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 # copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions :
+# furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -51,7 +51,7 @@ class ResultPlotNode(EmitNode):
     def title_font(self):
         """Configure title font family, typeface, and size.
 
-        Value formated like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
+        Value formatted like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
         """
         val = self._get_property("Title Font")
         return val
@@ -77,7 +77,7 @@ class ResultPlotNode(EmitNode):
     def legend_font(self):
         """Configure legend font family, typeface, and size.
 
-        Value formated like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
+        Value formatted like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
         """
         val = self._get_property("Legend Font")
         return val
@@ -289,7 +289,7 @@ class ResultPlotNode(EmitNode):
     def axis_label_font(self):
         """Configure axis text labels font family, typeface, and size.
 
-        Value formated like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
+        Value formatted like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
         """
         val = self._get_property("Axis Label Font")
         return val
@@ -302,7 +302,7 @@ class ResultPlotNode(EmitNode):
     def axis_tick_label_font(self):
         """Configure axis tick numeric labels font family, typeface, and size.
 
-        Value formated like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
+        Value formatted like 'Sans Serif,10,-1,5,50,0,0,0,0,0'.
         """
         val = self._get_property("Axis Tick Label Font")
         return val
