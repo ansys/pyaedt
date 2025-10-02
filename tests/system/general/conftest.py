fa3eb44ae8295dad830b18bbf95a22ba60269cad
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2025-10-02 13:03:08 +0200

    Updated conftest docs AEDT version

--- tests/system/general/conftest.py
+++ tests/system/general/conftest.py
@@ -34,7 +34,7 @@ This module contains the configuration and fixture for the pytest-based unit tes
 The default configuration can be changed by placing a file called local_config.json in the same
 directory as this module. An example of the contents of local_config.json
 {
-  "desktopVersion": "2022.2",
+  "desktopVersion": "2025.2",
   "NonGraphical": false,
   "NewThread": false,
 }
