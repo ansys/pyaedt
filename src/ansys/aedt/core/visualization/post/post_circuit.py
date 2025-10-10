1c4d85f5133648119e13c69ad348ff0d8a0f6ae3
Alberto Di Maria <84965833+Alberto-DM@users.noreply.github.com>
2025-10-10 18:43:51 +0200

    FEAT: add dir as property (#6716)

--- src/ansys/aedt/core/visualization/post/post_circuit.py
+++ src/ansys/aedt/core/visualization/post/post_circuit.py
@@ -28,6 +28,7 @@ import warnings
 
 import numpy as np
 
+from ansys.aedt.core.base import PyAedtBase
 from ansys.aedt.core.generic.constants import unit_converter
 from ansys.aedt.core.generic.file_utils import generate_unique_name
 from ansys.aedt.core.generic.general_methods import pyaedt_function_handler
@@ -42,7 +43,7 @@ except ImportError:
     )
 
 
-class PostProcessorCircuit(PostProcessorCommon):
+class PostProcessorCircuit(PostProcessorCommon, PyAedtBase):
     """Manages the main schematic postprocessing functions.
 
     .. note::
