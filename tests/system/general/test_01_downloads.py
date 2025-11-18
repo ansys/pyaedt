62fe90450041cff88748615b36b4f5eca6dbf272
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-11-18 11:02:02 +0100

    Fix typo

--- tests/system/general/test_01_downloads.py
+++ tests/system/general/test_01_downloads.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
+from pathlib import Path
 import tempfile
 
 import pytest
@@ -43,72 +43,84 @@ class TestClass:
     def init(self):
         self.examples = downloads
 
-    def test_00_download_edb(self):
+    def test_download_edb(self):
         assert self.examples.download_aedb()
 
-    def test_01_download_touchstone(self):
+    def test_download_touchstone(self):
         assert self.examples.download_touchstone()
 
-    def test_02_download_netlist(self):
+    def test_download_netlist(self):
         assert self.examples.download_netlist()
 
-    def test_03_download_sbr(self):
+    def test_download_sbr(self):
         assert self.examples.download_sbr()
 
-    def test_04_download_antenna_array(self):
+    def test_download_antenna_array(self):
         assert self.examples.download_antenna_array()
 
-    def test_05_download_antenna_sherlock(self):
-        assert self.examples.download_sherlock(destination=os.path.join(tempfile.gettempdir(), "sherlock"))
+    def test_download_antenna_sherlock(self):
+        assert self.examples.download_sherlock(destination=Path(tempfile.gettempdir()) / "sherlock")
 
     @pytest.mark.skipif(is_linux, reason="Crashes on Linux")
-    def test_06_download_multiparts(self):
-        assert self.examples.download_multiparts(destination=os.path.join(tempfile.gettempdir(), "multi"))
+    def test_download_multiparts(self):
+        assert self.examples.download_multiparts(destination=Path(tempfile.gettempdir()) / "multi")
 
-    def test_07_download_wfp(self):
+    def test_download_wfp(self):
         assert self.examples.download_edb_merge_utility(True)
         assert self.examples.download_edb_merge_utility(True, destination=tempfile.gettempdir())
         out = self.examples.download_edb_merge_utility()
-        assert os.path.exists(out)
+        assert Path(out).exists()
         new_name = generate_unique_name("test")
-        new_path = os.path.split(out)[0] + new_name
-        os.rename(os.path.split(out)[0], new_path)
-        assert os.path.exists(new_path)
 
-    def test_08_download_leaf(self):
+        out_path = Path(out)
+        new_path = out_path.parent / new_name
+
+        out_path.parent.rename(new_path)
+
+        assert new_path.exists()
+
+    def test_download_leaf(self):
         out = self.examples.download_leaf()
-        assert os.path.exists(out[0])
-        assert os.path.exists(out[1])
+
+        assert Path(out[0]).exists()
+        assert Path(out[1]).exists()
+
         new_name = generate_unique_name("test")
-        new_path = os.path.split(out[0])[0] + new_name
-        os.rename(os.path.split(out[0])[0], new_path)
-        assert os.path.exists(new_path)
 
-    def test_09_download_custom_report(self):
+        orig_path = Path(out[0])
+        orig_dir = orig_path.parent
+
+        new_path = orig_dir.with_name(new_name)
+
+        orig_dir.rename(new_path)
+
+        assert new_path.exists()
+
+    def test_download_custom_report(self):
         out = self.examples.download_custom_reports()
-        assert os.path.exists(out)
+        assert Path(out).exists()
 
-    def test_10_download_3dcomp(self):
+    def test_download_3dcomp(self):
         out = self.examples.download_3dcomponent()
-        assert os.path.exists(out)
+        assert Path(out).exists()
 
-    def test_11_download_twin_builder_data(self):
+    def test_download_twin_builder_data(self):
         example_folder = self.examples.download_twin_builder_data("Ex1_Mechanical_DynamicRom.zip", True)
-        assert os.path.exists(example_folder)
+        assert Path(example_folder).exists()
 
-    def test_12_download_specific_file(self):
+    def test_download_specific_file(self):
         example_folder = self.examples.download_file("motorcad", "IPM_Vweb_Hairpin.mot")
-        assert os.path.exists(example_folder)
+        assert Path(example_folder).exists()
 
-    def test_13_download_specific_folder(self):
+    def test_download_specific_folder(self):
         example_folder = self.examples.download_file(directory="nissan")
-        assert os.path.exists(example_folder)
+        assert Path(example_folder).exists()
         example_folder = self.examples.download_file(directory="wpf_edb_merge")
-        assert os.path.exists(example_folder)
+        assert Path(example_folder).exists()
 
-    def test_14_download_icepak_3d_component(self):
+    def test_download_icepak_3d_component(self):
         assert self.examples.download_icepak_3d_component()
 
-    def test_15_download_fss_file(self):
+    def test_download_fss_file(self):
         example_folder = self.examples.download_fss_3dcomponent()
-        assert os.path.exists(example_folder)
+        assert Path(example_folder).exists()
