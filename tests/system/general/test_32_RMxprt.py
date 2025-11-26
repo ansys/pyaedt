ffa6dae05ec984fad7da02fbb9c55a7846435319
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2025-11-26 08:01:51 +0100

    TEST: Refactor already independent tests removing testclass (#6933)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- tests/system/general/test_32_RMxprt.py
+++ tests/system/general/test_32_RMxprt.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
+from pathlib import Path
 
 import pytest
 
@@ -32,73 +32,73 @@ from ansys.aedt.core.generic.general_methods import is_linux
 test_project_name = "motor"
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture()
 def aedtapp(add_app):
     app = add_app(application=Rmxprt)
-    return app
+    yield app
+    app.close_project(app.project_name)
 
 
 @pytest.mark.skipif(is_linux, reason="Emit API fails on linux.")
-class TestClass:
-    @pytest.fixture(autouse=True)
-    def init(self, aedtapp, local_scratch):
-        self.aedtapp = aedtapp
-        self.local_scratch = local_scratch
+def test_save(aedtapp, local_scratch):
+    test_project = Path(local_scratch.path) / f"{test_project_name}.aedt"
+    aedtapp.save_project(test_project)
+    assert test_project.exists()
 
-    def test_01_save(self):
-        test_project = os.path.join(self.local_scratch.path, test_project_name + ".aedt")
-        self.aedtapp.save_project(test_project)
-        assert os.path.exists(test_project)
 
-    def test_02_changeesolution(self):
-        assert self.aedtapp.disable_modelcreation("ORIM")
-        assert self.aedtapp.disable_modelcreation("AFIM")
-        assert self.aedtapp.disable_modelcreation("HM")
-        assert self.aedtapp.disable_modelcreation("LSSM")
-        assert self.aedtapp.disable_modelcreation("UNIM")
-        assert self.aedtapp.disable_modelcreation("LSSM")
-        assert self.aedtapp.enable_modelcreation("WRIM")
+def test_changeesolution(aedtapp):
+    assert aedtapp.disable_modelcreation("ORIM")
+    assert aedtapp.disable_modelcreation("AFIM")
+    assert aedtapp.disable_modelcreation("HM")
+    assert aedtapp.disable_modelcreation("LSSM")
+    assert aedtapp.disable_modelcreation("UNIM")
+    assert aedtapp.disable_modelcreation("LSSM")
+    assert aedtapp.enable_modelcreation("WRIM")
 
-    def test_03_getchangeproperty(self):
-        # test increment statorOD by 1mm
-        self.aedtapp.disable_modelcreation("ASSM")
-        statorOD = self.aedtapp.stator["Outer Diameter"]
-        assert statorOD
-        self.aedtapp.stator["Outer Diameter"] = statorOD + "+1mm"
 
-    def test_04_create_setup(self):
-        # first test GRM (use Inner-Rotor Induction Machine)
-        assert self.aedtapp.enable_modelcreation("IRIM")
-        mysetup = self.aedtapp.create_setup()
-        assert mysetup.props["RatedOutputPower"]
-        mysetup.props["RatedOutputPower"] = "100W"
-        assert mysetup.update()  # update only needed for assertion
-        # second test ASSM setup
-        self.aedtapp.delete_setup(mysetup.name)
-        assert self.aedtapp.disable_modelcreation("ASSM")
-        mysetup = self.aedtapp.create_setup()
-        assert mysetup.props["RatedSpeed"]
-        mysetup.props["RatedSpeed"] = "3600rpm"
-        assert mysetup.update()  # update only needed for assertion
-        # third test TPSM/SYNM setup
-        self.aedtapp.delete_setup(mysetup.name)
-        assert self.aedtapp.disable_modelcreation("TPSM")
-        mysetup = self.aedtapp.create_setup()
-        assert mysetup.props["RatedVoltage"]
-        mysetup.props["RatedVoltage"] = "208V"
-        assert mysetup.update()  # update only needed for assertion
+def test_getchangeproperty(aedtapp):
+    # test increment statorOD by 1mm
+    aedtapp.disable_modelcreation("ASSM")
+    statorOD = aedtapp.stator["Outer Diameter"]
+    assert statorOD
+    aedtapp.stator["Outer Diameter"] = statorOD + "+1mm"
 
-    def test_05_set_material_threshold(self):
-        assert self.aedtapp.set_material_threshold()
-        conductivity = 123123123
-        permeability = 3
-        assert self.aedtapp.set_material_threshold(conductivity, permeability)
-        assert self.aedtapp.set_material_threshold(str(conductivity), str(permeability))
-        assert not self.aedtapp.set_material_threshold("e", str(permeability))
-        assert not self.aedtapp.set_material_threshold(conductivity, "p")
 
-    def test_06_set_variable(self):
-        self.aedtapp.variable_manager.set_variable("var_test", expression="123")
-        self.aedtapp["var_test"] = "234"
-        assert "var_test" in self.aedtapp.variable_manager.design_variable_names
-        assert self.aedtapp.variable_manager.design_variables["var_test"].expression == "234"
+def test_create_setup(aedtapp):
+    # first test GRM (use Inner-Rotor Induction Machine)
+    assert aedtapp.enable_modelcreation("IRIM")
+    mysetup = aedtapp.create_setup()
+    assert mysetup.props["RatedOutputPower"]
+    mysetup.props["RatedOutputPower"] = "100W"
+    assert mysetup.update()  # update only needed for assertion
+    # second test ASSM setup
+    aedtapp.delete_setup(mysetup.name)
+    assert aedtapp.disable_modelcreation("ASSM")
+    mysetup = aedtapp.create_setup()
+    assert mysetup.props["RatedSpeed"]
+    mysetup.props["RatedSpeed"] = "3600rpm"
+    assert mysetup.update()  # update only needed for assertion
+    # third test TPSM/SYNM setup
+    aedtapp.delete_setup(mysetup.name)
+    assert aedtapp.disable_modelcreation("TPSM")
+    mysetup = aedtapp.create_setup()
+    assert mysetup.props["RatedVoltage"]
+    mysetup.props["RatedVoltage"] = "208V"
+    assert mysetup.update()  # update only needed for assertion
+
+
+def test_set_material_threshold(aedtapp):
+    assert aedtapp.set_material_threshold()
+    conductivity = 123123123
+    permeability = 3
+    assert aedtapp.set_material_threshold(conductivity, permeability)
+    assert aedtapp.set_material_threshold(str(conductivity), str(permeability))
+    assert not aedtapp.set_material_threshold("e", str(permeability))
+    assert not aedtapp.set_material_threshold(conductivity, "p")
+
+
+def test_set_variable(aedtapp):
+    aedtapp.variable_manager.set_variable("var_test", expression="123")
+    aedtapp["var_test"] = "234"
+    assert "var_test" in aedtapp.variable_manager.design_variable_names
+    assert aedtapp.variable_manager.design_variables["var_test"].expression == "234"
