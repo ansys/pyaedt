6dd48a9fa4a4ab26e60a0d91945554baf77b5f3d
Josh Salant <101826634+jsalant22@users.noreply.github.com>
2026-01-12 05:31:48 -0500

    FIX: Update a couple emit tests (#7073)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>

--- src/ansys/aedt/core/emit_core/nodes/generated/radio_node.py
+++ src/ansys/aedt/core/emit_core/nodes/generated/radio_node.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2021 - 2026 ANSYS, Inc. and/or its affiliates.
-# SPDX-FileCopyrightText: 2021 - 2025 ANSYS, Inc. and /or its affiliates.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -43,10 +42,6 @@ class RadioNode(EmitNode):
     def add_folder(self):
         """Create a New Folder to Organize Bands"""
         return self._add_child_node("Band Folder")
-
-    def rename(self, new_name: str = ""):
-        """Rename this node"""
-        self._rename(new_name)
 
     def duplicate(self, new_name: str = ""):
         """Duplicate this node"""
