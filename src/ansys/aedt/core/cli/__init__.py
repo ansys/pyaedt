bf9122ba065810b54460c88cc0b3a84ee50440b6
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2026-01-13 12:44:27 +0100

    Remove pre-commit

--- src/ansys/aedt/core/cli/__init__.py
+++ src/ansys/aedt/core/cli/__init__.py
@@ -33,6 +33,7 @@ except ImportError:  # pragma: no cover
 
 from ansys.aedt.core.cli.config import config_app
 from ansys.aedt.core.cli.doc import doc_app
+from ansys.aedt.core.cli.docstring_check import check_docstrings
 from ansys.aedt.core.cli.panels import panels_app
 from ansys.aedt.core.cli.process import processes
 from ansys.aedt.core.cli.process import start
@@ -48,6 +49,7 @@ app.add_typer(doc_app, name="doc")
 
 # Register top-level commands
 app.command()(version)
+app.command(name="check-docstrings")(check_docstrings)
 app.command()(processes)
 app.command()(start)
 app.command()(stop)
