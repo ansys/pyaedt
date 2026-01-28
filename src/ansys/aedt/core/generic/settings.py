55dd12b99ec50fe7b40f4f563a967b75b219cb08
Giulia Malinverno <giulia.malinverno@ansys.com>
2026-01-28 09:22:54 +0100

    enable beta feature for 2025.2

--- src/ansys/aedt/core/generic/settings.py
+++ src/ansys/aedt/core/generic/settings.py
@@ -200,6 +200,7 @@ class Settings(PyAedtBase):
             "ANSYSEM_FEATURE_F335896_MECHANICAL_STRUCTURAL_SOLN_TYPE_ENABLE": "1",
             "ANS_MESHER_PROC_DUMP_PREPOST_BEND_SM3": "1",
             "ANSYSEM_FEATURE_F826442_MULTI_FINITE_ARRAYS_ENABLE": "1",
+            "ANSYSEM_F1035460_EDDY_APHI_ENHANCEMENT": "1",
         }
         if is_linux:
             self.__aedt_environment_variables["ANS_NODEPCHECK"] = "1"
