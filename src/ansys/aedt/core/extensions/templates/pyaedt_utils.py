07dd900f1d438e3e33085fc1cfdf2afdd6c00feb
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2026-02-11 18:20:01 +0100

    Restored default value

--- src/ansys/aedt/core/extensions/templates/pyaedt_utils.py
+++ src/ansys/aedt/core/extensions/templates/pyaedt_utils.py
@@ -160,7 +160,7 @@ def environment_variables(oDesktop):
             )
 
 
-def generate_unique_name(root_name, suffix, n):
+def generate_unique_name(root_name, suffix="", n=6):
     char_set = string.ascii_uppercase + string.digits
     unique_name = root_name + "_" + "".join(random.choice(char_set) for _ in range(n))  # nosec B311
     if suffix:
