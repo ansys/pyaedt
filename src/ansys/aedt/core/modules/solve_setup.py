2a93e15c762b38a49db03f7bdaa985044eba5416
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2026-02-18 10:38:47 +0100

    Remove chnges

--- src/ansys/aedt/core/modules/solve_setup.py
+++ src/ansys/aedt/core/modules/solve_setup.py
@@ -2951,8 +2951,6 @@ class SetupHFSS(Setup, PyAedtBase):
             name = generate_unique_name("Sweep")
         if self.setuptype <= 4:
             sweep_n = SweepHFSS(self, name=name, sweep_type=sweep_type, props=props)
-        else:
-            sweep_n = SweepMatrix(self, name=name, sweep_type=sweep_type, props=props)
         sweep_n.create()
         self.sweeps.append(sweep_n)
         for setup in self._app.setups:
