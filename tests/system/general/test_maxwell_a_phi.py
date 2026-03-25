ac35961f71d75a846426a5cdd65c0fdfb6375c96
Anderson Santos Nunes <5827392+nunesanderson@users.noreply.github.com>
2026-03-25 16:46:50 +0100

    Update test_maxwell_a_phi.py

--- tests/system/general/test_maxwell_a_phi.py
+++ tests/system/general/test_maxwell_a_phi.py
@@ -193,7 +193,7 @@ def test_assign_setup(m3d_app_ac) -> None:
     )
 
     setup.update()
-    assert m3d_app_ac.setups[0].properties["Max. Number of Passes"] == 12
+    assert m3d_app_ac.setups[0].properties["Max. Number of Passes"] == '12'
     assert m3d_app_ac.setups[0].properties["Percent Error"] == 1
     assert m3d_app_ac.setups[0].properties["Percent Refine"] == 30
     assert m3d_app_ac.setups[0].properties["Min. Number of Passes"] == 2
