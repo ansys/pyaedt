4a83646de70699c222278359df2df918dd5302ed
Sébastien Morais <sebastien.morais@ansys.com>
2026-04-09 09:05:32 +0200

    Revert "FIX: COM connection (#7465)"
    This reverts commit a89e278ab9026b1ecfeb3a1dc2ceb985a94ef2df.

--- tests/system/filter_solutions/test_desktop_types.py
+++ tests/system/filter_solutions/test_desktop_types.py
@@ -29,77 +29,84 @@ from ansys.aedt.core import Hfss
 from ansys.aedt.core import Hfss3dLayout
 from ansys.aedt.core.generic.settings import is_linux
 from tests.conftest import DESKTOP_VERSION
+from tests.conftest import USE_GRPC
 
 
 @pytest.mark.skipif(is_linux, reason="FilterSolutions API is not supported on Linux.")
-@pytest.mark.skipif(DESKTOP_VERSION < "2026.1", reason="Skipped on versions earlier than 2026.1")
+@pytest.mark.skipif(
+    DESKTOP_VERSION < "2026.1" or USE_GRPC, reason="Skipped on versions earlier than 2026.1 or gRPC mode"
+)
 class TestClass:
     def test_lumped_exported_desktop(self, lumped_design):
         schem_name = lumped_design.export_to_aedt.schematic_name
         schem_name_length = len(schem_name)
+        app = lumped_design.export_to_aedt.export_design()
         circuit = lumped_design.export_to_aedt.export_design()
         assert isinstance(circuit, Circuit)
         assert circuit.project_name.split()[0][:schem_name_length] == schem_name
         assert circuit.design_name == schem_name
         assert next(iter(circuit.setup_sweeps_names)) == "FS_Advanced Setup"
         variables = circuit.variable_manager.variables
-        assert variables["C1"].si_value == pytest.approx(1.967e-12)
-        assert variables["L2"].si_value == pytest.approx(1.288e-8)
-        assert variables["C3"].si_value == pytest.approx(6.366e-12)
-        circuit.desktop_class.close_desktop()
+        assert variables["C1"].value == pytest.approx(1.967e-12)
+        assert variables["L2"].value == pytest.approx(1.288e-8)
+        assert variables["C3"].value == pytest.approx(6.366e-12)
+        app.desktop_class.close_desktop()
 
     def test_distributed_circuit_exported_desktop(self, distributed_design):
         schem_name = distributed_design.export_to_aedt.schematic_name
         schem_name_length = len(schem_name)
         distributed_design.export_to_aedt.insert_circuit_design = True
+        app = distributed_design.export_to_aedt.export_design()
         circuit = distributed_design.export_to_aedt.export_design()
         assert isinstance(circuit, Circuit)
         assert circuit.project_name.split()[0][:schem_name_length] == schem_name
         assert circuit.design_name == schem_name
         assert next(iter(circuit.setup_sweeps_names)) == "FS_Advanced Setup"
         variables = circuit.variable_manager.variables
-        assert variables["W1"].si_value == pytest.approx(5.08e-3)
-        assert variables["W2"].si_value == pytest.approx(3.175e-4)
-        assert variables["W3"].si_value == pytest.approx(5.08e-3)
-        assert variables["S1"].si_value == pytest.approx(3.362e-3)
-        assert variables["S2"].si_value == pytest.approx(2.172e-2)
-        assert variables["S3"].si_value == pytest.approx(1.008e-2)
-        circuit.desktop_class.close_desktop()
+        assert variables["W1"].value == pytest.approx(5.08e-3)
+        assert variables["W2"].value == pytest.approx(3.175e-4)
+        assert variables["W3"].value == pytest.approx(5.08e-3)
+        assert variables["S1"].value == pytest.approx(3.362e-3)
+        assert variables["S2"].value == pytest.approx(2.172e-2)
+        assert variables["S3"].value == pytest.approx(1.008e-2)
+        app.desktop_class.close_desktop()
 
     def test_distributed_hfss3dl_exported_desktop(self, distributed_design):
         schem_name = distributed_design.export_to_aedt.schematic_name
         schem_name_length = len(schem_name)
         distributed_design.export_to_aedt.insert_hfss_3dl_design = True
+        app = distributed_design.export_to_aedt.export_design()
         hfss3dl = distributed_design.export_to_aedt.export_design()
         assert isinstance(hfss3dl, Hfss3dLayout)
         assert hfss3dl.project_name.split()[0][:schem_name_length] == schem_name
         assert hfss3dl.design_name == schem_name
         assert next(iter(hfss3dl.setup_sweeps_names)) == "FS_Advanced Setup"
         variables = hfss3dl.variable_manager.variables
-        assert variables["Win"].si_value == pytest.approx(1.234e-3)
-        assert variables["W1"].si_value == pytest.approx(5.08e-3)
-        assert variables["W2"].si_value == pytest.approx(3.175e-4)
-        assert variables["W3"].si_value == pytest.approx(5.08e-3)
-        assert variables["S1"].si_value == pytest.approx(3.36225452227e-3)
-        assert variables["S2"].si_value == pytest.approx(2.17231965814e-2)
-        assert variables["S3"].si_value == pytest.approx(1.00773795179e-2)
-        hfss3dl.desktop_class.close_desktop()
+        assert variables["Win"].value == pytest.approx(1.234e-3)
+        assert variables["W1"].value == pytest.approx(5.08e-3)
+        assert variables["W2"].value == pytest.approx(3.175e-4)
+        assert variables["W3"].value == pytest.approx(5.08e-3)
+        assert variables["S1"].value == pytest.approx(3.36225452227e-3)
+        assert variables["S2"].value == pytest.approx(2.17231965814e-2)
+        assert variables["S3"].value == pytest.approx(1.00773795179e-2)
+        app.desktop_class.close_desktop()
 
     def test_distributed_hfss_exported_desktop(self, distributed_design):
         schem_name = distributed_design.export_to_aedt.schematic_name
         schem_name_length = len(schem_name)
         distributed_design.export_to_aedt.insert_hfss_design = True
+        app = distributed_design.export_to_aedt.export_design()
         hfss = distributed_design.export_to_aedt.export_design()
         assert isinstance(hfss, Hfss)
         assert hfss.project_name.split()[0][:schem_name_length] == schem_name
         assert hfss.design_name == schem_name
         assert next(iter(hfss.setup_sweeps_names)) == "FS_Advanced_Setup"
         variables = hfss.variable_manager.variables
-        assert variables["Win"].si_value == pytest.approx(1.234e-3)
-        assert variables["W1"].si_value == pytest.approx(5.08e-3)
-        assert variables["W2"].si_value == pytest.approx(3.175e-4)
-        assert variables["W3"].si_value == pytest.approx(5.08e-3)
-        assert variables["S1"].si_value == pytest.approx(3.36225452227e-3)
-        assert variables["S2"].si_value == pytest.approx(2.17231965814e-2)
-        assert variables["S3"].si_value == pytest.approx(1.00773795179e-2)
-        hfss.desktop_class.close_desktop()
+        assert variables["Win"].value == pytest.approx(1.234e-3)
+        assert variables["W1"].value == pytest.approx(5.08e-3)
+        assert variables["W2"].value == pytest.approx(3.175e-4)
+        assert variables["W3"].value == pytest.approx(5.08e-3)
+        assert variables["S1"].value == pytest.approx(3.36225452227e-3)
+        assert variables["S2"].value == pytest.approx(2.17231965814e-2)
+        assert variables["S3"].value == pytest.approx(1.00773795179e-2)
+        app.desktop_class.close_desktop()
