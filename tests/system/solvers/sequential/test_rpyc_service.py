6125d94e777ddb9760075ecf6a1a8d22d3515f4e
maxcapodi78 <massimo.capodiferro@ansys.com>
2026-04-10 08:48:23 +0200

    skipped test on windows

--- tests/system/solvers/sequential/test_rpyc_service.py
+++ tests/system/solvers/sequential/test_rpyc_service.py
@@ -21,7 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import os
 import threading
 import time
 
@@ -77,7 +76,7 @@ def rpc_server():
 
 
 @pytest.mark.skipif(
-    DESKTOP_VERSION < "2026.1" or os.name == "posix",
+    DESKTOP_VERSION < "2027.1",
     reason="Not working in versions without grpc patch",
 )
 def test_remote_hfss_workflow_with_mtls(rpc_server, tmp_path, monkeypatch):
