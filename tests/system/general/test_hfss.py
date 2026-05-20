f5b3bbec5b2e9ad1ae5abd7e35053ccd81d1f6c3
slopez <samuel.lopez@synopsys.com>
2026-05-21 00:19:35 +0200

    FEAT: enhance mapping_layers validation to support layer names

--- tests/system/general/test_hfss.py
+++ tests/system/general/test_hfss.py
@@ -2048,8 +2048,8 @@ def test_import_gds_3d(aedt_app, test_tmp_dir) -> None:
     assert aedt_app.import_gds_3d(str(gds_file), {7: (100e-3, 10e-3), 9: (110e-3, 5e-3)}, "mm", 0)
     assert len(aedt_app.modeler.solid_names) == 6
     assert not aedt_app.import_gds_3d(str(gds_file), {})
-    gds_file = TESTS_GENERAL_PATH / "example_models" / "cad" / "GDS" / "gds1not.gds"
-    assert not aedt_app.import_gds_3d(str(gds_file), {7: (100, 10), 9: (110, 5)})
+    gds_file2 = TESTS_GENERAL_PATH / "example_models" / "cad" / "GDS" / "gds1not.gds"
+    assert not aedt_app.import_gds_3d(str(gds_file2), {7: (100, 10), 9: (110, 5)})
 
     with pytest.raises(TypeError):
         aedt_app.import_gds_3d(str(gds_file), {7: [(0, 0)]})
