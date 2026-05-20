3ef20887a63c003ab864fe70d0fb92c4198e79b1
slopez <samuel.lopez@synopsys.com>
2026-05-20 08:16:52 +0200

    Fix subprocess command to filter output for ansysedt

--- src/ansys/aedt/core/generic/general_methods.py
+++ src/ansys/aedt/core/generic/general_methods.py
@@ -746,7 +746,7 @@ def _run_ss_xlp() -> dict[int, int]:
 
     try:
         proc = subprocess.run(
-            [ss_cmd, "-Hnlp"],
+            f"{ss_cmd} -Hnlp | grep ansysedt",
             check=False,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
