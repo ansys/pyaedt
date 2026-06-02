ansys/pyaedt

commit fbad6acab1667237a95dac6a700ceee50fa81841

REFACTOR: Pathlib in multiple files (#6367)
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

src/ansys/aedt/core/hfss.py
@@ -7644,7 +7644,7 @@ class Hfss(FieldAnalysis3D, ScatteringMethods, CreateBoundaryMixin):
         export : bool, optional
             Whether to export the Touchstone file after the simulation.
             The default is ``True``.
-        output_dir : str, optional
+        output_dir : str or :class:`pathlib.Path`, optional
             Path to the directory of exported file. The default is the project path.
 
         Returns
@@ -7656,13 +7656,15 @@ class Hfss(FieldAnalysis3D, ScatteringMethods, CreateBoundaryMixin):
         ----------
         >>> oDesign.SetDesignSettings
         """
+        if isinstance(output_dir, Path):
+            output_dir = str(output_dir)
         if export:
             self.logger.info("Enabling Export On Completion")
         else:
             self.logger.info("Disabling Export On Completion")
         if not output_dir:
             output_dir = ""
-        props = {"Export After Simulation": export, "Export Dir": output_dir}
+        props = {"ExportAfterSolve": export, "ExportDir": output_dir}
         return self.change_design_settings(props)
 
     @pyaedt_function_handler()

src/ansys/aedt/core/application/analysis_3d.py
@@ -53,7 +53,7 @@ class FieldAnalysis3D(Analysis, object):
     ----------
     application : str
         3D application that is to initialize the call.
-    projectname : str, optional
+    projectname : str or :class:`pathlib.Path`, optional
         Name of the project to select or the full path to the project
         or AEDTZ archive to open. The default is ``None``, in which
         case an attempt is made to get an active project. If no
@@ -341,7 +341,9 @@ class FieldAnalysis3D(Analysis, object):
         dict
             Dictionary of variables in the component file.
         """
-        if str(name) not in self.components3d:
+        if isinstance(name, Path):
+            name = str(name)
+        if name not in self.components3d:
             return read_component_file(name)
         else:
             return read_component_file(self.components3d[name])

src/ansys/aedt/core/generic/file_utils.py
@@ -371,23 +371,20 @@ def open_file(
         Opened file object or ``None`` if the file or folder does not exist.
     """
 
-    file_path = str(file_path)
-    file_path = file_path.replace("\\", "/") if file_path[0] != "\\" else file_path
-
     file_path = Path(file_path)
-
     dir_name = file_path.parent
+
     if "r" in file_options:
         if file_path.exists():
-            return open(file_path, file_options, encoding=encoding)
+            return open(str(file_path), file_options, encoding=encoding)
         elif settings.remote_rpc_session and settings.remote_rpc_session.filemanager.pathexists(
             str(file_path)
         ):  # pragma: no cover
             local_file = Path(tempfile.gettempdir()) / file_path.name
             settings.remote_rpc_session.filemanager.download_file(str(file_path), str(local_file))
-            return open(local_file, file_options, encoding=encoding)
+            return open(str(local_file), file_options, encoding=encoding)
     elif dir_name.exists():
-        return open(file_path, file_options, encoding=encoding)
+        return open(str(file_path), file_options, encoding=encoding)
     elif settings.remote_rpc_session and settings.remote_rpc_session.filemanager.pathexists(str(dir_name)):
         if "w" in file_options:
             return settings.remote_rpc_session.create_file(

src/ansys/aedt/core/hfss3dlayout.py
@@ -2525,7 +2525,7 @@ class Hfss3dLayout(FieldAnalysis3DLayout, ScatteringMethods):
             self.logger.info("Disabling Export On Completion")
         if not output_dir:
             output_dir = ""
-        props = {"ExportAfterSolve": export, "ExportDir": output_dir}
+        props = {"ExportAfterSolve": export, "ExportDir": str(output_dir)}
         return self.change_design_settings(props)
 
     @pyaedt_function_handler()

src/ansys/aedt/core/icepak.py
@@ -1685,7 +1685,7 @@ class Icepak(FieldAnalysisIcepak, CreateBoundaryMixin):
             "SherlockExportAsFatigue:=",
             True,
             "SherlockExportDirectory:=",
-            export_directory,
+            str(export_directory),
             "AmbientPressure:=",
             self.value_with_units(gauge_pressure, "n_per_meter_sq"),
             "AmbientRadiationTemperature:=",
@@ -1699,7 +1699,7 @@ class Icepak(FieldAnalysisIcepak, CreateBoundaryMixin):
             "ExportOnSimulationComplete:=",
             export_monitor,
             "ExportDirectory:=",
-            export_directory,
+            str(export_directory),
         ]
         if not isinstance(ambient_temperature, (BoundaryDictionary, dict)):
             arg1.append("AmbientTemperature:=")

src/ansys/aedt/core/internal/filesystem.py
@@ -23,6 +23,7 @@
 # SOFTWARE.
 
 import os
+from pathlib import Path
 import secrets
 import shutil
 import string
@@ -42,14 +43,13 @@ def search_files(dirname, pattern="*"):
     -------
     list
     """
-    import pathlib
 
-    return [os.path.abspath(i) for i in pathlib.Path(dirname).glob(pattern)]
+    return [Path(i).absolute() for i in Path(dirname).glob(pattern)]
 
 
 def my_location():
     """ """
-    return os.path.normpath(os.path.dirname(__file__))
+    return Path(__file__).parent.resolve(strict=False)
 
 
 class Scratch:
@@ -70,15 +70,15 @@ class Scratch:
         self._cleaned = True
         char_set = string.ascii_uppercase + string.digits
         name = "".join(secrets.choice(char_set) for _ in range(6))
-        self._scratch_path = os.path.normpath(os.path.join(local_path, "scratch" + name))
-        if os.path.exists(self._scratch_path):
+        self._scratch_path = (Path(local_path) / ("scratch" + name)).resolve(strict=False)
+        if self._scratch_path.exists():
             try:
                 self.remove()
             except Exception:
                 self._cleaned = False
         if self._cleaned:
             try:
-                os.mkdir(self.path)
+                self.path.mkdir(parents=True, exist_ok=True)
                 os.chmod(self.path, permission)
             except FileNotFoundError as fnf_error:  # Raise error if folder doesn't exist.
                 print(fnf_error)
@@ -86,7 +86,6 @@ class Scratch:
     def remove(self):
         """ """
         try:
-            # TODO check why on Anaconda 3.7 get errors with os.path.exists
             shutil.rmtree(self._scratch_path, ignore_errors=True)
         except Exception:
             logger.error(f"An error occurred while removing {self._scratch_path}")
@@ -98,9 +97,9 @@ class Scratch:
 
         Parameters
         ----------
-        src_file : str
+        src_file : str or :class:`pathlib.Path`
             Source file with fullpath.
-        dst_filename : str, optional
+        dst_filename : str or :class:`pathlib.Path`, optional
             Destination filename with the extension. The default is ``None``,
             in which case the destination file is given the same name as the
             source file.
@@ -111,12 +110,12 @@ class Scratch:
             Full path and file name of the copied file.
         """
         if dst_filename:
-            dst_file = os.path.join(self.path, dst_filename)
+            dst_file = self.path / dst_filename
         else:
-            dst_file = os.path.join(self.path, os.path.basename(src_file))
-        if os.path.exists(dst_file):
+            dst_file = self.path / Path(src_file).name
+        if dst_file.exists():
             try:
-                os.unlink(dst_file)
+                dst_file.unlink()
             except OSError:  # pragma: no cover
                 pass
         try:
@@ -124,7 +123,7 @@ class Scratch:
         except FileNotFoundError as fnf_error:
             print(fnf_error)
 
-        return dst_file
+        return str(dst_file)
 
     def copyfolder(self, src_folder, destfolder):
         """
@@ -169,16 +168,17 @@ def get_json_files(start_folder):
 def is_safe_path(path, allowed_extensions=None):
     """Validate if a path is safe to use."""
     # Ensure path is an existing file or directory
-    if not os.path.exists(path) or not os.path.isfile(path):
+    path = Path(path)
+    if not path.exists() or not path.is_file():
         return False
 
-    # Restrict to allowed file extensions:
+    # # Restrict to allowed file extensions:
     if allowed_extensions:
-        if not any(path.endswith(extension) for extension in allowed_extensions):
+        if path.suffix not in allowed_extensions:
             return False
 
-    # Ensure path does not contain dangerous characters
-    if any(char in path for char in (";", "|", "&", "$", "<", ">", "`")):
+    # # Ensure path does not contain dangerous characters
+    if any(char in str(path) for char in (";", "|", "&", "$", "<", ">", "`")):
         return False
 
     return True

src/ansys/aedt/core/internal/grpc_plugin_dll_class.py
@@ -29,6 +29,7 @@ from ctypes import c_int
 from ctypes import c_wchar_p
 from ctypes import py_object
 import os
+from pathlib import Path
 import re
 import types
 
@@ -266,11 +267,9 @@ class AEDT:
     def __init__(self, pathDir):
         is_linux = os.name == "posix"
         is_windows = not is_linux
+        pathDir = Path(pathDir)
         self.original_path = pathDir
-        self.pathDir = pathDir
-        self.pathDir = os.path.dirname(self.pathDir)  # PythonFiles
-        self.pathDir = os.path.dirname(self.pathDir)  # DesktopPlugin or Win64
-        # dirName = os.path.basename(pathDir)
+        self.pathDir = pathDir.parents[1]
 
         # Plugin filename depends on OS
         if is_linux:
@@ -278,25 +277,22 @@ class AEDT:
         else:
             pluginFileName = r"PyDesktopPlugin.dll"
 
-        AedtAPIDll_file = os.path.join(self.pathDir, pluginFileName)  # install dir
+        AedtAPIDll_file = self.pathDir / pluginFileName  # install dir
 
-        if not os.path.isfile(AedtAPIDll_file):
-            self.pathDir = os.path.dirname(self.pathDir)  # lib
-            self.pathDir = os.path.dirname(self.pathDir)  # core
-            self.pathDir = os.path.dirname(self.pathDir)  # view
-            AedtAPIDll_file = os.path.join(self.pathDir, r"build_output\64Release\PyDesktopPlugin.dll")  # develop dir
-            # AedtAPIDll_file = os.path.join(pathDir, r"PyAedtStub/x64/Debug/PyAedtStub.dll") #develop dir
+        if not AedtAPIDll_file.is_file():
+            self.pathDir = self.pathDir.parents[2]
+            AedtAPIDll_file = self.pathDir / r"build_output\64Release\PyDesktopPlugin.dll"  # develop dir
 
         # load dll
         if is_windows:
             # on windows, modify path
-            aedtDir = os.path.dirname(AedtAPIDll_file)
+            aedtDir = AedtAPIDll_file.parent
             originalPath = os.environ["PATH"]
-            os.environ["PATH"] = originalPath + os.pathsep + aedtDir
-            AedtAPI = PyDLL(AedtAPIDll_file)
+            os.environ["PATH"] = originalPath + os.pathsep + str(aedtDir)
+            AedtAPI = PyDLL(str(AedtAPIDll_file))
             os.environ["PATH"] = originalPath
         else:
-            AedtAPI = PyDLL(AedtAPIDll_file)
+            AedtAPI = PyDLL(str(AedtAPIDll_file))
         # AedtAPI.SetPyObjCalbacks.argtypes = py_object, py_object, py_object
         AedtAPI.SetPyObjCalbacks.restype = None
 
@@ -305,7 +301,7 @@ class AEDT:
         self.callbackCreateBlock = None
         self.callbackGetObjID = None
         version = None
-        with open(os.path.join(self.pathDir, "product.info"), "r") as f:
+        with open(self.pathDir / "product.info", "r") as f:
             for line in f:
                 if "AnsProductVersion" in line:
                     version = line.split("=")[1].strip('\n"')

commit 91c27218a4785cdb43a7ad49e1ac241cc6b7c7a2

Bug introduced in #6367

src/ansys/aedt/core/hfss.py
@@ -7496,7 +7496,7 @@ class Hfss(FieldAnalysis3D, ScatteringMethods, CreateBoundaryMixin, PyAedtBase):
             self.logger.info("Disabling Export On Completion")
         if not output_dir:
             output_dir = ""
-        props = {"ExportAfterSolve": export, "ExportDir": output_dir}
+        props = {"Export After Simulation": export, "Export Dir": output_dir}
         return self.change_design_settings(props)
 
     @pyaedt_function_handler()