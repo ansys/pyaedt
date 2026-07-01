ca6e2299463cb58223fc9c8ebc6ba9938ad36feb
Samuelopez-ansys <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-07-01 10:39:33 +0200

    Fix conflict

--- tests/unit/extensions/test_push_excitation_from_file_3dl.py
+++ tests/unit/extensions/test_push_excitation_from_file_3dl.py
@@ -77,9 +77,7 @@ def test_push_excitation_3dlayout_extension_default(
 def test_push_excitation_3dlayout_extension_generate_button(
     mock_hfss3dl_app_with_excitations,
 ) -> None:
-   """Test the generate button in the Push Excitation 3D Layout
-    extension.
-    """
+    """Test the generate button in the Push Excitation 3D Layout extension."""
     extension = PushExcitation3DLayoutExtension(withdraw=True)
 
     # Set a test file path
