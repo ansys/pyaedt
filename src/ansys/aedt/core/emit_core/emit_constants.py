55ee054ee6c8100c66bef30fd2ddce8290129d76
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-07-01 08:41:24 +0200

    Update src/ansys/aedt/core/emit_core/emit_constants.py
    Co-authored-by: Eduardo Blanco <eduardo.blancofernandez@ansys.com>

--- src/ansys/aedt/core/emit_core/emit_constants.py
+++ src/ansys/aedt/core/emit_core/emit_constants.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""Enums from the ``EmitApiPython`` module are defined as ``None`` until The module initializes.
+"""Enums from the ``EmitApiPython`` module are defined as ``None`` until the module initializes.
 
 This allows IDE auto-complete to find them and ``emit_constants`` to import before the
 ``EmitApiPython`` module has loaded (typically when a ``ansys.aedt.core.Emit()`` object is created).
