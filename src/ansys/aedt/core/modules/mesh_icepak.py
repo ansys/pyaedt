66695e195990a5e9bc416502b895509e894e6710
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-07-01 10:42:07 +0200

    Update src/ansys/aedt/core/modules/mesh_icepak.py
    Co-authored-by: Eduardo Blanco <eduardo.blancofernandez@ansys.com>

--- src/ansys/aedt/core/modules/mesh_icepak.py
+++ src/ansys/aedt/core/modules/mesh_icepak.py
@@ -1510,8 +1510,7 @@ class IcepakMesh(PyAedtBase):
 
     @pyaedt_function_handler()
     def assign_mesh_from_file(self, assignment: list, file_name: str, name: str = None) -> MeshOperation | bool:
-        r"""
-        Assign a mesh from a file to objects.
+        """Assign a mesh from a file to objects.
 
         Parameters
         ----------
