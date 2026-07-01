7ed2e0f4094d106a3c3210b9e91d5a1864e022a2
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-07-01 10:43:17 +0200

    Update src/ansys/aedt/core/modeler/modeler_pcb.py
    Co-authored-by: Eduardo Blanco <eduardo.blancofernandez@ansys.com>

--- src/ansys/aedt/core/modeler/modeler_pcb.py
+++ src/ansys/aedt/core/modeler/modeler_pcb.py
@@ -637,8 +637,7 @@ class Modeler3DLayout(Modeler, Primitives3DLayout, PyAedtBase):
 
     @pyaedt_function_handler()
     def import_ipc2581(self, input_file: str, output_dir: str = None, name: str | None = None) -> bool:
-        r"""
-        Import an IPC file.
+        """Import an IPC file.
 
         Parameters
         ----------
