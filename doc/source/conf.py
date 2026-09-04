3f70f046e9f80079c097303115f3bf739ef1f0c6
Sébastien Morais <sebastien.morais@synopsys.com>
2026-09-04 16:03:53 +0200

    docs: update extra sources link

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -302,9 +302,9 @@ html_theme_options = {
     "show_breadcrumbs": True,
     "use_edit_page_button": True,
     "search_extra_sources": {
-        "Examples": "https://examples.aedt.docs.pyansys.com/",
+        "Examples": "https://examples.aedt.docs.pyansys.com/version/dev",
         "Workflows": "https://workflows.docs.pyansys.com/",
-        "PyEDB": "https://edb.docs.pyansys.com/",
+        "PyEDB": "https://edb.docs.pyansys.com/version/stable",
     },
     "search_filters": {
         "User guide": ["User_guide/"],
