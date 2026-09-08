9d2e320ecfbfdc519d509441b77bbda7b24eefb3
Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-09-08 17:06:24 +0100

    FEAT: Add polygon to HFSS extents (#8073)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- tests/system/layout/test_3dlayout_modeler.py
+++ tests/system/layout/test_3dlayout_modeler.py
@@ -1299,6 +1299,8 @@ def test_clip_plane(aedt_app) -> None:
 
 
 def test_edit_3dlayout_extents(aedt_app) -> None:
+    rect = aedt_app.modeler.create_rectangle("Outline", [0, 0], [6, 8], 3, 2, "myrectangle")
+
     assert aedt_app.edit_hfss_extents(
         diel_extent_type="ConformalExtent",
         diel_extent_horizontal_padding="1mm",
@@ -1306,6 +1308,13 @@ def test_edit_3dlayout_extents(aedt_app) -> None:
         air_vertical_positive_padding="10mm",
         air_vertical_negative_padding="10mm",
         air_horizontal_padding="1mm",
+    )
+
+    assert aedt_app.edit_hfss_extents(
+        diel_extent_type="PolygonExtent",
+        air_extent_type="PolygonExtent",
+        diel_extent_polygon=rect.name,
+        air_extent_polygon=rect.name,
     )
 
 
