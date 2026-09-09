5828f9ed899969dfc8a427fa69fd28ad978ad852
jsalant <101826634+jsalant22@users.noreply.github.com>
2026-09-09 08:11:15 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -65,7 +65,7 @@ EMIT_TEST_PREFIX = "tests/system/emit"
 # ================================
 
 DEFAULT_CONFIG = {
-    "desktopVersion": "2027.1",
+    "desktopVersion": "2026.1",
     "NonGraphical": True,
     "NewThread": True,
     "use_grpc": True,
