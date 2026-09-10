7479c634cbfdc0f8405996c521e938e7e73aa4f0
jsalant <101826634+jsalant22@users.noreply.github.com>
2026-09-10 11:14:07 -0400

    Update emit for python 313

--- src/ansys/aedt/core/modeler/schematic.py
+++ src/ansys/aedt/core/modeler/schematic.py
@@ -35,8 +35,9 @@ from ansys.aedt.core.modeler.circuits.object_3d_circuit import Wire
 
 if TYPE_CHECKING or (3, 7) < sys.version_info < (3, 13):
     from ansys.aedt.core.edb import Edb
-    from ansys.aedt.core.modeler.circuits.primitives_emit import EmitComponent
-    from ansys.aedt.core.modeler.circuits.primitives_emit import EmitComponents
+
+from ansys.aedt.core.modeler.circuits.primitives_emit import EmitComponent
+from ansys.aedt.core.modeler.circuits.primitives_emit import EmitComponents
 
 from ansys.aedt.core.base import PyAedtBase
 from ansys.aedt.core.modeler.circuits.primitives_maxwell_circuit import MaxwellCircuitComponents
@@ -902,12 +903,6 @@ class ModelerEmit(ModelerCircuit, PyAedtBase):
     def __init__(self, app) -> None:
         self._app = app
         ModelerCircuit.__init__(self, app)
-        if not (3, 7) < sys.version_info < (3, 13):
-            self.logger.warning(
-                f"EMIT API is only supported for Python 3.8-3.12. "
-                f"Current version is {sys.version_info.major}.{sys.version_info.minor}. "
-                f"Some features may not be available."
-            )
         self.components = EmitComponents(app, self)
         self.logger.info("ModelerEmit class has been initialized!")
 
