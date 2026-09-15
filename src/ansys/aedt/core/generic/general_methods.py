3c3cc25761cec0afa9193d83efad73302e4b1044
Samuelopez-ansys <85613111+Samuelopez-ansys@users.noreply.github.com>
2026-09-15 13:45:59 +0200

    test

--- src/ansys/aedt/core/generic/general_methods.py
+++ src/ansys/aedt/core/generic/general_methods.py
@@ -232,7 +232,7 @@ def _check_types(arg) -> str:
 def raise_exception_or_return_false(e):
     """Return raise exception or return false."""
     if not settings.enable_error_handler:
-        if settings.release_on_exception:
+        if True:
             from ansys.aedt.core.internal.desktop_sessions import _desktop_sessions
 
             for v in list(_desktop_sessions.values())[:]:
